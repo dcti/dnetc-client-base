c18ac8cd4cd41dc5abbfbe346797a97808ed4382
David Avery <daa@distributed.net>
1998-05-25 08:32:55 +0000

    fixed preferred_contest_id + 1  in the menu display

--- common/cliconfig.cpp
+++ common/cliconfig.cpp
@@ -187,7 +187,7 @@ s32 Client::Configure( void )
                         httpproxy, httpport, uuehttpmode, httpid, cputype,
                         messagelen,smtpsrvr,smtpport,smtpfrom,smtpdest,numcpu,
                         checkpoint_file[0], checkpoint_file[1], randomprefix,
-                        preferred_blocksize, preferred_contest_id);
+                        preferred_blocksize, preferred_contest_id + 1);
     for ( choice = 0; choice < OPTION_COUNT; choice++ )
     {
       if ((choice >= 0 && choice < 1+CONF_FIREMODE) ||
