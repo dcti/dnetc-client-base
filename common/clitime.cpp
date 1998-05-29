80f6cd509fe44a487768955bcd5700e5ac4a553e
Stefan Smietanowski <blast@distributed.net>
1998-05-29 15:05:11 +0000

    AmigaOS changes.

--- common/clitime.cpp
+++ common/clitime.cpp
@@ -92,7 +92,7 @@ struct timeval *CliTimer( struct timeval *tv )
     stv.tv_usec = (stv.tv_usec%1000000);
   }
 #elif (CLIENT_OS == OS_AMIGAOS)
-  int dofallback = timer((unsigned int *)(stv)) );
+  int dofallback = timer((unsigned int *)&stv );
   #define REQUIRES_TIMER_FALLBACK
 #else
   struct timezone tz;
