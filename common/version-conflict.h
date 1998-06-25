26ceb8ea54d71e606caac069cb4629fd14cb6ecc
David Avery <daa@distributed.net>
1998-06-25 14:07:18 +0000

    add DCTI copyright notice and cvs log header bump version to 7026.411

--- common/version-conflict.h
+++ common/version-conflict.h
@@ -1,10 +1,23 @@
+// Hey, Emacs, this a -*-C++-*- file !
+
+// Copyright distributed.net 1997-1998 - All Rights Reserved
+// For use in distributed.net projects only.
+// Any other distribution or use of this source violates copyright.
+// 
+// $Log: version-conflict.h,v $
+// Revision 1.2  1998/06/25 14:07:18  daa
+// add DCTI copyright notice and cvs log header
+// bump version to 7026.411
+//
+//
+
 #ifndef _VERSION_H
 #define _VERSION_H
 
 #define CLIENT_CONTEST      70
-#define CLIENT_BUILD        25
-#define CLIENT_BUILD_FRAC   410
+#define CLIENT_BUILD        26
+#define CLIENT_BUILD_FRAC   411
 
-#define CLIENT_VERSIONSTRING "v2.7025.410"
+#define CLIENT_VERSIONSTRING "v2.7026.411"
 
 #endif // _VERSION_H
