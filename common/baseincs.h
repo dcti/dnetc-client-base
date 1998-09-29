f30c9fb0c3101f2c891ee716985d7db794dc665a
Mike Silbersack <silby@distributed.net>
1998-09-29 23:14:19 +0000

    Fix for the syntax of the last fix. :)

--- common/baseincs.h
+++ common/baseincs.h
@@ -10,6 +10,9 @@
 // ------------------------------------------------------------------
 //
 // $Log: baseincs.h,v $
+// Revision 1.20  1998/09/29 23:14:19  silby
+// Fix for the syntax of the last fix. :)
+//
 // Revision 1.19  1998/09/29 23:11:25  silby
 // Change for freebsd (errno.h)
 //
@@ -214,7 +217,7 @@ extern "C" {
 #elif (CLIENT_OS == OS_LINUX) || (CLIENT_OS == OS_FREEBSD)
   #include <sys/time.h>
   #include <unistd.h>
-  #if ((CLIENT_OS == OS_LINUX) && (__GLIBC__ >= 2)) || (CLIENT_OS==OS_FREEBSD))
+  #if (((CLIENT_OS == OS_LINUX) && (__GLIBC__ >= 2)) || (CLIENT_OS==OS_FREEBSD))
     #include <errno.h> // glibc2 has errno only here
   #endif
 #elif (CLIENT_OS == OS_NETBSD) && (CLIENT_CPU == CPU_ARM)
