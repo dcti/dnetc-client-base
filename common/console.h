ff8df96295628ee3d65e838cf721e8a7b6921e71
Cyrus Patel <cyp@distributed.net>
1998-10-26 02:52:46 +0000

    Remved IS_A_TTY() macros.

--- common/console.h
+++ common/console.h
@@ -5,6 +5,9 @@
 // Any other distribution or use of this source violates copyright.
 //
 // $Log: console.h,v $
+// Revision 1.21  1998/10/26 02:52:46  cyp
+// Remved IS_A_TTY() macros.
+//
 // Revision 1.2  1998/10/11 05:24:31  cyp
 // Implemented ConIsScreen(): a real (not a macro) isatty wrapper.
 //
@@ -40,11 +43,11 @@ int ConOutModal(const char *str); //currently no use for it.
 // do key is not echoed. timeout ==> 0 == don't wait, -1 == wait forever. 
 int ConInKey(int timeout_millisecs); // Returns -1 if err. 0 if timed out.
 
-// ConInStr() does what gets() would do (without the trailing '\n') and
+// ConInStr() does what gets() would do (without the trailing '\n') and the
 // buffer is always '\0' terminated. Returns -1 if err (stdin is not a tty)
 int ConInStr(char *buffer, unsigned int len, int flags );
-#define CONINSTR_BYEXAMPLE  1
-#define CONINSTR_ASPASSWORD 2
+#define CONINSTR_BYEXAMPLE  1  /* the buffer contains a 'live' example */
+#define CONINSTR_ASPASSWORD 2  /* print '*' for each character typed */
 
 // ConClear() clears the screen. 
 // returns -1 if console is not a tty;
@@ -54,16 +57,16 @@ int ConClear(void);
 // returns -1 if console is not a tty;
 int ConGetPos( int *row, int *col );
 
-//set the cursor position (zero-based)
+// Set the cursor position (zero-based)
 // returns -1 if console is not a tty
 int ConSetPos( int row, int col );  
 
-// Deinitialize and Initialize console functionality.
-// returns -1 if console could not be initialized
-int DeinitializeConsole(void);
-int InitializeConsole(int runhidden);
+// Deinitialize console functionality. 
+// Waits for a user keypress (on some platforms) if !autoclose.
+int DeinitializeConsole(int autoclose);
 
-#define IS_STDOUT_A_TTY() (ConIsScreen())
-#define IS_STDIN_A_TTY() (ConIsScreen())
+// Initialize console functionality. 
+// Returns !0 on failure.
+int InitializeConsole(int runhidden);
 
 #endif
