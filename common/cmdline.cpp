d6663153dcfecd1cb08bd84a9a435655eb0bc0a1
Remi Guyomarch <remi@distributed.net>
1998-11-11 03:09:41 +0000

    Synced with :
     Revision 1.96  1998/11/10 21:45:59  cyp
     ParseCommandLine() is now one-pass. (a second pass is available so that
     lusers can see the override messages on the screen)
    
     Revision 1.93  1998/11/08 19:03:22  cyp
     -help (and invalid command line options) are now treated as "mode" requests.

--- common/cmdline.cpp
+++ common/cmdline.cpp
@@ -3,6 +3,16 @@
 // Any other distribution or use of this source violates copyright.
 
 // $Log: cmdline.cpp,v $
+// Revision 1.92.2.3  1998/11/11 03:09:41  remi
+// Synced with :
+//
+//  Revision 1.96  1998/11/10 21:45:59  cyp
+//  ParseCommandLine() is now one-pass. (a second pass is available so that
+//  lusers can see the override messages on the screen)
+//
+//  Revision 1.93  1998/11/08 19:03:22  cyp
+//  -help (and invalid command line options) are now treated as "mode" requests.
+//
 // Revision 1.92.2.2  1998/11/08 11:50:51  remi
 // Lots of $Log tags.
 //
@@ -13,127 +23,94 @@
 #include "logstuff.h"  // Log()/LogScreen()/LogScreenPercent()/LogFlush()
 #include "modereq.h"   // get/set/clear mode request bits
 
-/* ------------------------------------------------------------------------
- * runlevel == 0 = pre-anything    (-quiet, -ini, -guistart etc done here)
- *          >= 1 = post-readconfig (override ini options)
- *          == 2 = run "modes"
- *
- * Sequence of events:
- *
- *   ParseCommandLine( 0, argc, argv, NULL, NULL, 0 );
- *   if ( InitializeLogging() == 0 ) //let -quiet take affect
- *     {
- *     inimissing = ReadConfig();
- *     if (ParseCommandLine( X, argc, argv, &inimissing, &retcode, x )==0)
- *       {                   |                                     
- *       if (inimissing)     `-- X==2 for OS_xxx that do "modes", 1 for others
- *         { 
- *         Configure() ...  
- *         }
- *       else 
- *         {
- *         if ( RunStartup() == 0 )
- *           {
- *           ValidateConfig()
- *           Run();
- *           RunShutdown();
- *           }
- *         }
- *       }
- *     DeinitializeLogging();
- *     }
- *   
- *------------------------------------------------------------------------ */
+/* -------------------------------------- */
 
-int Client::ParseCommandline( int runlevel, int argc, const char *argv[], 
+int Client::ParseCommandline( int run_level, int argc, const char *argv[], 
                               int *retcodeP, int logging_is_initialized )
 {
-  int pos, skip_next = 0, do_break = 0, retcode = 0;
+  int inimissing, pos, skip_next, terminate_app, havemode;
   const char *thisarg, *nextarg;
 
-  //---------------------------------------
-  //first handle the options that affect option handling
-  //--------------------------------------
+  terminate_app = 0;
+  inimissing = 0;
+  havemode = 0;
 
-  if (runlevel >= 0) // this is only to protect against an invalid runlevel
+  //-----------------------------------
+  // In the next loop we parse the other options
+  //-----------------------------------
+
+  if (!terminate_app && ((run_level == 0) || (logging_is_initialized)))
     {
-    if (runlevel == 0) 
-      {    
-      ModeReqClear( -1 ); // clear all mode request bits
-      }
-    } //if (runlevel >= 0)
-
-  //---------------------------------------
-  // handle the other options 
-  //--------------------------------------
-
-  static const char *ignoreX[]={ /* options handled in other loops */
-  "-cpuinfo","-test", "-help", "--help", "-h", "-?",
-  "-benchmark2rc5","-benchmark2des","-benchmark2","-benchmarkrc5","-benchmarkdes",
-  "-benchmark","" };
-
-  if (runlevel >= 1 && !do_break)
-    {
-    for (pos = 1;((!do_break) && (pos<argc)); pos+=(1+skip_next))
+    for (pos = 1;pos<argc; pos+=(1+skip_next))
       {
       thisarg = argv[pos];
+      if (thisarg && *thisarg=='-' && thisarg[1]=='-')
+        thisarg++;
       nextarg = ((pos < (argc-1))?(argv[pos+1]):(NULL));
-  
-      skip_next = -1;
-      for (unsigned int i=0;i<(sizeof(ignoreX)/sizeof(ignoreX[1]));i++)
-        {
-        if (*thisarg == '-' && strcmp( thisarg+1, ignoreX[i]+1 )==0 )
-          {
-          skip_next++;
-          break;
-          }
-        }
-      if (skip_next != -1) //not found in ignoreX
-        continue;
       skip_next = 0;
-      
-      if ( strcmp( thisarg, "-c" ) == 0)      // set cpu type
+
+      if ( thisarg == NULL )
+        { //nothing
+        }
+      else if (*thisarg == 0)
+        { //nothing
+        }
+      else if (
+          ( strcmp( thisarg, "-cpuinfo"     ) == 0 ) ||
+          ( strcmp( thisarg, "-test"        ) == 0 ) ||
+          ( strncmp( thisarg, "-benchmark", 10 ) == 0))
         {
-        if (nextarg)
-          {
-          skip_next = 1;
-          cputype = (s32) atoi( nextarg );
-          }
+        ; //nothing - handled in next loop
+        havemode = 1;
+        }
+      else if (run_level==0)
+        {
+        ModeReqClear(-1); /* clear all */
+        ModeReqSet( MODEREQ_CMDLINE_HELP );
+        ModeReqSetArg(MODEREQ_CMDLINE_HELP,(void *)thisarg);
+        inimissing = 0; // don't need an .ini file if we just want help
+        havemode = 0;
+        break;
         }
       }
-    if (!do_break)
-      ValidateConfig();
+    ValidateConfig();
     }
-      
-  //---------------------------------------
-  // handle the run modes
-  //--------------------------------------
-  
-  if (runlevel >= 2 && !do_break)
+        
+  //-----------------------------------
+  // In the final loop we parse the "modes".
+  //-----------------------------------
+
+  if (!terminate_app && havemode && run_level == 0)
     {
-    for (pos = 1;((!do_break) && (pos<argc)); pos+=(1+skip_next))
+    for (pos = 1;pos<argc;pos+=(1+skip_next))
       {
       thisarg = argv[pos];
+      if (thisarg && *thisarg=='-' && thisarg[1]=='-')
+        thisarg++;
       nextarg = ((pos < (argc-1))?(argv[pos+1]):(NULL));
       skip_next = 0;
-
-      if ( strcmp( thisarg, "-cpuinfo" ) == 0 )
+  
+      if ( thisarg == NULL )
+        ; //nothing
+      else if (*thisarg == 0)
+        ; //nothing
+      else if ( strcmp( thisarg, "-cpuinfo" ) == 0 )
         {
-        do_break = 1;
+        inimissing = 0; // Don't complain if the inifile is missing
         ModeReqClear(-1); //clear all - only do -cpuinfo
         ModeReqSet( MODEREQ_CPUINFO );
-        retcode = 0; //and break out of loop
+        break;
         }
       else if ( strcmp( thisarg, "-test" ) == 0 )
         {
-        do_break = 1;
+        inimissing = 0; // Don't complain if the inifile is missing
         ModeReqClear(-1); //clear all - only do -test
         ModeReqSet( MODEREQ_TEST );
-        SelectCore( 0 /* not quietly */ );
+        SelectCore( 1 /* quietly */ );
+        break;
         }
       else if (strncmp( thisarg, "-benchmark", 10 ) == 0)
         {
-        do_break = 1;
         int do_mode = 0;
         thisarg += 10;
 
@@ -149,20 +126,16 @@ int Client::ParseCommandline( int runlevel, int argc, const char *argv[],
         else 
           do_mode |= (MODEREQ_BENCHMARK_DES | MODEREQ_BENCHMARK_RC5);
 
+        inimissing = 0; // Don't complain if the inifile is missing
         ModeReqClear(-1); //clear all - only do benchmark
         ModeReqSet( do_mode );
-        SelectCore( 0 /* not quietly */ );
+        SelectCore( 1 /* quietly */ );
+        break;
         }
-      else
-	{
-	ModeReqSet( MODEREQ_HELP ); // display help text if no other "modes" requested
-	retcode = 1;
-	do_break = 1;
-	}
       }
     }
 
-  if (retcodeP && do_break) 
-    *retcodeP = retcode;
-  return do_break;
+  if (retcodeP) 
+    *retcodeP = 0;
+  return terminate_app;
 }
