0df6771b750316c895a0cef4b1405908d47d194c
Remi Guyomarch <remi@distributed.net>
1998-11-15 15:37:59 +0000

    Synced with :  Revision 1.25  1998/11/13 15:52:13  silby  Bumping to 420.

--- common/version.h
+++ common/version.h
@@ -5,6 +5,11 @@
 // Any other distribution or use of this source violates copyright.
 
 // $Log: version.h,v $
+// Revision 1.22.2.3  1998/11/15 15:37:59  remi
+// Synced with :
+//  Revision 1.25  1998/11/13 15:52:13  silby
+//  Bumping to 420.
+//
 // Revision 1.22.2.2  1998/11/08 11:52:11  remi
 // Lots of $Log tags.
 //
@@ -14,10 +19,10 @@
 
 #define CLIENT_CONTEST      71
 #define CLIENT_BUILD        01
-#define CLIENT_BUILD_FRAC   418
+#define CLIENT_BUILD_FRAC   420
 
-#define CLIENT_VERSIONSTRING    "v2.7101.418-TEST"
-#define CLIENT_VERSIONSTRING2   "2.7101.418-TEST"        // no leading "v"
+#define CLIENT_VERSIONSTRING    "v2.7102.420-TEST"
+#define CLIENT_VERSIONSTRING2   "2.7102.420-TEST"        // no leading "v"
 
 #endif // _VERSION_H
 
