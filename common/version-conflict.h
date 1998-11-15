95df2a1c5062fc93c64594aba95f5dc9d1316dff
Remi Guyomarch <remi@distributed.net>
1998-11-15 15:39:07 +0000

    Fixed CLIENT_BUILD

--- common/version-conflict.h
+++ common/version-conflict.h
@@ -5,6 +5,9 @@
 // Any other distribution or use of this source violates copyright.
 
 // $Log: version-conflict.h,v $
+// Revision 1.22.2.4  1998/11/15 15:39:07  remi
+// Fixed CLIENT_BUILD
+//
 // Revision 1.22.2.3  1998/11/15 15:37:59  remi
 // Synced with :
 //  Revision 1.25  1998/11/13 15:52:13  silby
@@ -18,7 +21,7 @@
 #define _VERSION_H
 
 #define CLIENT_CONTEST      71
-#define CLIENT_BUILD        01
+#define CLIENT_BUILD        02
 #define CLIENT_BUILD_FRAC   420
 
 #define CLIENT_VERSIONSTRING    "v2.7102.420-TEST"
