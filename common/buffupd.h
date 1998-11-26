90b678b45cf42f33a91dab04b45422c178020f15
Cyrus Patel <cyp@distributed.net>
1998-11-26 07:19:09 +0000

    Created. blah.

---
+++ common/buffupd.h
@@ -0,0 +1,18 @@
+// Hey, Emacs, this a -*-C++-*- file !
+
+// Copyright distributed.net 1997-1998 - All Rights Reserved
+// For use in distributed.net projects only.
+// Any other distribution or use of this source violates copyright.
+//
+// $Log: buffupd.h,v $
+// Revision 1.1  1998/11/26 07:19:09  cyp
+// Created. blah.
+//
+//
+#ifndef __BUFFUPD_H__
+#define __BUFFUPD_H__
+
+#define BUFFERUPDATE_FETCH 0x01
+#define BUFFERUPDATE_FLUSH 0x02
+
+#endif /* __BUFFUPD_H__ */
