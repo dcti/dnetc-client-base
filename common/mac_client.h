352ed0463061f747f4032e7206736ddb03d332c9
Peter DiCamillo <dicamillo@distributed.net>
1998-12-09 07:19:20 +0000

    Created

--- common/mac_client.h
+++ common/mac_client.h
@@ -1,3 +1,12 @@
+// Copyright distributed.net 1997-1998 - All Rights Reserved
+// For use in distributed.net projects only.
+// Any other distribution or use of this source violates copyright.
+//
+// $Log: mac_client.h,v $
+// Revision 1.2  1998/12/09 07:19:20  dicamillo
+// Created
+//
+
 #if (CLIENT_OS == OS_MACOS)
 
 class Mac_Client : public Client
