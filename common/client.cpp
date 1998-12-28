8e5d427952f4fcac302d29ff2457635acdc38042
Remi Guyomarch <remi@distributed.net>
1998-12-28 14:13:57 +0000

    Synced with :
     Revision 1.177  1998/12/22 15:58:24  jcmichot
     QNX changes
    
     Revision 1.176  1998/12/12 12:28:49  cyp
     win16/win32 change: win16/win32 console code needs access to the client
     object so that it can force a shutdown when it gets an endsession message.
    
     Revision 1.175  1998/12/08 05:35:47  dicamillo
     MacOS updates: allow PrintBanner to be called by Mac client;
     added help text; Mac client provides its own "main".
    
     Revision 1.173  1998/11/28 19:44:34  cyp
     InitializeLogging() and DeinitializeLogging() are no longer Client class
     methods.
    
     Revision 1.172  1998/11/26 06:54:25  cyp
     Updated client contructor.
    
     Revision 1.171  1998/11/25 09:23:27  chrisb
     various changes to support x86 coprocessor under RISC OS
    
     Revision 1.168  1998/11/19 23:36:26  cyp
     PrintBanner() now gets its level and restart flag from ::Main()

--- common/client.cpp
+++ common/client.cpp
@@ -3,6 +3,33 @@
 // Any other distribution or use of this source violates copyright.
 
 // $Log: client.cpp,v $
+// Revision 1.152.2.8  1998/12/28 14:13:57  remi
+// Synced with :
+//
+//  Revision 1.177  1998/12/22 15:58:24  jcmichot
+//  QNX changes
+//
+//  Revision 1.176  1998/12/12 12:28:49  cyp
+//  win16/win32 change: win16/win32 console code needs access to the client
+//  object so that it can force a shutdown when it gets an endsession message.
+//
+//  Revision 1.175  1998/12/08 05:35:47  dicamillo
+//  MacOS updates: allow PrintBanner to be called by Mac client;
+//  added help text; Mac client provides its own "main".
+//
+//  Revision 1.173  1998/11/28 19:44:34  cyp
+//  InitializeLogging() and DeinitializeLogging() are no longer Client class
+//  methods.
+//
+//  Revision 1.172  1998/11/26 06:54:25  cyp
+//  Updated client contructor.
+//
+//  Revision 1.171  1998/11/25 09:23:27  chrisb
+//  various changes to support x86 coprocessor under RISC OS
+//
+//  Revision 1.168  1998/11/19 23:36:26  cyp
+//  PrintBanner() now gets its level and restart flag from ::Main()
+//
 // Revision 1.152.2.7  1998/11/18 15:57:15  remi
 // Another clean-up.
 //
@@ -71,77 +98,116 @@ Client::Client()
 {
   totalBlocksDone[0] = totalBlocksDone[1] = 0;
   cputype=-1;
+#if defined(__QNX__)
+  srand( (unsigned) time(NULL)/*(unsigned) CliTimer( NULL )->tv_usec*/ );
+#else
   srand( (unsigned) CliTimer( NULL )->tv_usec );
+#endif
 }
 
 // --------------------------------------------------------------------------
 
-static void PrintBanner(void)
+static const char *GetBuildOrEnvDescription(void)
 {
-  static unsigned int level = 0; //incrementing so messages are not repeated
-            //0 = show copyright/version,  1 = show startup message
- 
-  if (level == 0)
-    {
-    level++; //will never print this message again
+  #if (CLIENT_OS == OS_DOS)
+  return dosCliGetEmulationDescription(); //if in win/os2 VM 
+  #elif ((CLIENT_OS==OS_WIN32) || (CLIENT_OS==OS_WIN16) || (CLIENT_OS==OS_WIN32S))
+  static char buffer[32]; long ver = winGetVersion(); /* w32pre.cpp */
+  sprintf(buffer,"under Windows%s %u.%u", (ver>=2000)?(" NT"):(""), (ver/100)%20, ver%100 );
+  return buffer;
+  #else
+  return "";
+  #endif
+}  
 
-    LogScreenRaw( "\nRC5DES " CLIENT_VERSIONSTRING 
-               " client - a project of distributed.net\n"
-               "Copyright 1997-1998 distributed.net\n" );
-    
-    #if (CLIENT_CPU == CPU_68K)
-    LogScreenRaw( "RC5 68K assembly by John Girvin\n");
-    #endif
-    #if (CLIENT_CPU == CPU_POWERPC)
-    LogScreenRaw( "PowerPC assembly by Dan Oetting at USGS\n");
-    #endif
-    #if defined(KWAN) && defined(MEGGS)
-    LogScreenRaw( "DES bitslice driver Copyright 1997-1998, Andrew Meggs\n" 
-                  "DES sboxes routines Copyright 1997-1998, Matthew Kwan\n" );
-    #elif defined(KWAN) 
-    LogScreenRaw( "DES search routines Copyright 1997-1998, Matthew Kwan\n" );
-    #endif
-    #if (CLIENT_CPU == CPU_X86)
-    LogScreenRaw( "DES search routines Copyright 1997-1998, Svend Olaf Mikkelsen\n");
-    #endif
-    #if (CLIENT_OS == OS_DOS)  
-    LogScreenRaw( "PMODE DOS extender Copyright 1994-1998, Charles Scheffold and Thomas Pytel\n");
-    #endif
-    LogScreenRaw( "Please visit http://www.distributed.net/ for up-to-date contest information.\n" );
-    #if (CLIENT_OS == OS_DOS)
-      dosCliCheckPlatform(); //show warning if pure DOS client is in win/os2 VM
-    #endif
+// --------------------------------------------------------------------------
 
-    #if ((CLIENT_OS==OS_DOS) || (CLIENT_OS==OS_WIN16) || \
-         (CLIENT_OS==OS_WIN32S) || (CLIENT_OS==OS_OS2) || \
-         ((CLIENT_OS==OS_WIN32) && !defined(NEEDVIRTUALMETHODS)))
-    #if (CLIENT_OS == OS_WIN32) || (CLIENT_OS==OS_WIN16) || (CLIENT_OS==OS_WIN32S)
-    int major=0;
-    w32ConGetWinVersion(&major,NULL);
-    if ((major%20) <= 3) /* >=20 == NT */
-    #endif   
-    if (getenv("TZ") == NULL)
-      {
-      LogScreenRaw("Warning: The TZ= variable is not set in the environment. "
-       "The client will\nprobably display the wrong time and/or select the "
-       "wrong keyserver.\n");
-      putenv("TZ=GMT+0"); //use local time.
-      }
-    #endif
-    }
+void PrintBanner(const char */*dnet_id*/,int level,int restarted)
+{
+  //level = 0 = show copyright/version,  1 = show startup message
   
-  if ( level == 1 )
-    {  
-    level++; //will never print this message again
-    LogRaw("\nRC5DES Client %s for %s started.\n\n", CLIENT_VERSIONSTRING,
-                                                   CLIENT_OS_NAME );
+  if (!restarted)
+    {
+    if (level == 0)
+      {
+      LogScreenRaw( "\nRC5DES " CLIENT_VERSIONSTRING 
+                 " client - a project of distributed.net\n"
+                 "Copyright 1997-1998 distributed.net\n" );
+      
+      #if (CLIENT_CPU == CPU_68K)
+      LogScreenRaw( "RC5 68K assembly by John Girvin\n");
+      #endif
+      #if (CLIENT_CPU == CPU_POWERPC)
+      LogScreenRaw( "PowerPC assembly by Dan Oetting at USGS\n");
+      #endif
+
+      #if (CLIENT_CPU == CPU_ARM)
+      LogScreenRaw( "ARM assembly by Steve Lee\n");
+      #if (CLIENT_OS == OS_RISCOS)
+      LogScreenRaw( "PC Card support by Dominic Plunkett\n");
+      #endif
+      #endif
+
+
+      #if defined(KWAN) && defined(MEGGS)
+      LogScreenRaw( "DES bitslice driver Copyright 1997-1998, Andrew Meggs\n" 
+                    "DES sboxes routines Copyright 1997-1998, Matthew Kwan\n" );
+      #elif defined(KWAN) 
+      LogScreenRaw( "DES search routines Copyright 1997-1998, Matthew Kwan\n" );
+      #endif
+      #if (CLIENT_CPU == CPU_X86)
+      LogScreenRaw( "DES search routines Copyright 1997-1998, Svend Olaf Mikkelsen\n");
+      #endif
+      #if (CLIENT_OS == OS_DOS)  
+      LogScreenRaw( "PMODE DOS extender Copyright 1994-1998, Charles Scheffold and Thomas Pytel\n");
+      #endif
+      LogScreenRaw( "Please visit http://www.distributed.net/ for up-to-date contest information.\n"
+                 "%s\n",
+              #if (CLIENT_OS == OS_RISCOS)
+              guiriscos ?
+              "Interactive help is available, or select 'Help contents' from the menu for\n"
+              "detailed client information.\n" :
+              #endif
+			  #if (CLIENT_OS == OS_MACOS)
+                "Select ""Help..."" from the Apple menu for detailed client information.\n"
+			  #else
+              "Start the client with '-help' for a list of valid command line options.\n"
+			  #endif
+              );
+      }
+    else if ( level == 1 )
+      {  
+      #if ((CLIENT_OS==OS_DOS) || (CLIENT_OS==OS_WIN16) || \
+           (CLIENT_OS==OS_WIN32S) || (CLIENT_OS==OS_OS2) || \
+           ((CLIENT_OS==OS_WIN32) && !defined(NEEDVIRTUALMETHODS)))
+      #if (CLIENT_OS == OS_WIN32) || (CLIENT_OS==OS_WIN16) || (CLIENT_OS==OS_WIN32S)
+      if ((winGetVersion()%2000) < 400) /* w32pre.cpp. +2000==NT */
+      #endif   
+      if (getenv("TZ") == NULL)
+        {
+        LogScreenRaw("Warning: The TZ= variable is not set in the environment. "
+         "The client will\nprobably display the wrong time and/or select the "
+         "wrong keyserver.\n");
+        putenv("TZ=GMT+0"); //use local time.
+        }
+      #endif
+
+      const char *msg = GetBuildOrEnvDescription();
+      if (msg == NULL) msg="";
+
+      LogRaw("\nRC5DES Client %s for %s%s%s%s started.\n",CLIENT_VERSIONSTRING,
+             CLIENT_OS_NAME, ((*msg)?(" ("):("")), msg, ((*msg)?(")"):("")) );
+  
+      LogRaw( "\n" );
+      
+      }
     }
   return;
 }
 
 //------------------------------------------------------------------------
 
-int Client::Main( int argc, const char *argv[], int /* restarted */ )
+int Client::Main( int argc, const char *argv[], int restarted )
 {
   int retcode = 0;
   int domodes = 0;
@@ -157,7 +223,10 @@ int Client::Main( int argc, const char *argv[], int /* restarted */ )
       if (InitializeConsole(0,domodes) == 0)
         {
         InitializeLogging(0); //enable only screen logging for now
-        PrintBanner(); //tracks restart state itself
+        InitializeLogging( (quietmode!=0), (percentprintingoff!=0), 
+			   logname, LOGFILETYPE_NOLIMIT, 0, messagelen, 
+			   smtpsrvr, smtpport, smtpfrom, smtpdest, id );
+        PrintBanner(NULL, 0, restarted); //tracks restart state itself
         ParseCommandline( 1, argc, argv, NULL, 1 ); //show cmdline overrides
       
         if (domodes)
@@ -215,6 +284,13 @@ int realmain( int argc, char *argv[] )
 
   //----------------------------
 
+  #if (CLIENT_OS==OS_WIN16 || CLIENT_OS==OS_WIN32S || CLIENT_OS==OS_WIN32)
+  if ( init_success )
+    w32ConSetClientPointer( clientP ); // save the client * so we can bail out 
+  #endif                               // when we get a WM_ENDSESSION message
+
+  //----------------------------
+
   if ( init_success )
     {
     do {
@@ -236,8 +312,14 @@ int realmain( int argc, char *argv[] )
     nwCliExitClient(); // destroys AES process, screen, polling procedure
   #endif
 
+  //------------------------------  
+
+  #if (CLIENT_OS==OS_WIN16 || CLIENT_OS==OS_WIN32S || CLIENT_OS==OS_WIN32)
+  w32ConSetClientPointer( NULL ); // clear the client * 
+  #endif
+
   //------------------------------
-  
+
   if (clientP)
     delete clientP;
 
