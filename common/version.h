aedd424b09394effb59fb4182c46cdff0fd638ad
Remi Guyomarch <remi@distributed.net>
1998-12-29 20:05:04 +0000

    Synced with :  Revision 1.29  1998/12/28 23:12:43  cyp  .424 and counting...

--- common/version.h
+++ common/version.h
@@ -5,6 +5,11 @@
 // Any other distribution or use of this source violates copyright.
 
 // $Log: version.h,v $
+// Revision 1.22.2.6  1998/12/29 20:05:04  remi
+// Synced with :
+//  Revision 1.29  1998/12/28 23:12:43  cyp
+//  .424 and counting...
+//
 // Revision 1.22.2.5  1998/12/28 15:55:08  remi
 // Synced with :
 //  Revision 1.28  1998/12/25 09:25:22  cyp
@@ -36,10 +41,10 @@
 
 #define CLIENT_CONTEST      71
 #define CLIENT_BUILD        02
-#define CLIENT_BUILD_FRAC   423
+#define CLIENT_BUILD_FRAC   424
 
-#define CLIENT_VERSIONSTRING    "v2.7102.423-TEST"
-#define CLIENT_VERSIONSTRING2   "2.7102.423-TEST"        // no leading "v"
+#define CLIENT_VERSIONSTRING    "v2.7102.424-TEST"
+#define CLIENT_VERSIONSTRING2   "2.7102.424-TEST"        // no leading "v"
 
 #endif // _VERSION_H
 
