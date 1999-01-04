3709cfdac98e5694139512bcba51326ff3afca55
Remi Guyomarch <remi@distributed.net>
1999-01-04 02:06:39 +0000

    Synced with :
     Revision 1.111  1999/01/01 02:45:14  cramer
     Part 1 of 1999 Copyright updates...

--- common/client.h
+++ common/client.h
@@ -1,10 +1,16 @@
 // Hey, Emacs, this a -*-C++-*- file !
 
-// Copyright distributed.net 1997-1998 - All Rights Reserved
+// Copyright distributed.net 1997-1999 - All Rights Reserved
 // For use in distributed.net projects only.
 // Any other distribution or use of this source violates copyright.
 
 // $Log: client.h,v $
+// Revision 1.94.2.4  1999/01/04 02:06:39  remi
+// Synced with :
+//
+//  Revision 1.111  1999/01/01 02:45:14  cramer
+//  Part 1 of 1999 Copyright updates...
+//
 // Revision 1.94.2.3  1998/12/28 16:42:41  remi
 // Synced with :
 //   Revision 1.100  1998/11/28 19:44:34  cyp
