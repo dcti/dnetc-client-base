cc3d9eb770d162f7af8526115781217b021b43bb
Remi Guyomarch <remi@distributed.net>
1999-01-04 02:12:00 +0000

    Synced with :
     Revision 1.112  1999/01/01 02:45:15  cramer
     Part 1 of 1999 Copyright updates...

--- common/cmdline.cpp
+++ common/cmdline.cpp
@@ -1,8 +1,14 @@
-// Copyright distributed.net 1997-1998 - All Rights Reserved
+// Copyright distributed.net 1997-1999 - All Rights Reserved
 // For use in distributed.net projects only.
 // Any other distribution or use of this source violates copyright.
 
 // $Log: cmdline.cpp,v $
+// Revision 1.92.2.7  1999/01/04 02:12:00  remi
+// Synced with :
+//
+//  Revision 1.112  1999/01/01 02:45:15  cramer
+//  Part 1 of 1999 Copyright updates...
+//
 // Revision 1.92.2.6  1998/12/28 14:32:39  remi
 // Merge with 1.111 :
 //  Revision 1.101  1998/11/21 13:08:09  remi
