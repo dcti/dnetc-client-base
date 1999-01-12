28c2dae13f4be8270312e7c75e668112a90b7539
Mike Silbersack <silby@distributed.net>
1999-01-12 09:13:40 +0000

    Moved to 2.7104.428 in honor of new des mmx core.

--- common/version.h
+++ common/version.h
@@ -5,6 +5,9 @@
 // Any other distribution or use of this source violates copyright.
 // 
 // $Log: version.h,v $
+// Revision 1.35  1999/01/12 09:13:40  silby
+// Moved to 2.7104.428 in honor of new des mmx core.
+//
 // Revision 1.34  1999/01/09 01:40:52  silby
 // Welcome to .427
 //
@@ -123,11 +126,11 @@
 // as well.
 
 #define CLIENT_CONTEST      71
-#define CLIENT_BUILD        03
-#define CLIENT_BUILD_FRAC   427
+#define CLIENT_BUILD        04
+#define CLIENT_BUILD_FRAC   428
 
-#define CLIENT_VERSIONSTRING    "v2.7103.427"
-#define CLIENT_VERSIONSTRING2   "2.7103.427"        // no leading "v"
+#define CLIENT_VERSIONSTRING    "v2.7104.428"
+#define CLIENT_VERSIONSTRING2   "2.7104.428"        // no leading "v"
 
 // When releasing a beta client, please set the expiration time to
 // about two weeks into the future; that should be an adequate beta
