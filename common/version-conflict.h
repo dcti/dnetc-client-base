3343e26f1ab059c28bad1a0bb2f981ac602c4df2
Cyrus Patel <cyp@distributed.net>
1999-02-03 17:56:16 +0000

    2.7106.435 for new alpha core; discarded/merged VERSIONSTRING (was with "v", now without) and VERSIONSTRING2 (was without "v", now gone); added doc.

--- common/version-conflict.h
+++ common/version-conflict.h
@@ -3,8 +3,27 @@
 // Copyright distributed.net 1997-1999 - All Rights Reserved
 // For use in distributed.net projects only.
 // Any other distribution or use of this source violates copyright.
-// 
+/*
+   Version bump guide: 
+   "2.7106.435"
+    | | |   |
+    | | |   `--- Significant changes, eg serious bugs squashed, in 
+    | | |        ./common/ code, or simply a significant number (your call)
+    | | |        of bug fixes, gets a "build fraction" change.
+    | | `------- New cores, for any platform, *requires* a "build version"
+    | |          change, ie 2.7103.x to 2.7104.x. This is needed to be able
+    | |          to isolate bad blocks in the master keyspace bitmap.
+    | `--------- A "client contest" change follows a code freeze at which
+    |            point the client is assumed to be stable. Code that
+    |            would make clients incompatible with previous clients or
+    |            proxies must be a accompanied by 'client contest' change.
+    `----------- Denotes a client rewrite.
+*/
 // $Log: version-conflict.h,v $
+// Revision 1.41  1999/02/03 17:56:16  cyp
+// 2.7106.435 for new alpha core; discarded/merged VERSIONSTRING (was with "v",
+// now without) and VERSIONSTRING2 (was without "v", now gone); added doc.
+//
 // Revision 1.40  1999/01/26 17:29:04  michmarc
 // .434
 //
@@ -134,18 +153,12 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-// Note regarding beta vs release versions. Since we can only see
-// CLIENT_CONTEST and CLIENT_BUILD in proxy logs for now, let's make
-// sure to keep all betas an ODD CLIENT_BUILD and all releases
-// an EVEN CLIENT_BUILD. (Naturally, incrementing the CLIENT_BUILD_FRAC
-// as well.
-
+#define CLIENT_MAJOR_VER     2
 #define CLIENT_CONTEST      71
-#define CLIENT_BUILD        05
-#define CLIENT_BUILD_FRAC   434
+#define CLIENT_BUILD        06
+#define CLIENT_BUILD_FRAC   435
 
-#define CLIENT_VERSIONSTRING    "v2.7105.434"
-#define CLIENT_VERSIONSTRING2   "2.7105.434"        // no leading "v"
+#define CLIENT_VERSIONSTRING    "2.7106.435"
 
 // When releasing a beta client, please set the expiration time to
 // about two weeks into the future; that should be an adequate beta
