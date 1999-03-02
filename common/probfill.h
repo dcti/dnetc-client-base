7be2a92a548def9498e9885ace2ca52bea061c25
Mike Silbersack <silby@distributed.net>
1999-03-02 03:48:14 +0000

    Shuffled bits around to allow for larger build.

--- common/probfill.h
+++ common/probfill.h
@@ -5,6 +5,9 @@
 // Any other distribution or use of this source violates copyright.
 //
 // $Log: probfill.h,v $
+// Revision 1.6  1999/03/02 03:48:14  silby
+// Shuffled bits around to allow for larger build.
+//
 // Revision 1.5  1999/01/01 02:45:16  cramer
 // Part 1 of 1999 Copyright updates...
 //
@@ -32,8 +35,8 @@
 
 // --------------------------------------------------------------------------
 #if (( CLIENT_CPU         > 0x01F  /* 0-31 */  ) || \
-     ((CLIENT_CONTEST-64) > 0x0F   /* 64-79 */ ) || \
-     ( CLIENT_BUILD       > 0x07   /* 0-7 */   ) || \
+     ((CLIENT_CONTEST-64) > 0x07   /* 64-71 */ ) || \
+     ( CLIENT_BUILD       > 0x0F   /* 0-15 */   ) || \
      ( CLIENT_BUILD_FRAC  > 0x03FF /* 0-1023 */) || \
      ( CLIENT_OS          > 0x3F   /* 0-63 */  ))      /* + cputype 0-15 */
 #error CLIENT_CPU/_OS/_CONTEST/_BUILD are out of range for FileEntry check tags
@@ -47,8 +50,8 @@
 
 #define FILEENTRY_OS      ((CLIENT_OS & 0x3F) | ((CLIENT_CPU & 0x10) << 3) | \
                            (((CLIENT_BUILD_FRAC>>8)&2)<<5))
-#define FILEENTRY_BUILDHI ((((CLIENT_CONTEST-64)&0x0F)<<4) | \
-                            ((CLIENT_BUILD & 0x07)<<1) | \
+#define FILEENTRY_BUILDHI ((((CLIENT_CONTEST-64)&0x07)<<5) | \
+                            ((CLIENT_BUILD & 0x0F)<<1) | \
                             ((CLIENT_BUILD_FRAC>>8)&1)) 
 #define FILEENTRY_BUILDLO ((CLIENT_BUILD_FRAC) & 0xff)  
 
