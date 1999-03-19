8680428d813499113802935b55bfe9ca3f62cb55
Cyrus Patel <cyp@distributed.net>
1999-03-19 01:29:25 +0000

    er, 2**iter is 1<<iter.

--- common/bench.cpp
+++ common/bench.cpp
@@ -3,6 +3,9 @@
 // Any other distribution or use of this source violates copyright.
 //
 // $Log: bench.cpp,v $
+// Revision 1.22  1999/03/19 01:29:25  cyp
+// er, 2**iter is 1<<iter.
+//
 // Revision 1.21  1999/03/18 02:59:35  cyp
 // fixed that silly ((int)pow(2,int)). Whats next? "#def 1 (cos(pi)-sin(pi))"?
 //
@@ -81,7 +84,7 @@
 
 #if (!defined(lint) && defined(__showids__))
 const char *bench_cpp(void) {
-return "@(#)$Id: bench.cpp,v 1.21 1999/03/18 02:59:35 cyp Exp $"; }
+return "@(#)$Id: bench.cpp,v 1.22 1999/03/19 01:29:25 cyp Exp $"; }
 #endif
 
 #include "cputypes.h"  // CLIENT_OS, CLIENT_CPU
@@ -352,7 +355,7 @@ u32 Benchmark( unsigned int contestid, u32 numkeys, int cputype, int *numblocks)
              "enough for approximately %u %s.\n", 
              CliGetContestNameFromID(contestid), 
              (unsigned int)itersize, 
-             (unsigned int)((itersize-28)*(itersize-28)),
+             (1<<(itersize-28)),
              CliGetTimeString( &tv, 2 ),recommendedblockcount,
              ((hourstobuffer > 24)?(hourstobuffer/24):(hourstobuffer)),
              ((hourstobuffer > 24)?("days"):("hours")) );
