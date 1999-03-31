1c21767d230e08a8089b324f01cff58b5c5ea3ed
Cyrus Patel <cyp@distributed.net>
1999-03-31 21:50:32 +0000

    SelfTest() no longer needs the threadindex parameter. It will loop to handle the RISC OS x86 magic thread.

--- common/selftest.h
+++ common/selftest.h
@@ -5,6 +5,10 @@
 // Any other distribution or use of this source violates copyright.
 //
 // $Log: selftest.h,v $
+// Revision 1.4  1999/03/31 21:50:32  cyp
+// SelfTest() no longer needs the threadindex parameter. It will loop to
+// handle the RISC OS x86 magic thread.
+//
 // Revision 1.3  1999/01/08 10:05:42  chrisb
 // Added 'threadindex' parameter (defaults to -1L, as with Problem::Problem) to SelfTest(). Allows RISC OS to self test the x86 core.
 //
@@ -23,6 +27,6 @@
 
 /* returns number of tests if all passed or 
    negative number of the test that failed */
-int SelfTest( unsigned int contest, int cputype , int threadindex = -1L);
+int SelfTest( unsigned int contest, int cputype );
 
 #endif
