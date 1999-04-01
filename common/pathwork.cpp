04fed8d46192e2a26116f435d897ae7b5db5bc44
Cyrus Patel <cyp@distributed.net>
1999-04-01 03:04:37 +0000

    Created GetFullPathForFilenameAndDir(). Like GetFullPathForFilename() but can use a directory other than the client's wd.

--- common/pathwork.cpp
+++ common/pathwork.cpp
@@ -1,64 +1,68 @@
-// Copyright distributed.net 1997 - All Rights Reserved
-// For use in distributed.net projects only.
-// Any other distribution or use of this source violates copyright.
-//
-// This module contains functions for setting the "working directory"
-// and pathifying a filename that has no dirspec. Functions need to be
-// initialized from main() with InitWorkingDirectoryFromSamplePaths();
-// [ ...( inipath, apppath) where inipath should not have been previously
-// merged from argv[0] + default, although it doesn't hurt if it has.]
-//
-// The "working directory" is assumed to be the app's directory, unless the
-// ini filename contains a dirspec, in which case the path to the ini file
-// is used. The exception here is win32/win16 which, for reasons of backward
-// compatability, always use the app's directory.
-//
-// GetFullPathForFilename() is ideally intended for use in (or just prior to)
-// a call to fopen(). This obviates the necessity of having to pre-parse
-// filenames or maintain duplicate filename buffers. In addition, each
-// platform has its own code sections to avoid cross-platform assumptions
-// altogether.
-//
-// --------------------------------------------------------------------
-//
-// $Log: pathwork.cpp,v $
-// Revision 1.12  1999/01/23 21:29:48  patrick
-//
-// OS2-EMX does not need special care here
-//
-// Revision 1.11  1999/01/08 21:20:38  sugalskd
-// Fixed VMS path-finding code
-//
-// Revision 1.10  1998/10/04 11:35:49  remi
-// Id tags fun.
-//
-// Revision 1.9  1998/08/05 16:49:32  cberry
-// commented out #define DEBUG that KB had put in for some reason.
-//
-// Revision 1.8  1998/07/30 02:11:52  blast
-// AmigaOS update
-//
-// Revision 1.7  1998/07/13 12:40:31  kbracey
-// RISC OS update.
-// Added -noquiet option.
-//
-// Revision 1.6  1998/07/05 23:32:12  cyruspatel
-// Fixed missing semicolon in *nix'ish code section.
-//
-// Revision 1.3  1998/07/05 20:27:02  jlawson
-// headers for win32s and win16 and borland/dos
-//
-// Revision 1.2  1998/07/05 20:13:41  jlawson
-// modified headers for Win32
-//
-// Revision 1.1  1998/07/05 13:09:07  cyruspatel
-// Created - see documentation above.
-//
-//
+/*
+ * Copyright distributed.net 1997 - All Rights Reserved
+ * For use in distributed.net projects only.
+ * Any other distribution or use of this source violates copyright.
+ *
+ * This module contains functions for setting the "working directory"
+ * and pathifying a filename that has no dirspec. Functions need to be
+ * initialized from main() with InitWorkingDirectoryFromSamplePaths();
+ * [ ...( inipath, apppath) where inipath should not have been previously
+ * merged from argv[0] + default, although it doesn't hurt if it has.]
+ *
+ * The "working directory" is assumed to be the app's directory, unless the
+ * ini filename contains a dirspec, in which case the path to the ini file
+ * is used. The exception here is win32/win16 which, for reasons of backward
+ * compatability, always use the app's directory.
+ *
+ * GetFullPathForFilename() is ideally intended for use in (or just prior to)
+ * a call to fopen(). This obviates the necessity of having to pre-parse
+ * filenames or maintain duplicate filename buffers. In addition, each
+ * platform has its own code sections to avoid cross-platform assumptions
+ * altogether.
+ *
+ * --------------------------------------------------------------------
+ *
+ * $Log: pathwork.cpp,v $
+ * Revision 1.13  1999/04/01 03:04:37  cyp
+ * Created GetFullPathForFilenameAndDir(). Like GetFullPathForFilename() but
+ * can use a directory other than the client's wd.
+ *
+ * Revision 1.12  1999/01/23 21:29:48  patrick
+ * OS2-EMX does not need special care here
+ *
+ * Revision 1.11  1999/01/08 21:20:38  sugalskd
+ * Fixed VMS path-finding code
+ *
+ * Revision 1.10  1998/10/04 11:35:49  remi
+ * Id tags fun.
+ *
+ * Revision 1.9  1998/08/05 16:49:32  cberry
+ * commented out #define DEBUG that KB had put in for some reason.
+ *
+ * Revision 1.8  1998/07/30 02:11:52  blast
+ * AmigaOS update
+ *
+ * Revision 1.7  1998/07/13 12:40:31  kbracey
+ * RISC OS update. Added -noquiet option.
+ *
+ * Revision 1.6  1998/07/05 23:32:12  cyruspatel
+ * Fixed missing semicolon in *nix'ish code section.
+ *
+ * Revision 1.3  1998/07/05 20:27:02  jlawson
+ * headers for win32s and win16 and borland/dos
+ *
+ * Revision 1.2  1998/07/05 20:13:41  jlawson
+ * modified headers for Win32
+ *
+ * Revision 1.1  1998/07/05 13:09:07  cyruspatel
+ * Created - see documentation above. - my apologies to all non-win32 people,
+ * for the sake of one platform, we bork the rest. :(
+ *
+*/
 
 #if (!defined(lint) && defined(__showids__))
 const char *pathwork_cpp(void) {
-return "@(#)$Id: pathwork.cpp,v 1.12 1999/01/23 21:29:48 patrick Exp $"; }
+return "@(#)$Id: pathwork.cpp,v 1.13 1999/04/01 03:04:37 cyp Exp $"; }
 #endif
 
 #include <stdio.h>
@@ -84,57 +88,60 @@ return "@(#)$Id: pathwork.cpp,v 1.12 1999/01/23 21:29:48 patrick Exp $"; }
 #if (CLIENT_OS == OS_RISCOS)
 #define MAX_FULLPATH_BUFFER_LENGTH (1024)
 #else
-#define MAX_FULLPATH_BUFFER_LENGTH (256)
+#define MAX_FULLPATH_BUFFER_LENGTH (512)
 #endif
 
-// ---------------------------------------------------------------------
+/* ------------------------------------------------------------------------ */
 
-static int IsFilenamePathified( const char *filename )
+/* get the offset of the filename component in fully qualified path */
+/* previously called IsFilenamePathified() */
+unsigned int GetFilenameBaseOffset( const char *fullpath )
 {
   char *slash;
+  if ( !fullpath )
+    return 0;
   #if (CLIENT_OS == OS_MACOS)
-    slash = strrchr( filename, ':' );
+    slash = strrchr( fullpath, ':' );
   #elif (CLIENT_OS == OS_VMS)
-    slash = strrchr( filename, ':' );
-    char *slash2 = strrchr( filename, '$' );
+    slash = strrchr( fullpath, ':' );
+    char *slash2 = strrchr( fullpath, '$' );
     if (slash2 > slash) slash = slash2;
   #elif (CLIENT_OS == OS_RISCOS)
-    slash = strrchr( filename, '.' );
+    slash = strrchr( fullpath, '.' );
   #elif (CLIENT_OS == OS_DOS) || (CLIENT_OS == OS_WIN16) || \
     (CLIENT_OS == OS_WIN32) || (CLIENT_OS == OS_WIN32S) || \
     (CLIENT_OS == OS_OS2)
-    slash = strrchr( (char*) filename, '\\' );
-    char *slash2 = strrchr( (char*) filename, '//' );
+    slash = strrchr( (char*) fullpath, '\\' );
+    char *slash2 = strrchr( (char*) fullpath, '//' );
     if (slash2 > slash) slash = slash2;
-    slash2 = strrchr( (char*) filename, ':' );
+    slash2 = strrchr( (char*) fullpath, ':' );
     if (slash2 > slash) slash = slash2;
   #elif (CLIENT_OS == OS_NETWARE)
-    slash = strrchr( filename, '\\' );
-    char *slash2 = strrchr( filename, '//' );
+    slash = strrchr( fullpath, '\\' );
+    char *slash2 = strrchr( fullpath, '//' );
     if (slash2 > slash) slash = slash2;
-    slash2 = strrchr( filename, ':' );
+    slash2 = strrchr( fullpath, ':' );
     if (slash2 > slash) slash = slash2;
   #elif (CLIENT_OS == OS_AMIGAOS)
-    slash = strrchr( filename, '/' );
-    char *slash2 = strrchr( filename, ':' );
+    slash = strrchr( fullpath, '/' );
+    char *slash2 = strrchr( fullpath, ':' );
     if (slash2 > slash) slash = slash2;
   #else
-    slash = strrchr( filename, '/' );
+    slash = strrchr( fullpath, '/' );
   #endif
-  return (( slash == NULL ) ? (0) : (( slash - filename )+1) );
+  return (( slash == NULL ) ? (0) : (( slash - fullpath )+1) );
 }
 
-// ---------------------------------------------------------------------
+/* ------------------------------------------------------------------------ */
 
 static char cwdBuffer[MAX_FULLPATH_BUFFER_LENGTH+1];
 static unsigned int cwdBufferLength = 0xFFFFFFFFL;
 
-// ---------------------------------------------------------------------
-
-// the working directory is the app's directory, unless the ini filename
-// contains a dirspec, in which case the path to the ini file is used.
-// Exception to the rule: win32/win16 which always use the app's directory.
-//
+/* ------------------------------------------------------------------------ */
+/* the working directory is the app's directory, unless the ini filename    */
+/* contains a dirspec, in which case the path to the ini file is used.      */
+/* Exception to the rule: win32/win16 which always use the app's directory. */
+/* ------------------------------------------------------------------------ */
 
 int InitWorkingDirectoryFromSamplePaths( const char *inipath, const char *apppath )
 {
@@ -142,66 +149,53 @@ int InitWorkingDirectoryFromSamplePaths( const char *inipath, const char *apppat
   if ( apppath == NULL ) apppath = "";
 
   #if (CLIENT_OS == OS_MACOS)
-    {
+  {
     strcpy( cwdBuffer, inipath );
     char *slash = strrchr(cwdBuffer, ':');
     if (slash != NULL) *(slash+1) = 0;   //<peterd> On the Mac, the current
     else cwdBuffer[0] = 0; // directory is always the apps directory at startup.
-    }
+  }
   #elif (CLIENT_OS == OS_VMS)
-    {
+  {
     strcpy( cwdBuffer, inipath );
     char *slash, *bracket, *dirend;
     slash = strrchr(cwdBuffer, ':');
     bracket = strrchr(cwdBuffer, ']');
     dirend = (slash > bracket ? slash : bracket);
     if (dirend == NULL && apppath != NULL && strlen( apppath ) > 0)
-      {
+    {
       strcpy( cwdBuffer, apppath );
       slash = strrchr(cwdBuffer, ':');
       bracket = strrchr(cwdBuffer, ']');
       dirend = (slash > bracket ? slash : bracket);
-      }
+    }
     if (dirend != NULL) *(dirend+1) = 0;
     else cwdBuffer[0] = 0;  //current directory is also always the apps dir
-    }
+  }
   #elif (CLIENT_OS == OS_NETWARE)
-    {
+  {
     strcpy( cwdBuffer, inipath );
     char *slash = strrchr(cwdBuffer, '/');
     char *slash2 = strrchr(cwdBuffer, '\\');
     if (slash2 > slash) slash = slash2;
     if ( slash == NULL )
-      {
+    {
       if ( strlen( cwdBuffer ) < 2 || cwdBuffer[1] != ':' )
-        {
+      {
         strcpy( cwdBuffer, apppath );
         slash = strrchr(cwdBuffer, '/');
         slash2 = strrchr(cwdBuffer, '\\');
         if (slash2 > slash) slash = slash2;
-        }
+      }
       if ( slash == NULL && strlen( cwdBuffer ) >= 2 && cwdBuffer[1] == ':' )
         slash = &( cwdBuffer[1] );
-      }
+    }
     if (slash != NULL) *(slash+1) = 0;
     else cwdBuffer[0] = 0;
-    }
+  }
   #elif (CLIENT_OS == OS_WIN32) || (CLIENT_OS == OS_WIN32S) || \
         (CLIENT_OS == OS_WIN16)
-    {
-    if ( *apppath == 0 && *inipath == 0) //post-main call
-      {
-      cwdBuffer[0] = 0;
-      ::GetModuleFileName(NULL, cwdBuffer, sizeof(cwdBuffer));
-      }
-    else
-      strcpy( cwdBuffer, apppath );
-    char *slash = strrchr(cwdBuffer, '\\');
-    if (slash != NULL) *(slash+1) = 0;
-    else cwdBuffer[0] = 0;
-    }
-  #elif (CLIENT_OS == OS_DOS) || ( (CLIENT_OS == OS_OS2) && !defined(__EMX__) )
-    {
+  {
     strcpy( cwdBuffer, inipath );
     char *slash = strrchr(cwdBuffer, '/');
     char *slash2 = strrchr(cwdBuffer, '\\');
@@ -209,27 +203,45 @@ int InitWorkingDirectoryFromSamplePaths( const char *inipath, const char *apppat
     slash2 = strrchr(cwdBuffer, ':');
     if (slash2 > slash) slash = slash2;
     if ( slash == NULL )
-      {
+    {
+      strcpy( cwdBuffer, apppath );
+      if ( *apppath == 0 )
+        GetModuleFileName(NULL, cwdBuffer, sizeof(cwdBuffer));
+      slash = strrchr(cwdBuffer, '\\');
+    }
+    if (slash != NULL) *(slash+1) = 0;
+    else cwdBuffer[0] = 0;
+  }
+  #elif (CLIENT_OS == OS_DOS) || ( (CLIENT_OS == OS_OS2) && !defined(__EMX__) )
+  {
+    strcpy( cwdBuffer, inipath );
+    char *slash = strrchr(cwdBuffer, '/');
+    char *slash2 = strrchr(cwdBuffer, '\\');
+    if (slash2 > slash) slash = slash2;
+    slash2 = strrchr(cwdBuffer, ':');
+    if (slash2 > slash) slash = slash2;
+    if ( slash == NULL )
+    {
       strcpy( cwdBuffer, apppath );
       slash = strrchr(cwdBuffer, '\\');
-      }
+    }
     if ( slash == NULL )
-      {
+    {
       cwdBuffer[0] = cwdBuffer[ sizeof( cwdBuffer )-2 ] = 0;
       if ( getcwd( cwdBuffer, sizeof( cwdBuffer )-2 )==NULL )
         strcpy( cwdBuffer, ".\\" ); //don't know what else to do
       else if ( cwdBuffer[ strlen( cwdBuffer )-1 ] != '\\' )
         strcat( cwdBuffer, "\\" );
-      }
+    }
     else
-      {
+    {
       *(slash+1) = 0;
       if ( ( *slash == ':' ) && ( strlen( cwdBuffer )== 2 ) )
-        {
+      {
         char buffer[256];
         buffer[0] = buffer[ sizeof( buffer )-1 ] = 0;
         #if (defined(__WATCOMC__))
-          {
+        {
           unsigned int drive1, drive2, drive3;
           _dos_getdrive( &drive1 );   /* 1 to 26 */
           drive2 = ( toupper( *cwdBuffer ) - 'A' )+1;  /* 1 to 26 */
@@ -238,76 +250,73 @@ int InitWorkingDirectoryFromSamplePaths( const char *inipath, const char *apppat
           if (drive2 != drive3 || getcwd( buffer, sizeof(buffer)-1 )==NULL)
             buffer[0]=0;
           _dos_setdrive( drive1, &drive3 );
-          }
+        }
         #else
           #error FIXME: need to get the current directory on that drive
           //strcat( cwdBuffer, ".\\" );  does not work
         #endif
         if (buffer[0] != 0 && strlen( buffer ) < (sizeof( cwdBuffer )-2) )
-          {
+        {
           strcpy( cwdBuffer, buffer );
           if ( cwdBuffer[ strlen( cwdBuffer )-1 ] != '\\' )
             strcat( cwdBuffer, "\\" );
-          }
         }
       }
     }
+  }
   #elif ( CLIENT_OS == OS_RISCOS )
+  {
+    const char *runpath = NULL;
+    if (*inipath == '\0')
     {
-    if (inipath[0] == '\0')
-      {
-      _swi(OS_FSControl, _INR(0,5), 37, apppath, cwdBuffer,
-                         "Run$Path", NULL, sizeof cwdBuffer);
-      }
-    else
-      {
-      _swi(OS_FSControl, _INR(0,5), 37, inipath, cwdBuffer,
-                         NULL, NULL, sizeof cwdBuffer);
-
-      }
+      inipath = apppath;
+      runpath = "Run$Path";
+    }
+    _swi(OS_FSControl, _INR(0,5), 37, inipath, cwdBuffer,
+                         runpath, NULL, sizeof cwdBuffer);
     char *slash = strrchr( cwdBuffer, '.' );
     if ( slash != NULL )
       *(slash+1) = 0;
     else cwdBuffer[0]=0;
-    }
+  }
   #elif (CLIENT_OS == OS_AMIGAOS)
-    {
+  {
     strcpy( cwdBuffer, inipath );
     char *slash = strrchr(cwdBuffer, ':');
     char *slash2 = strrchr(cwdBuffer, '/');
     if (slash2 > slash) slash = slash2;
     if (slash == NULL && apppath != NULL && strlen( apppath ) > 0)
-      {
+    {
       strcpy( cwdBuffer, apppath );
       slash = strrchr(cwdBuffer, ':');
-      }
+    }
     if (slash != NULL) *(slash+1) = 0;
     else cwdBuffer[0] = 0; // Means we're started from the dir the things are in...
-    }
+  }
   #else
-    {
+  {
     strcpy( cwdBuffer, inipath );
     char *slash = strrchr( cwdBuffer, '/' );
     if (slash == NULL)
-      {
+    {
       strcpy( cwdBuffer, apppath );
       slash = strrchr( cwdBuffer, '/' );
-      }
+    }
     if ( slash != NULL )
       *(slash+1) = 0;
     else
       strcpy( cwdBuffer, "./" );
-    }
+  }
   #endif
   cwdBufferLength = strlen( cwdBuffer );
 
-  #ifdef DEBUG
+  #ifdef DEBUG_PATHWORK
   printf( "Working directory is \"%s\"\n", cwdBuffer );
   #endif
   return 0;
 }
 
-// ---------------------------------------------------------------------
+/* --------------------------------------------------------------------- */
 
 const char *GetWorkingDirectory( char *buffer, unsigned int maxlen )
 {
@@ -317,10 +326,8 @@ const char *GetWorkingDirectory( char *buffer, unsigned int maxlen )
     return "";
   #if (CLIENT_OS == OS_WIN32) || (CLIENT_OS == OS_WIN16) || \
      (CLIENT_OS == OS_WIN32S)
-    {
-    if ( cwdBufferLength == 0xFFFFFFFFL )  //NOMAIN support
-      InitWorkingDirectoryFromSamplePaths( "", "" );
-    }
+  if ( cwdBufferLength == 0xFFFFFFFFL )
+    InitWorkingDirectoryFromSamplePaths( "", "" );
   #endif
   if ( cwdBufferLength == 0xFFFFFFFFL ) //not initialized
     buffer[0] = 0;
@@ -331,29 +338,60 @@ const char *GetWorkingDirectory( char *buffer, unsigned int maxlen )
   return buffer;
 }
 
-// ---------------------------------------------------------------------
+/* --------------------------------------------------------------------- */
+
+static char pathBuffer[MAX_FULLPATH_BUFFER_LENGTH+2];
 
 const char *GetFullPathForFilename( const char *filename )
 {
-  static char pathBuffer[MAX_FULLPATH_BUFFER_LENGTH+2];
   const char *outpath;
 
   if ( filename == NULL || filename[0] == 0 )
     outpath = "";
-  else if ( IsFilenamePathified( filename ) != 0 )
+  else if ( GetFilenameBaseOffset( filename ) != 0 )
     outpath = filename;
   else if ( GetWorkingDirectory( pathBuffer, sizeof( pathBuffer ) )==NULL )
     outpath = "";
-  else if (strlen(pathBuffer)+strlen(filename)+1 > sizeof pathBuffer)
+  else if (strlen(pathBuffer)+strlen(filename)+1 > sizeof(pathBuffer))
     outpath = "";
   else
     outpath = strcat(pathBuffer, filename);
 
-  #ifdef DEBUG
+  #ifdef DEBUG_PATHWORK
   printf( "got \"%s\" returning \"%s\"\n", filename, outpath );
   #endif
 
   return ( outpath );
 }
 
-// ---------------------------------------------------------------------
+/* --------------------------------------------------------------------- */
+
+const char *GetFullPathForFilenameAndDir( const char *fname, const char *dir )
+{
+  if (!fname)
+    fname = "";
+  else if ( GetFilenameBaseOffset( fname ) != 0 )
+    return fname;
+  if (!dir)
+    return GetFullPathForFilename( fname );
+  if ( ( strlen( dir ) + strlen( fname ) + 2 ) > sizeof(pathBuffer) )
+    return "";
+  strcpy( pathBuffer, dir );
+  if ( strlen( pathBuffer ) > GetFilenameBaseOffset( pathBuffer ) )
+  {
+    /* dirname is not terminated with a directory separator - so add one */
+    #if (CLIENT_OS == OS_MACOS) || (CLIENT_OS == OS_VMS)
+      strcat( pathBuffer, ":" );
+    #elif (CLIENT_OS == OS_RISCOS)
+      strcat( pathBuffer, "." );
+    #elif (CLIENT_OS == OS_DOS) || (CLIENT_OS == OS_WIN16) || \
+      (CLIENT_OS == OS_WIN32) || (CLIENT_OS == OS_WIN32S) || \
+      (CLIENT_OS == OS_OS2)
+      strcat( pathBuffer, "\\" );
+    #else
+      strcat( pathBuffer, "/" );
+    #endif
+  }
+  strcat( pathBuffer, fname );
+  return pathBuffer;
+}  
--- common/pathwork.h
+++ common/pathwork.h
@@ -1,99 +1,81 @@
-// Hey, Emacs, this is a -*-C++-*- file !
-
-// Copyright distributed.net 1997 - All Rights Reserved
-// For use in distributed.net projects only.
-// Any other distribution or use of this source violates copyright.
-//
-// $Log: pathwork.h,v $
-// Revision 1.4  1998/08/19 17:00:25  cyruspatel
-// dos2unix'ified.
-//
-// Revision 1.3  1998/08/10 20:22:23  cyruspatel
-// Moved path related defines from client.h. Created macros so pathwork.cpp
-// functions are do-nothing functions if DONT_USE_PATHWORK is defined.
-//
-// Revision 1.2  1998/07/06 01:47:05  cyruspatel
-// Added signature so that emacs recognizes this to be a c++ file.
-//
-// Revision 1.1  1998/07/05 13:09:09  cyruspatel
-// Created - see pathwork.cpp for documentation 
-//
+/* Hey, Emacs, this is a -*-C++-*- file !
+ *
+ * Copyright distributed.net 1997 - All Rights Reserved
+ * For use in distributed.net projects only.
+ * Any other distribution or use of this source violates copyright.
+ *
+ * $Log: pathwork.h,v $
+ * Revision 1.5  1999/04/01 03:04:37  cyp
+ * Created GetFullPathForFilenameAndDir(). Like GetFullPathForFilename() but
+ * can use a directory other than the client's wd.
+ *
+ * Revision 1.4  1998/08/19 17:00:25  cyruspatel
+ * dos2unix'ified.
+ *
+ * Revision 1.3  1998/08/10 20:22:23  cyruspatel
+ * Moved path related defines from client.h. Created macros so pathwork.cpp
+ * functions are do-nothing functions if DONT_USE_PATHWORK is defined.
+ *
+ * Revision 1.2  1998/07/06 01:47:05  cyruspatel
+ * Added signature so that emacs recognizes this to be a c++ file.
+ *
+ * Revision 1.1  1998/07/05 13:09:09  cyruspatel
+ * Created - see pathwork.cpp for documentation. - my apologies to all 
+ * non-win32 people, for the sake of one platform, we bork the rest. :(
+ *
+*/
 
 #ifndef __PATHWORK_H__
 #define __PATHWORK_H__
 
-#ifndef DONT_USE_PATHWORK
-
-// -------------------------------------------------------------------
-// Get the working directory previously initialized with 
-// InitWorkingDirectoryFromSamplePaths(). returns NULL if buffer is too
-// small otherwise returns buffer. The directory returned can be 
-// strcat()ed with a filename (ie it always ends with '/' or whatever)
-
+/* -------------------------------------------------------------------
+ * Get the working directory previously initialized with 
+ * InitWorkingDirectoryFromSamplePaths(). returns NULL if buffer is too
+ * small otherwise returns buffer. The directory returned can be 
+ * strcat()ed with a filename (ie it always ends with '/' or whatever)
+ * ------------------------------------------------------------------ */
 const char *GetWorkingDirectory( char *buffer, unsigned int maxlen );
 
-// -------------------------------------------------------------------
-// the working directory is the app's directory, unless the ini filename
-// contains a dirspec, in which case the path to the ini file is used.
-// Exception to the rule: win32/win16 which always use the app's directory.
 
+/* -------------------------------------------------------------------
+ * the working directory is the app's directory, unless the ini filename
+ * contains a dirspec, in which case the path to the ini file is used.
+ * Exception to the rule: win32/win16 which always use the app's directory.
+ *------------------------------------------------------------------- */
 int InitWorkingDirectoryFromSamplePaths( const char *inipath, const char *apppath );
 
-// -------------------------------------------------------------------
-// prepends the working directory to 'filename' if the filename is
-// not already pathified. Returns a pointer to the pathified filename
-// or to a zero length string on error (which will cause fopen() to fail)
 
+/* -------------------------------------------------------------------
+ * prepends the working directory to 'filename' if the filename is
+ * not already pathified. Returns a pointer to the pathified filename
+ * or to a zero length string on error (which will cause fopen() to fail)
+ *------------------------------------------------------------------- */
 const char *GetFullPathForFilename( const char *filename );
 
-// -------------------------------------------------------------------
+
+/* -------------------------------------------------------------------
+ * prepends the directory 'dir' to 'fname' if the filename is
+ * not already pathified. Returns a pointer to the pathified filename
+ * or to a zero length string on error (which will cause fopen() to fail)
+ * Equivalent to GetFullPathForFilename() if 'dir' is NULL.
+ *------------------------------------------------------------------- */
+const char *GetFullPathForFilenameAndDir( const char *fname, const char *dir );
+
+
+/* -------------------------------------------------------------------
+ * get the offset of the filename component in 'fullpath' 
+ *------------------------------------------------------------------- */
+unsigned int GetFilenameBaseOffset( const char *fullpath );
+
+
+/* ------------------------------------------------------------------
+ * Suffix separator (called an 'extension' in the windos world)
+ * ------------------------------------------------------------------ */
 
 #if (CLIENT_OS == OS_RISCOS)
   #define EXTN_SEP   "/"
 #else
   #define EXTN_SEP   "."
 #endif
-
-#endif //#ifndef DONT_USE_PATHWORK
-
-// *******************************************************************
-
-#ifdef DONT_USE_PATHWORK
-
-#define GetFullPathForFilename( x ) ( x )
-#define InitWorkingDirectoryFromSamplePaths( ip, ap ) //nothing
-//#define GetWorkingDirectory( b, z ) 
-
-  #if (CLIENT_OS == OS_NETWARE)
-  //#define PATH_SEP   "\\"   //left undefined so I can see
-  //#define PATH_SEP_C '\\'   //where the bad references are
-  #define EXTN_SEP   "."
-  #define EXTN_SEP_C '.'
-  #elif ((CLIENT_OS == OS_DOS) || CLIENT_OS == OS_WIN32) || (CLIENT_OS == OS_WIN32S) || (CLIENT_OS == OS_WIN16) || (CLIENT_OS == OS_OS2)
-  #define PATH_SEP   "\\"
-  #define PATH_SEP_C '\\'
-  #define ALT_PATH_SEP '/'
-  #define ALT_PATH_SEP_C '/'
-  #define DRIVE_SEP ':'
-  #define DRIVE_SEP_C ':'
-  #define EXTN_SEP   "."
-  #define EXTN_SEP_C '.'
-  #elif (CLIENT_OS == OS_MACOS)
-  #define PATH_SEP   ":"
-  #define PATH_SEP_C ':'
-  #define EXTN_SEP   "."
-  #define EXTN_SEP_C '.'
-  #elif (CLIENT_OS == OS_RISCOS)
-  #define PATH_SEP   "."
-  #define PATH_SEP_C '.'
-  #define EXTN_SEP   "/"
-  #define EXTN_SEP_C '/'
-  #else
-  #define PATH_SEP   "/"
-  #define PATH_SEP_C '/'
-  #define EXTN_SEP   "."
-  #define EXTN_SEP_C '.'
-  #endif
-#endif //DONT_USE_PATHWORK
 
 #endif //__PATHWORK_H__
