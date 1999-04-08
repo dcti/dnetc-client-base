6c6e1358b057af93c8c5d13325ab45988e15835e
Cyrus Patel <cyp@distributed.net>
1999-04-08 16:33:09 +0000

    Added CSC data

--- common/clicdata.cpp
+++ common/clicdata.cpp
@@ -12,7 +12,7 @@
  * ----------------------------------------------------------------------
 */ 
 const char *clicdata_cpp(void) {
-return "@(#)$Id: clicdata.cpp,v 1.17 1999/04/05 17:56:50 cyp Exp $"; }
+return "@(#)$Id: clicdata.cpp,v 1.18 1999/04/08 16:33:09 cyp Exp $"; }
 
 #include "baseincs.h" //for timeval
 #include "clitime.h" //required for CliTimerDiff() and CliClock()
@@ -32,6 +32,7 @@ static struct contestInfo
 } conStats[] = {  { "RC5", 0,  1, 0, 0, {0,0}, {0,0} },
                   { "DES", 1,  2, 0, 0, {0,0}, {0,0} },
                   { "OGR", 2,  1, 0, 0, {0,0}, {0,0} },
+		  { "CSC", 3,  2, 0, 0, {0,0}, {0,0} },
                   {  NULL,-1,  0, 0, 0, {0,0}, {0,0} }  };
 
 /* ----------------------------------------------------------------------- */
