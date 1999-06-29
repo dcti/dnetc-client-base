13c4bf51b5fdf3211e7df3c7963ae351fc43e585
Nate Begeman <sampo@distributed.net>
1999-06-29 01:38:03 +0000

    You need to accumulate kitter from each call tocrunch_lintilla(). Also, it is only necessary process 1 key withcrunch_allitnil(). (256 not only waists time but will cause an error ifthere are less than 256 keys remaining in the timeslice) Try this instead...

--- rc5/ppc/crunch_ppc_wrappers.cpp
+++ rc5/ppc/crunch_ppc_wrappers.cpp
@@ -22,27 +22,27 @@ s32 rc5_unit_func_g1( RC5UnitWork *work, u32 *timeslice /* , void *scratch_area
   }
 }
 
-s32 rc5_unit_func_g2_g3( RC5UnitWork *work, u32 *timeslice /* , void *scratch_area */) {
-  u32 kiter = 0;
+s32 rc5_unit_func_g2_g3( RC5UnitWork *work, u32 *timeslice /* , void *scratch_area 
+*/) {
+	
+	u32 kiter = 0; /* keys processed so far */
+	do {
+		kiter += crunch_lintilla(work, *timeslice-kiter);
+		if (kiter < *timeslice)
+		{
+			if (crunch_allitnil(work,1) == 0) break;
+			kiter++;
+		}
+	} while (kiter < *timeslice);
 
-  do {
-    kiter = crunch_lintilla( work, *timeslice - kiter );
-    if (kiter < *timeslice) {
-      u32 kiter_allitnil = crunch_allitnil( work, 256 );
-      if (kiter_allitnil) 
-        kiter += kiter_allitnil;
-      else
-        break;
-    }
-  } while (kiter < *timeslice);
-  if (*timeslice == kiter) {
-    return RESULT_WORKING;
-  } else if (*timeslice < kiter) {
-    *timeslice = kiter;
-    return RESULT_FOUND;
-  } else { /* shouldn't happen with this core */
-    *timeslice = kiter; 
-    return -1; /* error */
-  }
+  	if (*timeslice == kiter) {
+		 return RESULT_WORKING;
+	} else if (*timeslice < kiter) {
+		*timeslice = kiter;
+		return RESULT_FOUND;
+	} else { /* shouldn't happen with this core */
+		*timeslice = kiter;
+		 return -1; /* error */
+	}
 }
-};
+}
