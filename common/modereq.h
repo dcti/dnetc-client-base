3c79be8c55c4f467ac73622f4fd10da7bb99dec7
Cyrus Patel <cyp@distributed.net>
1999-09-18 17:56:44 +0000

    incorporated constants from devel branch

--- common/modereq.h
+++ common/modereq.h
@@ -16,25 +16,28 @@
 */ 
 
 #ifndef __MODEREQ_H__
-#define __MODEREQ_H__ "@(#)$Id: modereq.h,v 1.11.2.2 1999/06/10 18:19:54 cyp Exp $"
+#define __MODEREQ_H__ "@(#)$Id: modereq.h,v 1.11.2.3 1999/09/18 17:56:44 cyp Exp $"
 
-#define MODEREQ_IDENT              0x0001    
-#define MODEREQ_CPUINFO            0x0002
-#define MODEREQ_TEST               0x0004
-#define MODEREQ_CONFIG             0x0008
-#define MODEREQ_FETCH              0x0010
-#define MODEREQ_FLUSH              0x0020
-#define MODEREQ_FQUIET             0x0040 /* fetch/flush quietly */
-#define MODEREQ_CONFRESTART        0x0080 /* set restart flag after successful config? */
-#define MODEREQ_BENCHMARK_RC5      0x0100
-#define MODEREQ_BENCHMARK_DES      0x0200
-#define MODEREQ_BENCHMARK_ALL      (MODEREQ_BENCHMARK_DES|MODEREQ_BENCHMARK_RC5)
-#define MODEREQ_BENCHMARK_QUICK    0x0400
-#define MODEREQ_CMDLINE_HELP       0x0800
-#define MODEREQ_UNLOCK             0x1000
-#define MODEREQ_IMPORT             0x2000
-#define MODEREQ_RESTART            0x8000 /* restart client after mode processing */
-#define MODEREQ_ALL                0xBFFF /* mask of all - needed internally */
+#define MODEREQ_IDENT              0x00000001
+#define MODEREQ_CPUINFO            0x00000002
+#define MODEREQ_TEST               0x00000004
+#define MODEREQ_CONFIG             0x00000008
+#define MODEREQ_FETCH              0x00000010
+#define MODEREQ_FLUSH              0x00000020
+#define MODEREQ_FQUIET             0x00000040 /* fetch/flush quietly */
+#define MODEREQ_CONFRESTART        0x00000080 /* set restart flag after successful config? */
+#define MODEREQ_BENCHMARK_RC5      0x00000100
+#define MODEREQ_BENCHMARK_DES      0x00000200
+#define MODEREQ_BENCHMARK_OGR      0x00000400
+#define MODEREQ_BENCHMARK_CSC      0x00000800
+#define MODEREQ_BENCHMARK_ALL      (MODEREQ_BENCHMARK_DES|MODEREQ_BENCHMARK_RC5|\
+                                    MODEREQ_BENCHMARK_CSC|MODEREQ_BENCHMARK_OGR)
+#define MODEREQ_BENCHMARK_QUICK    0x00001000
+#define MODEREQ_CMDLINE_HELP       0x00002000
+#define MODEREQ_UNLOCK             0x00004000
+#define MODEREQ_IMPORT             0x00008000
+#define MODEREQ_RESTART            0x00010000 /* restart client after mode processing */
+#define MODEREQ_ALL                0x0001FFFF /* mask of all - needed internally */
 
 /* get mode bit(s). if modemask is -1, all bits are returned */
 extern int ModeReqIsSet(int modemask);
