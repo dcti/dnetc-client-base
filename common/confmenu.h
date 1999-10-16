ced990c43807f44597c1ce8ee2171c2995a05ae4
Cyrus Patel <cyp@distributed.net>
1999-10-16 22:36:36 +0000

    bah. where did this go?

---
+++ common/confmenu.h
@@ -0,0 +1,12 @@
+/* Hey, Emacs, this a -*-C++-*- file !
+ *
+ * Copyright distributed.net 1997-1998 - All Rights Reserved
+ * For use in distributed.net projects only.
+ * Any other distribution or use of this source violates copyright.
+*/
+#ifndef __CONFMENU_H__
+#define __CONFMENU_H__ "@(#)$Id: confmenu.h,v 1.2 1999/10/16 22:36:36 cyp Exp $"
+
+int Configure( Client *client ); /* returns >0==success, <0==cancelled */
+
+#endif /* __CONFMENU_H__ */
