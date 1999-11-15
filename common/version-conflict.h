886b33cdf534a12b150e6177aeee3969259efb54
Cyrus Patel <cyp@distributed.net>
1999-11-15 03:29:51 +0000

    2.8002-446

--- common/version-conflict.h
+++ common/version-conflict.h
@@ -23,12 +23,12 @@
  * ---------------------------------------------------------------------
 */
 #ifndef __VERSION_H__
-#define __VERSION_H__ "@(#)$Id: version-conflict.h,v 1.57 1999/10/11 17:59:20 cyp Exp $"
+#define __VERSION_H__ "@(#)$Id: version-conflict.h,v 1.58 1999/11/15 03:29:51 cyp Exp $"
 
 #define CLIENT_MAJOR_VER     2
 #define CLIENT_CONTEST       80
-#define CLIENT_BUILD         01
-#define CLIENT_BUILD_FRAC    445
-#define CLIENT_VERSIONSTRING "2.8001-445"
+#define CLIENT_BUILD         02
+#define CLIENT_BUILD_FRAC    446
+#define CLIENT_VERSIONSTRING "2.8002-446"
 
 #endif /* __VERSION_H__ */
