38490e8b7f2639821a96219f0bfd427a19cf30f4
Jeff Lawson <bovine@distributed.net>
1999-11-23 16:17:22 +0000

    changed buffer size check

--- common/base64.cpp
+++ common/base64.cpp
@@ -10,7 +10,7 @@
  *
 */ 
 const char *base64_cpp(void) {
-return "@(#)$Id: base64.cpp,v 1.2.2.3 1999/10/24 10:47:14 remi Exp $"; }
+return "@(#)$Id: base64.cpp,v 1.2.2.4 1999/11/23 16:17:22 jlawson Exp $"; }
 
 static unsigned char base64table[64] = 
 {
@@ -23,11 +23,11 @@ static unsigned char base64table[64] =
 
 int base64_encode(char *outbuf, const char *inbuf, 
                   unsigned int outbuflen, unsigned int inbuflen )
-{ /* outbuff must be at least (strlen(inbuf))*4/3) bytes */
+{ /* outbuff must be at least (((strlen(inbuf) + 2) / 3) * 4) bytes */
 
   unsigned int outlen = 0, errors = 0;
     
-  if (outbuflen < (((inbuflen)*4)/3))
+  if (outbuflen < ((inbuflen + 2) / 3) * 4)
     errors++;
   else if (inbuflen)
   {
