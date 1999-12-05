3a990c08c98e31f67441069b96fa933b1fcf5906
Cyrus Patel <cyp@distributed.net>
1999-12-05 12:37:57 +0000

    sync

--- rc5/mips/mips-irix.cpp
+++ rc5/mips/mips-irix.cpp
@@ -1,9 +1,15 @@
 /*
 // Just declare it so we can call the assebler version
 */
-extern "C" u32 rc5_unit_func_mips_crunch(register RC5UnitWork *, u32 iterations);
+#include "cputypes.h" /* u32 */
+#include "problem.h" /* RC5UnitWork */
 
-/*
-// end of file
-*/
+extern "C" {
+u32 rc5_unit_func_mips_crunch_asm(register RC5UnitWork *, u32 iter); /* that */
+u32 rc5_unit_func_mips_crunch(RC5UnitWork *, u32 iterations); /* this */
+}
 
+u32 rc5_unit_func_mips_crunch(RC5UnitWork *work, u32 iterations)
+{
+  return rc5_unit_func_mips_crunch_asm( work, iterations );
+}  
