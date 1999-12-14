909f0162c8f906dc5d3e3eb11533066677e886f6
Don Bruder <dakidd@distributed.net>
1999-12-14 10:04:40 +0000

    Fix typo in my previous fix. Oops...

--- common/selftest.cpp
+++ common/selftest.cpp
@@ -4,7 +4,7 @@
  * Any other distribution or use of this source violates copyright.
 */
 const char *selftest_cpp(void) {
-return "@(#)$Id: selftest.cpp,v 1.47.2.21 1999/12/14 03:07:19 dakidd Exp $"; }
+return "@(#)$Id: selftest.cpp,v 1.47.2.22 1999/12/14 10:04:40 dakidd Exp $"; }
 
 #include "cputypes.h"
 #include "client.h"    // CONTEST_COUNT
@@ -405,7 +405,7 @@ int SelfTest( unsigned int contest )
           #if (CLIENT_OS == OS_WIN16) || (CLIENT_OS == OS_WIN32) /* win32s */
           w32Yield(); /* pump waiting messages */
           #elif (CLIENT_OS == OS_MACOS)
-          NonPolledSleep(0); // was DoYieldToMain(0);
+          NonPolledUSleep(0);
           #elif (CLIENT_OS == OS_RISCOS)
           riscos_upcall_6();
           #elif (CLIENT_OS == OS_NETWARE)
