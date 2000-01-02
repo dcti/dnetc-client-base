d40a4d77b2eea8625d998732295a1da4bf76b654
Cyrus Patel <cyp@distributed.net>
2000-01-02 03:23:08 +0000

    fixed a trucation warning

--- common/probfill.h
+++ common/probfill.h
@@ -5,7 +5,7 @@
  * Any other distribution or use of this source violates copyright.
 */ 
 #ifndef __PROBFILL_H__
-#define __PROBFILL_H__ "@(#)$Id: probfill.h,v 1.8.2.4 1999/12/23 21:43:26 cyp Exp $"
+#define __PROBFILL_H__ "@(#)$Id: probfill.h,v 1.8.2.5 2000/01/02 03:23:08 cyp Exp $"
 
 #define PROBFILL_ANYCHANGED  1
 #define PROBFILL_GETBUFFERRS 2
@@ -28,7 +28,7 @@ extern int SetProblemLoaderFlags( const char *loaderflags_map /* 1 char per cont
 #define FILEENTRY_BUILDHI    ((CLIENT_BUILD_FRAC >> 8) & 0xff)
 #define FILEENTRY_BUILDLO    ((CLIENT_BUILD_FRAC     ) & 0xff)
 #define FILEENTRY_CPU(_core_cpu,_core_sel) \
-                             (((_core_cpu & 0x0f)<<4) | (_core_sel & 0x0f))
+                         ((char)(((_core_cpu & 0x0f)<<4) | (_core_sel & 0x0f)))
 
 #define FILEENTRY_CPU_TO_CPUNUM( _fe_cpu ) (( _fe_cpu >> 4 ) & 0x0f)
 #define FILEENTRY_CPU_TO_CORENUM( _fe_cpu ) ( _fe_cpu & 0x0f)
