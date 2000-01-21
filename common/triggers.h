4c2a823c14eac9fec9b2d2f01b36d24e63ce757d
Cyrus Patel <cyp@distributed.net>
2000-01-21 23:48:54 +0000

    pass 'doingmodes' bits to initializetriggers(); don't do some init if set

--- common/triggers.h
+++ common/triggers.h
@@ -5,14 +5,11 @@
  * Any other distribution or use of this source violates copyright.
 */ 
 #ifndef __TRIGGERS_H__
-#define __TRIGGERS_H__ "@(#)$Id: triggers.h,v 1.6 1999/04/06 10:20:48 cyp Exp $"
-
-#define PAUSEFILE_CHECKTIME_WHENON  (3)  //seconds
-#define PAUSEFILE_CHECKTIME_WHENOFF (3*PAUSEFILE_CHECKTIME_WHENON)
-#define EXITFILE_CHECKTIME          (PAUSEFILE_CHECKTIME_WHENOFF)
+#define __TRIGGERS_H__ "@(#)$Id: triggers.h,v 1.6.2.1 2000/01/21 23:48:55 cyp Exp $"
 
 //initialize... first call initializes the signal handler. args can be NULL
-extern int InitializeTriggers(const char *exitfile, const char *pausefile);
+extern int InitializeTriggers(int doingmodes, 
+                              const char *exitfile, const char *pausefile);
 
 //deinitialize...
 extern int DeinitializeTriggers(void);
@@ -48,12 +45,5 @@ extern int CheckExitRequestTriggerNoIO(void);
 //just return the pause trigger state (no poll cycle)
 //preferred method for child threads 
 extern int CheckPauseRequestTriggerNoIO(void); 
-
-//Hooks whatever signals are needed to be hooked.
-//Normally should NEVER be called, since initializetriggers
-//handles this.  Win32 needs to reinit this when consoles
-//are created, however.
-extern void CliSetupSignals( void );
-
 
 #endif //__TRIGGERS_H__
