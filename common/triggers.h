d830969dab4bd1c6cf27826a282f8c8b6705bf9d
Cyrus Patel <cyp@distributed.net>
2000-01-22 01:20:25 +0000

    *** empty log message ***

--- common/triggers.h
+++ common/triggers.h
@@ -5,30 +5,26 @@
  * Any other distribution or use of this source violates copyright.
 */ 
 #ifndef __TRIGGERS_H__
-#define __TRIGGERS_H__ "@(#)$Id: triggers.h,v 1.6.2.2 2000/01/22 00:56:34 ctate Exp $"
+#define __TRIGGERS_H__ "@(#)$Id: triggers.h,v 1.6.2.3 2000/01/22 01:20:25 cyp Exp $"
 
-#include "baseincs.h"
-
-/* These constants define symbolically the signal names used by the
- * dnetc -pause / -unpause mechanism.  The idea is that the "usual"
- * alternatives, SIGTSTP and SIGCONT, aren't as portable as one
- * might like, so here we provide a mechanism to allow per-platform
- * selection of the appropriate signal pair.
- *
- * Currently these defines are only used for __unix__ platforms, as
- * well as BeOS because it acts like Unix for most CLI purposes.
- */
-
-#if (CLIENT_OS == OS_BEOS)
-  /* Even though the BeOS build defines __unix__ for most purposes,
-   * SIGCONT is reserved by the native thread suspend/resume mechanism
-   * so we have to use something else */
-  #define TRIGGER_PAUSE_SIGNAL SIGUSR1
-  #define TRIGGER_UNPAUSE_SIGNAL SIGUSR2
-#elif defined(__unix__)
-  /* the usual Unix way to handle things like this is SIGTSTP/SIGCONT */
-  #define TRIGGER_PAUSE_SIGNAL SIGTSTP
-  #define TRIGGER_UNPAUSE_SIGNAL SIGCONT
+#if defined(__unix__)
+  /* These constants define symbolically the signal names used by the
+   * dnetc -pause / -unpause mechanism.  The idea is that the "usual"
+   * alternatives, SIGTSTP and SIGCONT, aren't as portable as one
+   * might like, so here we provide a mechanism to allow per-platform
+   * selection of the appropriate signal pair.
+   */
+  #include "baseincs.h"
+  #if (CLIENT_OS == OS_BEOS)
+    /* Even though the BeOS build defines __unix__ for most purposes,
+     * SIGCONT is reserved by the native thread suspend/resume mechanism
+     * so we have to use something else */
+    #define TRIGGER_PAUSE_SIGNAL SIGUSR1
+    #define TRIGGER_UNPAUSE_SIGNAL SIGUSR2
+  #else
+    #define TRIGGER_PAUSE_SIGNAL SIGTSTP
+    #define TRIGGER_UNPAUSE_SIGNAL SIGCONT
+  #endif
 #endif
 
 //initialize... first call initializes the signal handler. args can be NULL
