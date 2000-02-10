3e1b34bd1a6f4ad2e371d55454a9abe03d80bb3f
Cyrus Patel <cyp@distributed.net>
2000-02-10 16:46:41 +0000

    dialup.Stop must come after DeinitializeLogging() or mail may not get flushed.

--- common/client.cpp
+++ common/client.cpp
@@ -4,7 +4,7 @@
  * Any other distribution or use of this source violates copyright.
 */
 const char *client_cpp(void) {
-return "@(#)$Id: client.cpp,v 1.206.2.55 2000/02/06 06:17:08 cyp Exp $"; }
+return "@(#)$Id: client.cpp,v 1.206.2.56 2000/02/10 16:46:41 cyp Exp $"; }
 
 /* ------------------------------------------------------------------------ */
 
@@ -368,12 +368,17 @@ static int ClientMain( int argc, char *argv[] )
         TRACE_OUT((0,"initializeconnectivity\n"));
         if (InitializeConnectivity() == 0) //do global initialization
         {
+          #ifdef LURK /* start must come just after initializeconnectivity */
+          TRACE_OUT((0,"dialup.Start()\n")); /*and always before initlogging*/
+          dialup.Start(client->offlinemode, &(client->lurk_conf));
+          #endif
           TRACE_OUT((0,"initializeconsole\n"));
           if (InitializeConsole(client->quietmode,domodes) == 0)
           {
             //some plats need to wait for user input before closing the screen
             int con_waitforuser = 0; //only used if doing modes (and !-config)
-            TRACE_OUT((+1,"initializelogging\n"));
+
+            TRACE_OUT((0,"initializelogging\n"));
             InitializeLogging( (client->quietmode!=0), 
                                (client->percentprintingoff!=0),
                                client->logname, 
@@ -392,9 +397,6 @@ static int ClientMain( int argc, char *argv[] )
                                     (client->quietmode==0)); //show overrides
             TRACE_OUT((-1,"parsecmdline(1)\n"));
             InitRandom2( client->id );
-            #ifdef LURK
-            dialup.Start(client->offlinemode, &(client->lurk_conf));
-            #endif
             TRACE_OUT((+1,"initcoretable\n"));
             InitializeCoreTable( &(client->coretypes[0]) );
             TRACE_OUT((-1,"initcoretable\n"));
@@ -422,14 +424,15 @@ static int ClientMain( int argc, char *argv[] )
             ClientSetNumberOfProcessorsInUse(-1); /* reset */
             TRACE_OUT((0,"deinit coretable\n"));
             DeinitializeCoreTable();
-            #ifdef LURK
-            dialup.Stop();
-            #endif
             TRACE_OUT((0,"deinitialize logging\n"));
             DeinitializeLogging();
             TRACE_OUT((0,"deinitialize console\n"));
             DeinitializeConsole(con_waitforuser);
           }
+          #ifdef LURK 
+          TRACE_OUT((0,"dialup.Stop()\n"));
+          dialup.Stop(); /* just before DeinitializeConnectivity() */
+          #endif
           TRACE_OUT((0,"deinitialize connectivity\n"));
           DeinitializeConnectivity(); //netinit.cpp
         }
