cf06f174ec56f638c23d2e7a28c605220d1c3dc7
Michael Feiri <mfeiri@distributed.net>
2000-05-06 22:04:55 +0000

    Changed the name and options of the MacOS yield call.

--- common/bench.cpp
+++ common/bench.cpp
@@ -4,7 +4,7 @@
  * Any other distribution or use of this source violates copyright.
 */
 const char *bench_cpp(void) {
-return "@(#)$Id: bench.cpp,v 1.27.2.31 2000/02/07 01:56:43 mfeiri Exp $"; }
+return "@(#)$Id: bench.cpp,v 1.27.2.32 2000/05/06 22:04:55 mfeiri Exp $"; }
 
 #include "cputypes.h"  // CLIENT_OS, CLIENT_CPU
 #include "baseincs.h"  // general includes
@@ -311,7 +311,7 @@ long TBenchmark( unsigned int contestid, unsigned int numsecs, int flags )
         #if (CLIENT_OS == OS_WIN16) || (CLIENT_OS == OS_WIN32) /* win32s */
         w32Yield(); /* pump waiting messages */
         #elif (CLIENT_OS == OS_MACOS)
-        macosTickSleep(0);
+        macosSmartYield();
         #elif (CLIENT_OS == OS_RISCOS)
         riscos_upcall_6();
         #elif (CLIENT_OS == OS_NETWARE)
