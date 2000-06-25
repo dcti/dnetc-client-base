cad4f67ff3b361903356c4e6bb51ef9f13565134
Michael Feiri <mfeiri@distributed.net>
2000-06-25 04:26:48 +0000

    *** empty log message ***

---
+++ common/xmlserve.h
@@ -0,0 +1,23 @@
+// Copyright distributed.net 1997-1999 - All Rights Reserved
+// For use in distributed.net projects only.
+// Any other distribution or use of this source violates copyright.
+//
+
+
+#include "minihttp.h"
+
+#include <time.h>
+#include <string.h>
+
+#define MAX_CONNECTIONS   5
+#define CLIENTIDLETIMEOUT 30
+
+bool ProcessClientPacket(MiniHttpDaemonConnection *client);
+
+void __fdsetsockadd(SOCKET sock, fd_set *setname, int *sockmax, int *writecnt);
+
+void initxmlserve(int address, int port);
+
+void poll();
+
+void deinitxmlserve();
