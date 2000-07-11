309ae337973cbb5419b32c72f0a2d935bd3cffc0
Michael Feiri <mfeiri@distributed.net>
2000-07-11 01:37:17 +0000

    sync

--- rc5/68k/rc5-020_030-jg.s
+++ rc5/68k/rc5-020_030-jg.s
@@ -1,72 +1,65 @@
 
+	SECTION	rc5core,CODE
 	OPT	O+,W-
 
-	XDEF	_rc5_unit_func_000_030_asm
-
-;-------------------- 
-;@(#)$Id: rc5-000_030-jg.s,v 1.2 1999/12/08 01:28:52 cyp Exp $
-
-	; Define P, Q
-
-P:	equ	$b7e15163
-Q:	equ	$9e3779b9
-P0QR3:	equ	$bf0a8b1d	;(0*Q + P) <<< 3
-
 ;--------------------
 
-RUF_CODEALIGN:	MACRO
-	;Ensure next instruction is aligned on quadword boundary
-.align\@:	equ	*-_rc5_unit_func_000_030_asm
-	IFNE	.align\@&7
-	nop
-	RUF_CODEALIGN
-	ENDC
-	ENDM
+	XDEF	_rc5_unit_func_020_030
+
+	INCLUDE	rc5-0x0-jg.i
 
 ;--------------------
+;@(#)$Id: rc5-020_030-jg.s,v 1.2 2000/07/11 01:39:52 mfeiri Exp $
 
-	;680x0 RC5 core function
-	;Dual key, rolled loops, 020/030 optimised
+	; $VER: MC68020/68030 RC5 core 24-Jan-2000
 	;
-	;Entry: a0=rc5unitwork structure:
+	; MC680x0 RC5 key checking function
+	; for distributed.net RC5-64 clients.
+	;
+	; Dual key, rolled loops, MC68020/MC68030 optimised
+	;
+	; Written by John Girvin <girv@girvnet.freeserve.co.uk>
+	;
+	; Entry: a0=rc5unitwork structure:
 	;	 0(a0) = plain.hi	- plaintext
 	;	 4(a0) = plain.lo
 	;	 8(a0) = cypher.hi	- cyphertext
 	;	12(a0) = cypher.lo
 	;	16(a0) = L0.hi	- key
 	;	20(a0) = L0.lo
-	;       d0=number of iterations to run for
+	;        d0=number of iterations to run for
 	;
-	;Exit:  d0=return code
 
-_rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
+_rc5_unit_func_020_030:
+	movem.l	d2-7/a2-6,-(a7)
 
 	move.l	d0,-(a7)	;Save initial key iterations counter
 	move.l	#Q,a4	;a4=constant (P+nQ increment)
 	move.l	16(a0),d2	;d2=L0.hi=L1a
-	lea	-212(a7),a1	;a1=Sx[] storage
+	lea	-220(a7),a7	;Reserve space off stack for temporary storage
 	move.l	20(a0),d1	;d1=L0.lo=L0a and L0b for a while
 	move.l	d0,a6	;Keep loop counter in a6
+	lea	8(a7),a1	;a1=Sx[] storage
 
 	move.l	#P0QR3,d0	;Calculate initial L0x and Ax
 	add.l	d0,d1
 	ror.l	#3,d1
-	move.l	d1,-216(a7)
+	move.l	d1,4(a7)	;L0x
 
 	add.l	#P+Q,d0
 	add.l	d1,d0
 	rol.l	#3,d0
-	move.l	d0,-220(a7)
+	move.l	d0,(a7)	;Ax
 
-	RUF_CODEALIGN
-.ruf_mainloop:
+	RUF_ALIGN	_rc5_unit_func_020_030,d1
+ruf020_mainloop:
  	;---- Round 1 of key expansion ----
 	;A : d0 d4
 	;L0: d1 d5
 	;L1: d2 d6
 
 	move.l	a1,a2	;a2=Sa[] array
-	move.l	-216(a7),d1	;d1=L0x
+	move.l	4(a7),d1	;d1=L0x
 	move.l	#P0QR3,d7	;d7=Sx[00]
 	move.l	d2,d6	;d6=L1b=L1a
 	move.l	d7,(a2)+	;Sa[00]=Aa (first round special case)
@@ -74,7 +67,7 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 	add.l	#1<<24,d6	;d6=L1b=L1a+1<<24
 	move.l	d7,(a3)+	;Sb[00]=Ab (first round special case)
 	move.l	#P+Q+Q,a5	;a5=Initial P+nQ
-	move.l	-220(a7),d0	;d0=Ax
+	move.l	(a7),d0	;d0=Ax
 	moveq	#12-1,d7	;d7=loop counter
 	move.l	d0,d4	;Ab=Aa
 	move.l	d0,(a2)+	;Sa[01]=Aa
@@ -87,8 +80,8 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 	rol.l	d3,d2	;L1a=L1a<<<(Aa+L0a)
 	rol.l	d3,d6	;L1b=L1b<<<(Ab+L0b)
 
-	RUF_CODEALIGN
-.ruf_r1loop:	add.l	a5,d0	;d0=Aa=Aa+P+nQ
+	RUF_ALIGN	_rc5_unit_func_020_030,d3
+ruf020_r1loop:	add.l	a5,d0	;d0=Aa=Aa+P+nQ
 	add.l	a5,d4	;d4=Ab=Ab+P+nQ
 	add.l	d2,d0	;d0=Aa=Aa+P+nQ+L1a
 	add.l	d6,d4	;d4=Ab=Ab+P+nQ+L1b
@@ -119,21 +112,20 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 	rol.l	d3,d2	;d2=L1a=L1a<<<(Aa+L0a)
 	add.l	a4,a5	;Update P+nQ
 	move.l	d5,d3	;d3=L0b
-
 	add.l	d4,d3	;d3=Ab+L0b
 	add.l	d3,d6	;d6=L1b=L1b+Ab+L0b
 	rol.l	d3,d6	;d6=L1b=L1b<<<(Ab+L0b)
 
-	dbf	d7,.ruf_r1loop
+	dbf	d7,ruf020_r1loop
 
 	;---- Round 2 of key expansion ----
 
-	move.l	a1,a2	;a2=SArray
+	move.l	a1,a2	;a2=Sa[] array
 	moveq	#13-1,d7	;d7=loop counter
 	lea	104(a1),a3	;a3=Sb[] array
 
-	RUF_CODEALIGN
-.ruf_r2loop:	add.l	(a2),d0	;Aa=Aa+Sa[n]
+	RUF_ALIGN	_rc5_unit_func_020_030,d3
+ruf020_r2loop:	add.l	(a2),d0	;Aa=Aa+Sa[n]
 	add.l	d6,d4	;Ab=Ab+L1b
 	add.l	d2,d0	;Aa=Aa+Sa[n]+L1a
 	add.l	(a3),d4	;Ab=Ab+Sb[n]+L1b
@@ -168,53 +160,52 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 	add.l	d3,d6	;L1b=L1b+Ab+L0b
 	rol.l	d3,d6	;L1b=L1b<<<(Ab+L0b)
 
-	dbf	d7,.ruf_r2loop
+	dbf	d7,ruf020_r2loop
 
 	;---- Combined round 3 of key expansion and encryption round ----
 
 	move.l	d4,a2	;a2=Ab
 	move.l	d5,a3	;a3=L0b
 
-	bsr.s	.ruf_round3	;Do round 3 for 'a' key
-	beq	.ruf_gotit1	;Skip if we found a key!
+	bsr.s	ruf020_round3	;Do round 3 for 'a' key
 
 	move.l	a2,d0	;d0=Ab
 	move.l	a3,d1	;d1=L0b
 	move.l	d6,d2	;d2=L1b
 	addq.l	#8,a1	;a1=Sb[]
-	
-	bsr.s	.ruf_round3	;Do round 3 for 'b' key
-	beq	.ruf_gotit2
 
-;----------	;Mangle-increment current key by PIPELINE_COUNT
+	bsr.s	ruf020_round3	;Do round 3 for 'b' key
+
+	;---- Mangle-increment current key by PIPELINE_COUNT ----
 
 	addq.b	#2,16(a0)	;L1=L1+PIPELINE_COUNT
-	bcs.s	.ruf_l1carry	;Skip if carry
+	bcs.s	ruf020_l1_lsbcarry	;Skip if carry (every 256 keys)
 
-.ruf_midone: move.l	16(a0),d2	;d2=updated L1
+ruf020_midone: move.l	16(a0),d2	;d2=updated L1
 	subq.l	#1,a6	;Update loop counter
-	lea	-212(a7),a1	;a1=Sx[] storage for next iteration
+	lea	8(a7),a1	;a1=Sx[] storage for next iteration
 
 	move.l	a6,d0	;Loop back for next key
-	bpl	.ruf_mainloop
+	bne	ruf020_mainloop
 
-;---------- 	;Didnt find key :(
-
-	move.l	(a7)+,d0	;Return iterations*pipeline_count
-	add.l	d0,d0
-	movem.l	(a7)+,d2-7/a2-6
-	rts
+	bra	ruf020_didntgetit	;Didnt find key :(
 
 ;--------------------
 
-	RUF_CODEALIGN
-.ruf_l1carry:
-	addq.b	#1,17(a0)	;Every 256^1
-	bcc.s	.ruf_midone
-	addq.b	#1,18(a0)	;Every 256^2
-	bcc.s	.ruf_midone
-	addq.b	#1,19(a0)	;Every 256^3
-	bcc.s	.ruf_midone
+	CNOP	0,8
+ruf020_l1_lsbcarry:
+	; LSB of L1 has wrapped, so increment other bytes as well
+	; Reached every 256 keys
+
+	addq.b	#1,17(a0)
+	bcc.s	ruf020_midone
+	addq.b	#1,18(a0)	;Every 256^2 = 2^16
+	bcc.s	ruf020_midone
+	addq.b	#1,19(a0)	;Every 256^3 = 2^24
+	bcc.s	ruf020_midone
+
+	;L1 wrapped, so increment L0 as well
+	;Reached every 2^32 keys
 
 	move.l	20(a0),d1	;d1=L0
 	rol.w	#8,d1	;L0:abdc
@@ -226,29 +217,34 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 	rol.w	#8,d1	;L0:abcd
 	move.l	d1,20(a0)	;Store updated L0
 
-	move.l	#P0QR3,d0	;Recalculate initial L0x and Ax
+	; L0 has changed so recalculate "constant" L0x and Ax
+
+	move.l	#P0QR3,d0
 	add.l	d0,d1
 	ror.l	#3,d1
-	move.l	d1,-216(a7)
+	move.l	d1,4(a7)	;L0x
 
 	add.l	#P+Q,d0
 	add.l	d1,d0
 	rol.l	#3,d0
-	move.l	d0,-220(a7)
+	move.l	d0,(a7)	;Ax
 
-	bra.s	.ruf_midone
+	bra	ruf020_midone
 
 ;--------------------
 
-	RUF_CODEALIGN
-.ruf_round3:	;RC5 round 3
+	CNOP	0,8
+ruf020_round3:	;RC5-64 keycheck round 3
 	;Entry:	a0=RC5UnitWork structure
 	;	a1=S array to use
 	;	d0=A
 	;	d1=L0
 	;	d2=L1
-	;Exit:	EQ if key found, NE otherwise
-	;	d1-5/d7 corrupt, a1=96(a1)
+	;Exit:	If key is found, this does not return.
+	;	Otherwise:
+	;	  key was not found
+	;	  d1-5/d7 corrupt
+	;	  a1=96(a1)
 
 	add.l	(a1)+,d0	;d0=A=Sa[00]
 	move.l	d2,d3	;d3=L1
@@ -272,8 +268,8 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 
 	rol.l	d3,d2	;d2=L1=(L1+A+L0)<<<(A+L0)
 
-	RUF_CODEALIGN
-.ruf_r3_r3loop: 
+	RUF_ALIGN	_rc5_unit_func_020_030,d3
+ruf020_r3_r3loop:
 	add.l	(a1)+,d0	;d0=A=A+Sn
 	eor.l	d5,d4	;d4=eA=eA^eB
 	add.l	d2,d0	;d0=A=A+Sn+L1
@@ -298,54 +294,69 @@ _rc5_unit_func_000_030_asm:	movem.l	d2-7/a2-6,-(a7)
 	add.l	d3,d2	;d2=L1=L1+A+L0
 	rol.l	d3,d2	;d2=L1=L1<<<(A+L0)
 
-	dbf	d7,.ruf_r3_r3loop
+	dbf	d7,ruf020_r3_r3loop
 
-	add.l	(a1),d0	;d0=A=A+S24
+	add.l	(a1),d0	;d0=A=A+S[24]
 	eor.l	d5,d4	;d4=eA=eA^eB
-	add.l	d2,d0	;d0=A=A+S24+L1
+	add.l	d2,d0	;d0=A=A+S[24]+L1
 	rol.l	d5,d4	;d4=eA=((eA^eB)<<<eB)
-	rol.l	#3,d0	;d0=A=(A+S24+L1)<<<3
+	rol.l	#3,d0	;d0=A=(A+S[24]+L1)<<<3
 	move.l	12(a0),d3	;d3=cypher.lo
 	add.l	d0,d4	;d4=eA=((eA^eB)<<<eB)+A
 	cmp.l	d3,d4	;d4=eA=cypher.lo?
-	beq.s	.ruf_r3_tryeb
+	beq.s	ruf020_r3_checkhi	;Skip if yes (low 32 bits match)
 
-	rts		 ;Not the right key
+	rts		 ;Not the right key, return NE
 
-	RUF_CODEALIGN
-.ruf_r3_tryeb:
+ruf020_r3_checkhi:
+	; Low 32 bits match, so generate & check high 32 bits
+	; Reached every 2^32 keys
+
 	move.l	d0,d3	;d3=A
 	add.l	d2,d3	;d3=A+L1
 	add.l	d3,d1	;d1=L0=L0+A+L1
-	add.l	4(a1),d0	;d0=A=A+S25
+	add.l	4(a1),d0	;d0=A=A+S[25]
 	rol.l	d3,d1	;d1=L0=(L0+A+L1)<<<(A+L1)
 	eor.l	d4,d5	;d5=eB=eB^eA
-	add.l	d1,d0	;d0=A=A+S25+L0
+	add.l	d1,d0	;d0=A=A+S[25]+L0
 	rol.l	d4,d5	;d5=eB=((eB^eA)<<<eA)
-	rol.l	#3,d0	;d0=A=(A+S25+L0)<<<3
+	rol.l	#3,d0	;d0=A=(A+S[25]+L0)<<<3
 	move.l	8(a0),d3	;d3=cypher.hi
 	add.l	d0,d5	;d5=eB=((eB^eA)<<<eA)+A
 	cmp.l	d3,d5	;eB=cypher.hi?
+	beq.s	ruf020_r3_found_key	;Skip if yes (key found!)
 
-	rts		;EQ if right key, NE otherwise
+	rts		 ;Not the right key
 
-;--------------------
+ruf020_r3_found_key:
+	; We found a key!
+	; a0=RC5UnitWork structure
+	; a1=Sx[] array + 96
 
-.ruf_gotit1:	;Key found on pipeline 1!
+	sub.l	a7,a1	;a1<->a7 difference used to determine which pipeline the key was found on
+			;-96(a1) = &Sa[00] or &Sb[00] depending on pipeline
+
+	lea	4+220(a7),a7	;Discard ruf020_round3 return address and temporary storage
 	move.l	(a7)+,d0	;d0=initial iteration count
-	sub.l	a6,d0	;Return number of keys checked
-	add.l	d0,d0	; = loops*pipeline_count
+	sub.l	a6,d0	;d0=number of iterations performed
+	add.l	d0,d0	;d0=number of keys checked
+
+	cmp.l	#12+96+104,a1	;12+96+104 = &Sb[00]
+	bcs.s	ruf020_r3_notpipe2
+
+	addq.b	#1,16(a0)	;Adjust L0.hi and nkeys for pipeline 2
+	addq.l	#1,d0
+ruf020_r3_notpipe2:
 
 	movem.l	(a7)+,d2-7/a2-6
 	rts
 
-.ruf_gotit2:	;Key found on pipeline 2!
-	move.l	(a7)+,d0	;d0=initial iteration count
-	sub.l	a6,d0	;Return number of keys checked
-	add.l	d0,d0	; = loops*pipeline_count+1
-	addq.b	#1,16(a0)	;Adjust L0.hi for pipeline 2
-	addq.l	#1,d0
+;--------------------
 
+ruf020_didntgetit:	;Didn't find a key this time
+	lea	220(a7),a7	;Discard temporary storage
+	move.l	(a7)+,d0	;Return number of keys checked
+	add.l	d0,d0	; = loops*pipeline_count
 	movem.l	(a7)+,d2-7/a2-6
 	rts
 
---
+++ rc5/68k/rc5-0x0-jg.i
@@ -0,0 +1,47 @@
+;--------------------
+;@(#)$Id: rc5-0x0-jg.i,v 1.2 2000/07/11 01:39:55 mfeiri Exp $
+
+	RSRESET
+	;Offsets into ruf???_vars
+
+RUFV_AX:	rs.l	1	;A       \-- only change when (if?)
+RUFV_L0X:	rs.l	1	;L0       |  L0 changes
+RUFV_L0XAX:	rs.l	1	;L0+A     |
+RUFV_AXP2Q:	rs.l	1	;A+P+2Q  /
+RUFV_SIZE:	equ	__RS
+
+;--------------------
+
+	;Define P, Q and related constants
+
+P:	equ	$b7e15163	;P
+Q:	equ	$9e3779b9	;Q
+P0QR3:	equ	$bf0a8b1d	;(0*Q + P) <<< 3
+PR3Q:	equ	$15235639	;P0QR3+P+Q
+P2Q:	equ	$f45044d5	;(2*Q + P)
+
+;--------------------
+
+	;
+	; Ensure next instruction is aligned on
+	; quadword (8 byte) boundary
+	;
+	; \1 is base label to align respective to
+	; \2 is scratch data register, will be = 0
+	;
+
+RUF_ALIGN:	MACRO
+	;BEGIN RUF_ALIGN
+	  IFNE NARG-2
+	    FAIL "RUF_ALIGN: usage"
+	    MEXIT
+	  ENDC
+\1align\@:	  equ	*-\1
+	  IFNE	\1align\@&7
+	    moveq	#0,\2
+	    RUF_ALIGN \1,\2
+	  ENDC
+	;END RUF_ALIGN
+	ENDM
+
+;--------------------
