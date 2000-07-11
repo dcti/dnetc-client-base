47f5b754fe3ae01bb134b88176861da45a39e852
Michael Feiri <mfeiri@distributed.net>
2000-07-11 08:07:40 +0000

    Cleanup.

--- common/xmlserve.h
+++ common/xmlserve.h
@@ -3,22 +3,13 @@
 // Any other distribution or use of this source violates copyright.
 //
 
-
 #include "minihttp.h"
 
-#include <time.h>
-#include <string.h>
-#include "client.h"
-
 #define MAX_CONNECTIONS   5
+#define LISTENADDRESS     0
+#define LISTENPORT        81
 #define CLIENTIDLETIMEOUT 30
 
 bool ProcessClientPacket(MiniHttpDaemonConnection *httpcon, Client *client);
 
 void __fdsetsockadd(SOCKET sock, fd_set *setname, int *sockmax, int *writecnt);
-
-void initxmlserve(int address, int port);
-
-void poll();
-
-void deinitxmlserve();
