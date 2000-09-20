4ef9c2488424dd855192f4a0762898fd98bdd246
Cyrus Patel <cyp@distributed.net>
2000-09-20 18:25:18 +0000

    flags for BufferCheckIfUpdateNeeded()

--- common/buffupd.h
+++ common/buffupd.h
@@ -5,16 +5,23 @@
  * Any other distribution or use of this source violates copyright.
 */ 
 #ifndef __BUFFUPD_H__
-#define __BUFFUPD_H__ "@(#)$Id: buffupd.h,v 1.5.2.2 2000/01/08 23:18:02 cyp Exp $"
+#define __BUFFUPD_H__ "@(#)$Id: buffupd.h,v 1.5.2.3 2000/09/20 18:25:18 cyp Exp $"
 
-#define BUFFERUPDATE_FETCH 0x01
-#define BUFFERUPDATE_FLUSH 0x02
+// pass flags ORd with BUFFERUPDATE_FETCH/*_FLUSH. 
+// if interactive, prints "Input buffer full. No fetch required" etc.
+// returns updated flags or < 0 if failed. (offlinemode!=0/NetOpen() etc)
 
-#include "client.h" /* forward reference */
+// BUFFUPDCHECK_* bits are for use with BufferCheckIfUpdateNeeded().
+// For a longer description of how they are used refer to buffbase.cpp
+
+#define BUFFERUPDATE_FETCH       0x01 /* do or check_need_for fetch */
+#define BUFFERUPDATE_FLUSH       0x02 /* do or check_need_for flush */
+#define BUFFUPDCHECK_TOPOFF      0x20 /* fill_even_if_not_completely_empty */
+#define BUFFUPDCHECK_EITHER      0x40 /* either true sets both true */
+#define BUFFERUPDATE_LASTBIT     0x40 /* first free is _LASTBIT<<1 */
+
 int BufferUpdate( Client *client, int updatereq_flags, int interactive );
 
-    // pass flags ORd with BUFFERUPDATE_FETCH/*_FLUSH. 
-    // if interactive, prints "Input buffer full. No fetch required" etc.
-    // returns updated flags or < 0 if failed. (offlinemode!=0/NetOpen() etc)
+int BufferCheckIfUpdateNeeded(Client *client, int contestid, int upd_flags);
 
 #endif /* __BUFFUPD_H__ */
