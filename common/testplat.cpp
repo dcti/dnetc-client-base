0230200ba3ed5ebbf6cda9a93268e97e3c927cfd
Cyrus Patel <cyp@distributed.net>
2001-01-28 00:57:05 +0000

    dir sep fixes

--- common/testplat.cpp
+++ common/testplat.cpp
@@ -4,23 +4,19 @@
  * Any other distribution or use of this source violates copyright.
  *
  * This file contains functions used from ./configure
- * Specify 'cpu', 'os', 'intsizes' or 'build_dependancies' as argument.
+ * Specify 'build_dependancies' as argument 
+ * (which is all this needs to do anymore)
+ *
+ * $Id: testplat.cpp,v 1.4.2.4 2001/01/28 00:57:05 cyp Exp $
 */ 
-const char *testplat_cpp(void) { 
-return "@(#)$Id: testplat.cpp,v 1.4.2.3 2001/01/14 00:23:48 andreasb Exp $"; } 
-
-static const char *include_dirs[] = { "common", "rc5", "des", "ogr", "ogr/ansi" };
-
 #include <stdio.h>
 #include <string.h>
-#define IGNOREUNKNOWNCPUOS
-#include "cputypes.h"
 
-#if (CLIENT_OS == OS_RISCOS)
-#define INCLUDE_SUBDIR_SEP "."
-#else
-#define INCLUDE_SUBDIR_SEP "/"
-#endif
+static const char *include_dirs[] = { "common", 
+                                      "rc5", 
+                                      "des", 
+                                      "ogr/ansi"
+                                     };
 
 static int fileexists( const char *filename ) /* not all plats have unistd.h */
 {
@@ -29,20 +25,49 @@ static int fileexists( const char *filename ) /* not all plats have unistd.h */
   return ( file != NULL );
 }
 
+static void __fixup_pathspec_for_locate(char *foundbuf)
+{
+  foundbuf = foundbuf;
+  #if defined(__riscos)
+  unsigned int n;
+  for (n=0; foundbuf[n]; n++)
+  {
+    if (foundbuf[n] == '/') /* dirsep */
+      foundbuf[n] = '.';
+    else if (foundbuf[n] == '.') /* suffix */
+      foundbuf[n] = '/';  
+  }
+  #endif
+}
+
+static void __fixup_pathspec_for_makefile(char *foundbuf)
+{
+  foundbuf = foundbuf;
+  #if defined(__riscos)
+  unsigned int n;
+  for (n=0; foundbuf[n]; n++)
+  {
+    if (foundbuf[n] == '.')
+      foundbuf[n] = '/';
+    else if (foundbuf[n] == '/')
+      foundbuf[n] = '.';  
+  }
+  #endif
+}
+
 static unsigned int build_dependancies( char *cppname ) /* ${TARGETSRC} */
 {
   char linebuf[512], pathbuf[64], foundbuf[64];
   char *p, *r;
   unsigned int l, count = 0;
   FILE *file = fopen( cppname, "r" );
-  const char *subdir_sep = INCLUDE_SUBDIR_SEP;
+
+  //fprintf(stderr,"cppname='%s', file=%p\n",cppname,file);
 
   if ( file )
   {
     strcpy( pathbuf, cppname );
-    p = strrchr( pathbuf, *subdir_sep );
-    r = strrchr( pathbuf, '\\' );
-    if ( r > p ) p = r;
+    p = strrchr( pathbuf, '/' ); /* input specifiers are always unix paths */
     if ( p == NULL ) 
       pathbuf[0]=0;
     else *(++p)=0;
@@ -58,36 +83,45 @@ static unsigned int build_dependancies( char *cppname ) /* ${TARGETSRC} */
         p+=8;
         while ( *p == ' ' || *p == '\t' )
           p++;
-        if ( *p == '\"' || *p == '<' )
+        if ( *p == '\"' /* || *p == '<' */ )
         {
           r = linebuf;
           foundbuf[0]= ((*p == '<') ? ('>') : ('\"'));
           while (*(++p) != foundbuf[0] )
             *r++ = *p;
           *r = 0;
+          //fprintf(stderr, "'#include %s'\n", linebuf);
           if (linebuf[0])
           {
             strcpy( foundbuf, linebuf );
-            if ( strchr( linebuf, *subdir_sep ) == NULL )
+            /* include specifiers are always unix form */
+            if ( strchr( linebuf, '/' ) == NULL )
             {
               strcpy( foundbuf, pathbuf );
               strcat( foundbuf, linebuf );
               l = 0;
-              while ( !fileexists( foundbuf ) )
+              //fprintf(stderr, "%d) '%s'\n", l, foundbuf);
+              for (;;)
               {
+                __fixup_pathspec_for_locate(foundbuf);
+                if (fileexists( foundbuf ))
+                  break;
                 if (l >= (sizeof( include_dirs )/sizeof( include_dirs[0] )))
                   break;
                 strcpy( foundbuf, include_dirs[l] );
-                strcat( foundbuf, subdir_sep );
+                strcat( foundbuf, "/" ); /* always unix */
                 strcat( foundbuf, linebuf );
                 l++;
+                //fprintf(stderr, "%d) '%s'\n", l, foundbuf);
               }
             }
-            if ( fileexists( foundbuf ) )
+            if ( !fileexists( foundbuf ) )
             {
-              printf( "%s%s", ((count!=0)?(" "):("")), foundbuf );
-              count++;
+              strcpy(foundbuf,linebuf);
             }
+            __fixup_pathspec_for_makefile(foundbuf);
+            printf( "%s%s", ((count!=0)?(" "):("")), foundbuf );
+            count++;
           }
         }
       }
@@ -102,16 +136,10 @@ int main(int argc, char *argv[])
 {
   if (argc < 2)
   {
-    printf("Specify 'cpu', 'os', 'intsizes' or 'build_dependancies' as argument.\n");
+    fprintf(stderr,"Specify 'build_dependancies' as argument.\n");
     return -1;
   }
-  if (strcmp(argv[1], "cpu") == 0)
-    printf("%i\n", (int) CLIENT_CPU);
-  else if (strcmp(argv[1], "os") == 0)
-    printf("%i\n", (int) CLIENT_OS);
-  else if (strcmp(argv[1], "intsizes") == 0)
-    printf("%i%i%i\n", (int) sizeof(long), (int) sizeof(int), (int) sizeof(short));
-  else if (strcmp(argv[1], "build_dependancies" )== 0)
+  if (strcmp(argv[1], "build_dependancies" )== 0)
     build_dependancies( argv[2] );    
   return 0;
 }
