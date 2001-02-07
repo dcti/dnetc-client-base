9db72d68a30463741f7f733de70912235534ff96
Cyrus Patel <cyp@distributed.net>
2001-02-07 02:14:07 +0000

    ia64: let itanium use core #5 (RISC-rotate II) rather than P6 core. Rate is miserable though.

--- common/cpucheck.cpp
+++ common/cpucheck.cpp
@@ -9,7 +9,7 @@
  *
 */
 const char *cpucheck_cpp(void) {
-return "@(#)$Id: cpucheck.cpp,v 1.79.2.65 2001/02/03 21:37:54 cyp Exp $"; }
+return "@(#)$Id: cpucheck.cpp,v 1.79.2.66 2001/02/07 02:14:07 cyp Exp $"; }
 
 #include "cputypes.h"
 #include "baseincs.h"  // for platform specific header files
@@ -901,7 +901,7 @@ long __GetRawProcessorID(const char **cpuname, int whattoret = 0 )
           {  0x0680,  0x102, "Pentium III" },
           {  0x0690,  0x102, "Pentium III/Timna" }, /* 6547:0692 */
           {  0x06A0,  0x102, "Pentium III" }, //0.18 um w/ 1/2MB on-die L2
-          {  0x0700,  0x102, "Itanium" }, /* 6547:0701 */
+          {  0x0700,  0x105, "Itanium" }, /* 6547:0701. #5 == RG RISC-rotate II */
           {  0x0F00,  0x102, "Pentium 4" },
           {  0x0000,     -1, NULL }
           }; internalxref = &intelxref[0];
