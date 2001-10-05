8ff8d4e50df897484f8ec24f56f936bb372dd2d9
Michael Feiri <mfeiri@distributed.net>
2001-10-05 22:35:43 +0000

    Wrapper for the new crunch-vec-7450, based on the existing wrapper file for crunch-vec.

---
+++ rc5/ppc/rc5_unit_func_vec_7450.cpp
@@ -0,0 +1,29 @@
+#include "problem.h"
+
+extern "C" {
+int crunch_vec_7450( RC5UnitWork *work, unsigned long iterations ); /* asm */
+int crunch_allitnil( RC5UnitWork *, unsigned long iterations); /* asm */
+s32 rc5_unit_func_vec_7450( RC5UnitWork *, u32 *timeslice, void *); /* this */
+u32 rc5_unit_func_vec_7450_compat( RC5UnitWork *, u32 iterations ); /* this */
+}
+
+u32 rc5_unit_func_vec_7450_compat( RC5UnitWork *work, u32 iterations )
+{
+  return crunch_vec_7450( work, iterations );
+}  
+
+s32 rc5_unit_func_vec_7450( RC5UnitWork *work, u32 *timeslice, void *savstate)
+{
+  u32 kiter = rc5_unit_func_vec_7450_compat( work, *timeslice );
+  savstate = savstate; /* unused. shaddup compiler */
+
+  if (*timeslice == kiter) 
+     return RESULT_WORKING;
+  if (*timeslice > kiter) {
+    *timeslice = kiter;
+    return RESULT_FOUND;
+  }
+  /* shouldn't happen with this core */
+  *timeslice = kiter;
+  return -1; /* error */
+}
