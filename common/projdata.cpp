4c24abd04fbd470f6f1655e444f391d1bbc2b0c2
Andreas Beckmann <andreasb@distributed.net>
2002-10-06 19:25:44 +0000

    new files projdata.*; should be shared with proxy source some day split project ID definitions out of problem.h add PROJECTSTATE_* and PROJECTFLAG_* definitions basic functions: ProblemGet Flags/Name/UnitName/IniSectionName/Filename

---
+++ common/projdata.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright distributed.net 1997-2002 - All Rights Reserved
+ * For use in distributed.net projects only.
+ * Any other distribution or use of this source violates copyright.
+*/
+const char *projdata_cpp(void) {
+return "@(#)$Id: projdata.cpp,v 1.1 2002/10/06 19:25:44 andreasb Exp $"; }
+
+#include "cputypes.h"
+#include "projdata.h"
+
+
+static const struct ProjectInfo_t
+{
+  int ProjectID;
+  const char *ProjectName;
+  const char *FileExtension;    /* length=3, unique */
+  const char *IniSectionName;
+  const char *UnitName;
+  unsigned int Iter2_call_me_something_better_than__Key__Factor; /* by how much must iterations/keysdone
+                        be multiplied to get the number of keys checked. */
+} ProjectInfoVec[] =
+// obsolete projects may be omitted
+{
+//  ProjectID             Name      filext ini sect. unit
+  { RC5,                  "RC5",    "rc5", "rc5",    "keys",  1 },
+  { DES,                  "DES",    "des", "des",    "keys",  2 },
+  { OGR,                  "OGR",    "ogr", "ogr",    "nodes", 1 },
+  { CSC,                  "CSC",    "csc", "csc",    "keys",  1 },
+  { OGR_NEXTGEN_SOMEDAY,  "OGR_NG", "og2", "ogr_ng", "nodes", 1 },
+  { RC5_72,               "RC5-72", "r72", "rc5-72", "keys",  1 },
+  { -1,                   NULL,     NULL,  NULL,     NULL,    0 }
+#if (PROJECT_COUNT != 6)
+  #error PROJECT_NOT_HANDLED("ProjectInfo[]: static initializer was last updated for PROJECT_COUNT == 6")
+#endif
+};
+
+/* ----------------------------------------------------------------------- */
+
+static const struct ProjectInfo_t *__internalGetProjectInfoVector( int projectid )
+{
+  for (int i = 0; ProjectInfoVec[i].ProjectName != NULL; i++)
+  {
+    if (ProjectInfoVec[i].ProjectID == projectid)
+      return (&ProjectInfoVec[i]);
+  }
+  return ((const struct ProjectInfo_t *)(NULL));
+}
+
+// --------------------------------------------------------------------------
+
+/* returns PROJECT_UNSUPPORTED (0) if client has no core for projectid,
+   PROJECT_OK | (list of flags for projectid) otherwise */
+u32 ProjectGetFlags(int projectid)
+{
+  #if defined(HAVE_RC5_64_CORES)
+    #define PROJECT_OK_RC5_64 PROJECT_OK
+  #else
+    #define PROJECT_OK_RC5_64 PROJECT_UNSUPPORTED
+  #endif
+  #if defined(HAVE_DES_CORES)
+    #define PROJECT_OK_DES PROJECT_OK
+  #else
+    #define PROJECT_OK_DES PROJECT_UNSUPPORTED
+  #endif
+  #if defined(HAVE_OGR_CORES)
+    #define PROJECT_OK_OGR PROJECT_OK
+  #else
+    #define PROJECT_OK_OGR PROJECT_UNSUPPORTED
+  #endif
+  #if defined(HAVE_CSC_CORES)
+    #define PROJECT_OK_CSC PROJECT_OK
+  #else
+    #define PROJECT_OK_CSC PROJECT_UNSUPPORTED
+  #endif
+  #if defined(HAVE_RC5_72_CORES)
+    #define PROJECT_OK_RC5_72 PROJECT_OK
+  #else
+    #define PROJECT_OK_RC5_72 PROJECT_UNSUPPORTED
+  #endif
+  static const u32 projectflags[PROJECT_COUNT] = {
+    /* RC5    */ PROJECT_OK_RC5_64
+        | PROJECTFLAG_TIME_THRESHOLD | PROJECTFLAG_PREFERRED_BLOCKSIZE
+        | PROJECTFLAG_RANDOM_BLOCKS,
+    /* DES    */ PROJECT_OK_DES
+        | PROJECTFLAG_TIME_THRESHOLD | PROJECTFLAG_PREFERRED_BLOCKSIZE,
+    /* OGR    */ PROJECT_OK_OGR,
+    /* CSC    */ PROJECT_OK_CSC
+        | PROJECTFLAG_TIME_THRESHOLD | PROJECTFLAG_PREFERRED_BLOCKSIZE,
+    /* OGR_NG */ PROJECT_UNSUPPORTED,
+    /* RC5_72 */ PROJECT_OK_RC5_72
+        | PROJECTFLAG_TIME_THRESHOLD /* not yet! | PROJECTFLAG_PREFERRED_BLOCKSIZE */
+        | PROJECTFLAG_RANDOM_BLOCKS,
+  };
+  #undef PROJECT_OK_RC5_64
+  #undef PROJECT_OK_DES
+  #undef PROJECT_OK_OGR
+  #undef PROJECT_OK_CSC
+  #undef PROJECT_OK_RC5_72
+
+  if ( 0 <= projectid && projectid < PROJECT_COUNT )
+    if ((projectflags[projectid] & PROJECT_OK) == PROJECT_OK)
+      return projectflags[projectid];
+
+  return PROJECT_UNSUPPORTED;
+}
+
+// --------------------------------------------------------------------------
+
+const char *ProjectGetName(int projectid)
+{
+  const struct ProjectInfo_t *ProjectInfo =
+                    __internalGetProjectInfoVector( projectid );
+  if (ProjectInfo && ProjectInfo->ProjectName && *ProjectInfo->ProjectName)
+    return ProjectInfo->ProjectName;
+  return ((const char *)("???"));
+}
+
+const char *ProjectGetIniSectionName(int projectid)
+{
+  const struct ProjectInfo_t *ProjectInfo =
+                    __internalGetProjectInfoVector( projectid );
+  if (ProjectInfo && ProjectInfo->IniSectionName && *ProjectInfo->IniSectionName)
+    return ProjectInfo->IniSectionName;
+  return ((const char *)("unknown-project"));
+}
+
+const char *ProjectGetUnitName(int projectid)
+{
+  const struct ProjectInfo_t *ProjectInfo =
+                    __internalGetProjectInfoVector( projectid );
+  if (ProjectInfo && ProjectInfo->UnitName && *ProjectInfo->UnitName)
+    return ProjectInfo->UnitName;
+  return ((const char *)("units"));
+}
+
+const char *ProjectGetFileExtension(int projectid)
+{
+  const struct ProjectInfo_t *ProjectInfo =
+                    __internalGetProjectInfoVector( projectid );
+  if (ProjectInfo && ProjectInfo->FileExtension && *ProjectInfo->FileExtension)
+    return ProjectInfo->FileExtension;
+  return ((const char *)("zzz"));
+}
+
+// --------------------------------------------------------------------------
---
+++ common/projdata.h
@@ -0,0 +1,59 @@
+/* -*-C++-*-
+ *
+ * Copyright distributed.net 1997-2002 - All Rights Reserved
+ * For use in distributed.net projects only.
+ * Any other distribution or use of this source violates copyright.
+ *
+ * This file should be shared between client and proxynet some day ...
+ */
+#ifndef __PROJDATA_H__
+#define __PROJDATA_H__ "@(#)$Id: projdata.h,v 1.1 2002/10/06 19:25:44 andreasb Exp $"
+
+#include "cputypes.h"   // u32 ...
+
+/* ------------------------------------------------------------------------- */
+
+enum {
+  RC5, // RC5-64, http://www.rsa.com/rsalabs/97challenge/
+  DES, // http://www.rsa.com/rsalabs/des3/index.html
+  OGR, // http://members.aol.com/golomb20/
+  CSC, // http://www.cie-signaux.fr/security/index.htm
+  OGR_NEXTGEN_SOMEDAY,
+  RC5_72 // http://www.rsasecurity.com/rsalabs/challenges/secretkey/
+  // PROJECT_NOT_HANDLED("create your project id here and adjust count")
+};
+#define PROJECT_COUNT       6  /* RC5,DES,OGR,CSC,OGR_NEXTGEN,RC5_72 */
+#define CONTEST_COUNT       PROJECT_COUNT
+
+#define MAX_PROJECT_NAME_LEN 6 /* "RC5-72" */
+
+/* ------------------------------------------------------------------------- */
+  
+/* Project Flags - static, defined at compile time */
+
+#define PROJECT_UNSUPPORTED                       0x00000000
+#define PROJECT_OK                                0x00000001
+#define PROJECTFLAG_TIME_THRESHOLD                0x00000002
+#define PROJECTFLAG_PREFERRED_BLOCKSIZE           0x00000004
+#define PROJECTFLAG_RANDOM_BLOCKS                 0x00000008
+
+/* Project States - may change at runtime */
+
+#define PROJECTSTATE_USER_DISABLED                0x00000001
+#define PROJECTSTATE_SUSPENDED                    0x00000002
+#define PROJECTSTATE_CLOSED                       0x00000004
+
+/* ------------------------------------------------------------------------- */
+
+/* returns PROJECT_UNSUPPORTED (0) if client has no core for projectid,
+   PROJECT_OK | (list of flags for projectid) otherwise */
+u32 ProjectGetFlags(int projectid);
+
+const char *ProjectGetName(int projectid);
+const char *ProjectGetIniSectionName(int projectid);
+const char *ProjectGetUnitName(int projectid);
+const char *ProjectGetFileExtension(int projectid);
+
+/* ------------------------------------------------------------------------- */
+
+#endif /* __PROJDATA_H__ */
