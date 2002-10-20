4652e6c956bbd98564901036befbc6f2f91a5aea
Andreas Beckmann <andreasb@distributed.net>
2002-10-20 20:52:10 +0000

    text section options for OMF

--- rc5-72/x86/rc5-ses.asm
+++ rc5-72/x86/rc5-ses.asm
@@ -4,7 +4,13 @@
 ; For use by distributed.net. I do request that any
 ; changes to this core are also emailed to Samuel@Lightspeed.cx
 
+; $Id: rc5-ses.asm,v 1.4 2002/10/20 20:52:10 andreasb Exp $
+
+%ifdef __OMF__ ; Borland and Watcom compilers/linkers
+[SECTION _TEXT FLAT USE32 align=16 CLASS=CODE]
+%else
 [SECTION .text]
+%endif
 
 ;export the global function
 [GLOBAL _rc5_72_unit_func_ses]
