09fadc0e3e6806fb1061deb5f5261e8979c83f75
Oliver Roberts <oliver@distributed.net>
2003-01-11 13:53:44 +0000

    *** empty log message ***

--- plat/amigaos/guilib/dnetcgui_rev.h
+++ plat/amigaos/guilib/dnetcgui_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	7
-#define DATE		"25.7.2002"
-#define VERS		"dnetcgui 1.7"
-#define VSTRING		"dnetcgui 1.7 (25.7.2002)\r\n"
-#define VERSTAG		"\0$VER: dnetcgui 1.7 (25.7.2002)"
+#define REVISION	8
+#define DATE		"11.1.2003"
+#define VERS		"dnetcgui 1.8"
+#define VSTRING		"dnetcgui 1.8 (11.1.2003)\r\n"
+#define VERSTAG		"\0$VER: dnetcgui 1.8 (11.1.2003)"
