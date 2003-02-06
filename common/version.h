51886d16de2223b16daad968f4c3cc3c00674306
Andreas Beckmann <andreasb@distributed.net>
2003-02-06 23:30:53 +0000

    2.9003-481

--- common/version.h
+++ common/version.h
@@ -25,7 +25,7 @@
  * ---------------------------------------------------------------------
 */
 #ifndef __VERSION_H__
-#define __VERSION_H__ "@(#)$Id: version.h,v 1.76.2.5 2003/01/29 02:52:29 andreasb Exp $"
+#define __VERSION_H__ "@(#)$Id: version.h,v 1.76.2.6 2003/02/06 23:30:53 andreasb Exp $"
 
 /* BETA etc is handled internally/at-runtime by cliident.cpp. */
 /* Do not adjust for BETA here, particularly CLIENT_VERSIONSTRING. */
@@ -38,9 +38,9 @@
 #define CLIENT_CONTEST_HEX     0x5A   /* needed for macos version resource */
 #define CLIENT_BUILD           3
 #define CLIENT_BUILD_HEX       0x03   /* needed for macos version resource */
-#define CLIENT_BUILD_FRAC      480
-#define CLIENT_BUILD_FRAC_HEX  0x01E0 /* needed for macos version resource */
-#define CLIENT_VERSIONSTRING   "2.9003-480"
+#define CLIENT_BUILD_FRAC      481
+#define CLIENT_BUILD_FRAC_HEX  0x01E1 /* needed for macos version resource */
+#define CLIENT_VERSIONSTRING   "2.9003-481"
 
 /* combined version used in packets etc. ... */
 #define CLIENT_VERSION         ( (((u32)(CLIENT_CONTEST))    * 1000000UL) +  \
