0ac958fb369bb9fa4782300b515c66274b3c9a9b
Oliver Roberts <oliver@distributed.net>
2003-03-07 17:20:49 +0000

    fixed up OGROPT #defines for GCC

--- ogr/ppc/ogr-vec.cpp
+++ ogr/ppc/ogr-vec.cpp
@@ -5,7 +5,7 @@
 */
 
 const char *ogr_vec_cpp(void) {
-return "@(#)$Id: ogr-vec.cpp,v 1.3.4.1 2003/02/25 12:30:23 snake Exp $"; }
+return "@(#)$Id: ogr-vec.cpp,v 1.3.4.2 2003/03/07 17:20:49 oliver Exp $"; }
 
 #ifdef __VEC__ /* compiler supports AltiVec */
   #if (__MWERKS__)
@@ -16,6 +16,7 @@ return "@(#)$Id: ogr-vec.cpp,v 1.3.4.1 2003/02/25 12:30:23 snake Exp $"; }
     #define OGROPT_STRENGTH_REDUCE_CHOOSE         1 /* MWC does benefit */
     #define OGROPT_ALTERNATE_CYCLE                2 /* AltiVec support! */
     #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 2 /* use switch_asm   */
+    #define OGROPT_CYCLE_CACHE_ALIGN              0 /* no balignl       */
   #elif (__MRC__)
     #define OGROPT_BITOFLIST_DIRECT_BIT           0 /* 'no' irrelevant  */
     #define OGROPT_COPY_LIST_SET_BIT_JUMPS        0 /* 'no' irrelevant  */
@@ -24,6 +25,7 @@ return "@(#)$Id: ogr-vec.cpp,v 1.3.4.1 2003/02/25 12:30:23 snake Exp $"; }
     #define OGROPT_STRENGTH_REDUCE_CHOOSE         0 /* MrC is better    */
     #define OGROPT_ALTERNATE_CYCLE                2 /* AltiVec support! */
     #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* MrC is better    */
+    #define OGROPT_CYCLE_CACHE_ALIGN              0 /* no balignl       */
   #elif (__APPLE_CC__)//GCC with exclusive ppc, mach-o and ObjC extensions
     #define OGROPT_BITOFLIST_DIRECT_BIT           0 /* 'no' irrelevant  */
     #define OGROPT_COPY_LIST_SET_BIT_JUMPS        0 /* 'no' irrelevant  */
@@ -32,14 +34,16 @@ return "@(#)$Id: ogr-vec.cpp,v 1.3.4.1 2003/02/25 12:30:23 snake Exp $"; }
     #define OGROPT_STRENGTH_REDUCE_CHOOSE         1 /* ACC does benefit */
     #define OGROPT_ALTERNATE_CYCLE                2 /* AltiVec support! */
     #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* ACC is better    */
+    #define OGROPT_CYCLE_CACHE_ALIGN              0 /* no balignl       */
   #elif (__GNUC__)
     #define OGROPT_BITOFLIST_DIRECT_BIT           0 /* 'no' irrelevant  */
     #define OGROPT_COPY_LIST_SET_BIT_JUMPS        0 /* 'no' irrelevant  */
     #define OGROPT_FOUND_ONE_FOR_SMALL_DATA_CACHE 0 /* 'no' irrelevant  */
     #define OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM   1 /* we have cntlzw   */
-    #define OGROPT_STRENGTH_REDUCE_CHOOSE         1 /* GCC does benefit */
+    #define OGROPT_STRENGTH_REDUCE_CHOOSE         0 /* GCC is better    */
     #define OGROPT_ALTERNATE_CYCLE                2 /* AltiVec support! */
-    #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* GCC is better    */
+    #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 2 /* use switch_asm   */
+    #define OGROPT_CYCLE_CACHE_ALIGN              1
   #else
     #error play with the settins to find out optimal settings for your compiler
   #endif
