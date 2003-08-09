bac6d7197af3fd069c71e42b252f956d6af1a1f5
Michael Weiser <mweiser@distributed.net>
2003-08-09 12:21:41 +0000

    add slightly modified setenv and waitpid implementations for NeXTstep from OpenBSD/OpenSSH

--- plat/next/next_sup.h
+++ plat/next/next_sup.h
@@ -1,8 +1,18 @@
-/* $Id: next_sup.h,v 1.1.2.1 2003/01/19 14:33:56 andreasb Exp $ */
+/* $Id: next_sup.h,v 1.1.2.2 2003/08/09 12:21:41 mweiser Exp $ */
 
 #ifndef __NEXTSTEP_SUP_H__
 #define __NEXTSTEP_SUP_H__
 
+#include <sys/types.h> /* size_t, off_t */
+#include <sys/wait.h>  /* union wait, WIFEXITED */
+
 char *strdup(const char *);
+int setenv (const char *,const char *, int);
+
+#define WIFTERMSIG(x)  ((x).w_termsig)
+#define WEXITSTATUS(x) ((x).w_retcode)
+#define WSTOPSIG(x)    ((x).w_stopsig)
+
+int waitpid(int, int *, int);
 
 #endif
