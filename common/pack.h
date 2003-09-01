050cf3cad958803ef2fdc41634bf4e3ab9187a50
Jeff Lawson <bovine@distributed.net>
2003-09-01 07:17:26 +0000

    add Visual C++ warning supression, and cleanup organization to simplify support for additional compilers.

--- common/pack.h
+++ common/pack.h
@@ -6,31 +6,48 @@
 */
 
 #ifndef __PACK_H__
-#define __PACK_H__  "@(#)$Id: pack.h,v 1.1.2.2 2003/08/25 16:41:24 mweiser Exp $"
+#define __PACK_H__  "@(#)$Id: pack.h,v 1.1.2.3 2003/09/01 07:17:26 jlawson Exp $"
 
-#if (!defined(__GNUC__) || (__GNUC__ < 2) || \
+
+#if defined(__GNUC__) && ((__GNUC__ > 2) || \
+     ((__GNUC__ == 2) && (__GNUC_MINOR__ >= 91)))
+
+  /* newer versions of gcc use a vendor-specific attribute. */
+  #define DNETC_PACKED1   __attribute__((packed))
+  #define DNETC_ALIGNED2  __attribute__((aligned(2)))
+  #define DNETC_ALIGNED4  __attribute__((aligned(4)))
+  #define DNETC_ALIGNED8  __attribute__((aligned(8)))
+  #define DNETC_ALIGNED16 __attribute__((aligned(16)))
+  #define DNETC_ALIGNED32 __attribute__((aligned(32)))
+
+#elif defined(__GNUC__) && ((__GNUC__ < 2) || \
      ((__GNUC__ == 2) && (__GNUC_MINOR__ < 91)))
-# if defined(MIPSpro)
-   /* don't use anything on MIPSpro */
-#  pragma warning "no packed structures!"
-# else
-   /* use pack() on anything we don't know and old gcc's */
-#  define DNETC_USE_PACK 1
-# endif
+  /* use pack() on old gcc's. */
+  #define DNETC_USE_PACK 1
 
+#elif defined(MIPSpro)
+  /* don't use anything on MIPSpro. */
+  #pragma warning "no packed structures!"
+
+#elif defined(_MSC_VER) && (_MSC_VER >= 800)
+  /* Visual C++ prints an infomational warning with pack changes. */
+  #pragma warning(disable:4103)
+  #define DNETC_USE_PACK 1
+
+#else
+  /* use pack() on anything we don't know. */
+  #define DNETC_USE_PACK 1
+#endif
+
+
+/* If these were defined above, then just define them to nothing. */
+#ifndef DNETC_PACKED1
 # define DNETC_PACKED1
 # define DNETC_ALIGNED2
 # define DNETC_ALIGNED4
 # define DNETC_ALIGNED8
 # define DNETC_ALIGNED16
 # define DNETC_ALIGNED32
-#else
-# define DNETC_PACKED1   __attribute__((packed))
-# define DNETC_ALIGNED2  __attribute__((aligned(2)))
-# define DNETC_ALIGNED4  __attribute__((aligned(4)))
-# define DNETC_ALIGNED8  __attribute__((aligned(8)))
-# define DNETC_ALIGNED16 __attribute__((aligned(16)))
-# define DNETC_ALIGNED32 __attribute__((aligned(32)))
 #endif
 
 #endif /* __PACK_H__ */
