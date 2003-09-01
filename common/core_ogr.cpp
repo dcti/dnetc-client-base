3bc459a8c5a5dccf2679393cd5824604e43ccb3b
Michael Weiser <mweiser@distributed.net>
2003-09-01 22:16:53 +0000

    remove trailing whitespace and fix indenting

--- common/core_ogr.cpp
+++ common/core_ogr.cpp
@@ -1,10 +1,10 @@
-/* 
+/*
  * Copyright distributed.net 1998-2003 - All Rights Reserved
  * For use in distributed.net projects only.
  * Any other distribution or use of this source violates copyright.
 */
 const char *core_ogr_cpp(void) {
-return "@(#)$Id: core_ogr.cpp,v 1.1.2.2 2003/09/01 19:27:36 jlawson Exp $"; }
+return "@(#)$Id: core_ogr.cpp,v 1.1.2.3 2003/09/01 22:16:53 mweiser Exp $"; }
 
 //#define TRACE
 
@@ -27,7 +27,7 @@ return "@(#)$Id: core_ogr.cpp,v 1.1.2.2 2003/09/01 19:27:36 jlawson Exp $"; }
 /* ======================================================================== */
 
 /* all the core prototypes
-   note: we may have more prototypes here than cores in the client 
+   note: we may have more prototypes here than cores in the client
    note2: if you need some 'cdecl' value define it in selcore.h to CDECL */
 
 
@@ -115,7 +115,7 @@ const char **corenames_for_contest_ogr()
    they are different from their predecessor(s). If only one core,
    use the obvious "MIPS optimized" or similar.
   */
-  static const char *corenames_table[] = 
+  static const char *corenames_table[] =
     {
   /* ================================================================== */
   #if (CLIENT_CPU == CPU_X86)
@@ -132,7 +132,7 @@ const char **corenames_for_contest_ogr()
       "GARSP 5.13 68030",
       "GARSP 5.13 68040",
       "GARSP 5.13 68060",
-  #elif (CLIENT_CPU == CPU_ALPHA) 
+  #elif (CLIENT_CPU == CPU_ALPHA)
       "GARSP 5.13",
       "GARSP 5.13-CIX",
   #elif (CLIENT_CPU == CPU_POWERPC) || (CLIENT_CPU == CPU_POWER)
@@ -145,7 +145,7 @@ const char **corenames_for_contest_ogr()
       "GARSP 5.13",
   #else
       "GARSP 5.13",
-  #endif  
+  #endif
   /* ================================================================== */
       NULL
   };
@@ -155,7 +155,7 @@ const char **corenames_for_contest_ogr()
 
 /* -------------------------------------------------------------------- */
 
-/* 
+/*
 ** Apply substition according to the same rules enforced by
 ** selcoreSelectCore() [ie, return the cindex of the core actually used
 ** after applying appropriate OS/architecture/#define limitations to
@@ -163,13 +163,13 @@ const char **corenames_for_contest_ogr()
 **
 ** This is necessary when the list of cores is a superset of the
 ** cores supported by a particular build. For example, all x86 clients
-** display the same core list for RC5, but as not all cores may be 
-** available in a particular client/build/environment, this function maps 
+** display the same core list for RC5, but as not all cores may be
+** available in a particular client/build/environment, this function maps
 ** between the ones that aren't available to the next best ones that are.
 **
 ** Note that we intentionally don't do very intensive validation here. Thats
 ** selcoreGetSelectedCoreForContest()'s job when the user chooses to let
-** the client auto-select. If the user has explicitely specified a core #, 
+** the client auto-select. If the user has explicitely specified a core #,
 ** they have to live with the possibility that the choice will at some point
 ** no longer be optimal.
 */
@@ -184,9 +184,9 @@ int apply_selcore_substitution_rules_ogr(int cindex)
   #elif (CLIENT_CPU == CPU_POWERPC) || (CLIENT_CPU == CPU_POWER)
   {
     /* AIX note:
-    ** A power-only client running on PPC will never get here. So, at this 
+    ** A power-only client running on PPC will never get here. So, at this
     ** point its either a power-only client running on power, or a ppc-only
-    ** client (no power core) running on PPC or power, or _AIXALL client 
+    ** client (no power core) running on PPC or power, or _AIXALL client
     ** running on either power or PPC.
     */
     int have_vec = 0;
@@ -345,8 +345,8 @@ int selcoreGetPreselectedCoreForProject_ogr()
 
 /* ---------------------------------------------------------------------- */
 
-int selcoreSelectCore_ogr( unsigned int threadindex,
-                       int *client_cpuP, struct selcore *selinfo )
+int selcoreSelectCore_ogr(unsigned int threadindex,
+                          int *client_cpuP, struct selcore *selinfo)
 {
   int use_generic_proto = 0; /* if rc5/des unit_func proto is generic */
   unit_func_union unit_func; /* declared in problem.h */
@@ -430,7 +430,7 @@ int selcoreSelectCore_ogr( unsigned int threadindex,
   /* ================================================================== */
 
 
-  if (coresel >= 0 && unit_func.gen && 
+  if (coresel >= 0 && unit_func.gen &&
      coresel < ((int)corecount_for_contest(OGR)) )
   {
     if (client_cpuP)
