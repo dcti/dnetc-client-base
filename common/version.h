b63c90a74bd1bb623f1806cd4c83ca53494b9e1f
Didier Levet <kakace@distributed.net>
2004-07-13 22:26:53 +0000

    Bumped version to .493 due to new ASM cores (OGR-P2/Mac OS X)

--- common/version.h
+++ common/version.h
@@ -25,7 +25,7 @@
  * ---------------------------------------------------------------------
 */
 #ifndef __VERSION_H__
-#define __VERSION_H__ "@(#)$Id: version.h,v 1.76.2.18 2004/06/16 18:37:38 kakace Exp $"
+#define __VERSION_H__ "@(#)$Id: version.h,v 1.76.2.19 2004/07/13 22:26:53 kakace Exp $"
 
 /* BETA etc is handled internally/at-runtime by cliident.cpp. */
 /* Do not adjust for BETA here, particularly CLIENT_VERSIONSTRING. */
@@ -38,9 +38,9 @@
 #define CLIENT_CONTEST_HEX     0x5A   /* needed for macos version resource */
 #define CLIENT_BUILD           8
 #define CLIENT_BUILD_HEX       0x08   /* needed for macos version resource */
-#define CLIENT_BUILD_FRAC      492
-#define CLIENT_BUILD_FRAC_HEX  0x01EC /* needed for macos version resource */
-#define CLIENT_VERSIONSTRING   "2.9008-492"
+#define CLIENT_BUILD_FRAC      493
+#define CLIENT_BUILD_FRAC_HEX  0x01ED /* needed for macos version resource */
+#define CLIENT_VERSIONSTRING   "2.9008-493"
 
 /* combined version used in packets etc. ... */
 #define CLIENT_VERSION         ( (((u32)(CLIENT_CONTEST))    * 1000000UL) +  \
