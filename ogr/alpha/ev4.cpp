5717509a12e2f46d44b39a040069b65e1d0abd5e
Stephen A. Griffin <bdragon@distributed.net>
2004-08-13 04:01:48 +0000

    move alpha asm to it's own include file, and include baseincs.h to get endianness from endian.h for ev4 bump in speed of lookup_firstblank

--- ogr/alpha/ev4.cpp
+++ ogr/alpha/ev4.cpp
@@ -6,7 +6,7 @@
  */
 
 const char *ogr_ev4_cpp(void) {
-return "@(#)$Id: ev4.cpp,v 1.1.2.5.2.3 2004/08/11 16:48:27 bdragon Exp $"; }
+return "@(#)$Id: ev4.cpp,v 1.1.2.5.2.4 2004/08/13 04:01:48 bdragon Exp $"; }
 
 #if defined(__GNUC__)
   #define OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM   0 /* 0-2 - 'no'  (default) */
@@ -16,7 +16,6 @@ return "@(#)$Id: ev4.cpp,v 1.1.2.5.2.3 2004/08/11 16:48:27 bdragon Exp $"; }
   #define OGROPT_CYCLE_CACHE_ALIGN              0 /* 0/1 - 'no'  (default) */
   #define OGROPT_ALTERNATE_CYCLE                0 /* 0-2 - 'no'  (default) */
   #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* 0/1 - 'std' (default) */
-
 #else /* Compaq CC */
   #include <c_asm.h>
   #define OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM   0 /* 0-2 - 'no'  (default) */
@@ -26,10 +25,10 @@ return "@(#)$Id: ev4.cpp,v 1.1.2.5.2.3 2004/08/11 16:48:27 bdragon Exp $"; }
   #define OGROPT_CYCLE_CACHE_ALIGN              0 /* 0/1 - 'no'  (default) */
   #define OGROPT_ALTERNATE_CYCLE                0 /* 0-2 - 'no'  (default) */
   #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* 0/1 - 'std' (default) */
-
 #endif
 
-//#define ALPHA_CIX
 #define OGR_GET_DISPATCH_TABLE_FXN    ogr_get_dispatch_table_ev4
 
+#include "baseincs.h" //for endian detection
+#include "alpha/alpha-asm.h"
 #include "ansi/ogr.cpp"
--- ogr/alpha/ev67.cpp
+++ ogr/alpha/ev67.cpp
@@ -6,7 +6,7 @@
  */
 
 const char *ogr_ev67_cpp(void) {
-return "@(#)$Id: ev67.cpp,v 1.2.4.3.2.1 2004/08/08 19:29:18 kakace Exp $"; }
+return "@(#)$Id: ev67.cpp,v 1.2.4.3.2.2 2004/08/13 04:01:48 bdragon Exp $"; }
 
 #if defined(__GNUC__)
   #define OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM   2 /* 0-2 - "100% asm"      */
@@ -16,7 +16,6 @@ return "@(#)$Id: ev67.cpp,v 1.2.4.3.2.1 2004/08/08 19:29:18 kakace Exp $"; }
   #define OGROPT_CYCLE_CACHE_ALIGN              0 /* 0/1 - 'no'  (default) */
   #define OGROPT_ALTERNATE_CYCLE                0 /* 0-2 - 'no'  (default) */
   #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* 0/1 - 'std' (default) */
-
 #else /* Compaq CC */
   #include <c_asm.h>
   #define OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM   2 /* 0-2 - "100% asm"      */
@@ -31,23 +30,6 @@ return "@(#)$Id: ev67.cpp,v 1.2.4.3.2.1 2004/08/08 19:29:18 kakace Exp $"; }
 #define ALPHA_CIX
 #define OGR_GET_DISPATCH_TABLE_FXN    ogr_get_dispatch_table_cix
 
-
-#if (OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM == 2)
-  #if defined(__GNUC__)
-    static __inline__ int __CNTLZ__(register unsigned int i)
-    { 
-      register unsigned long j = (unsigned long)i << 32;
-      __asm__ ("ctlz %0,%0" : "=r"(j) : "0" (j));
-      return (int)j;
-    }
-  #else
-    static inline int __CNTLZ__(register unsigned int i)
-    {
-      __int64 r = asm("ctlz %a0, %v0;", (unsigned long)i << 32);
-      return (int)r;
-    } 
-  #endif
-  #define __CNTLZ(x) (__CNTLZ__(~(x))+1)
-#endif
-
+#include "baseincs.h" //for endian detection
+#include "alpha/alpha-asm.h"
 #include "ansi/ogr.cpp"
