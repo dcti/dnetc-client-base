af45f890c644c3bf2b33dc98d8eba148ee1e16d5
Roman Trunov <stream@distributed.net>
2007-09-01 09:37:38 +0000

    Faster OGR MMX/Intel core (about 2%)

--- ogr/x86/ogr-asm2-rt-mmx.asm
+++ ogr/x86/ogr-asm2-rt-mmx.asm
@@ -1,20 +1,8 @@
 ;
-; Assembly core for OGR, MMX version
+; Assembly core for OGR, MMX version. Based on generic assembly core.
+; $Id: ogr-asm2-rt-mmx.asm,v 1.1.2.8 2007/09/01 09:37:38 stream Exp $
 ;
 ; Created by Roman Trunov (proxyma@tula.net)
-;
-; Based on disassembled output of Watcom compiler which suddenly generated
-; code about 15% faster than prior Windows clients.
-;
-; Watcom's output was optimized a little more manually (alas, this compiler
-; cannot align loops), but pipeline optimization can be far away from complete.
-; This code was developed on and tuned for PII-Celeron CPU, on other processors
-; its performance can be comparable or even less then current cores.
-;
-; Anyway, on my system this code works about 30% faster than Windows client.
-;
-; Additional improvements can be achieved by better optimization of pipelines
-; and usage of MMX instruction/registers.
 ;
 ; 2005-06-18: Removed local variable "limit", it's allocated on stack and
 ;             contains only copy of "lev->limit". I cannot allocate it in
@@ -25,6 +13,9 @@
 ;
 ; 2007-02-03: More effecient registers assignment and math optimization
 ;             in inner loop. Less commands, 2% faster.
+;
+; 2007-09-01: More effecient registers assignment in loops so
+;	      'remdepth' now sits in register. 2% faster.
 ;
 
 cpu	586
@@ -115,67 +106,76 @@ ogr_cycle_:
 	push	esi
 	push	edi
 	push	ebp
+
+;	stack allocation ([SP+##h]):
+;	importance: inner loop -> outer -> checklimit
+;		variable		offset		used in
+
+	%define work_nodes		esp+00H		; outer
+	%define work_choosedat3		esp+04H		; outer
+	%define work_maxlength		esp+08H		; outer (R1), checklimit
+	%define work_depth		esp+0CH		; outer, push/pop
+	%define work_halfdepth2		esp+10H		; outer (R1)
+
+	%define work_halflength		esp+14H		; checklimit (R1)
+	%define work_max_nodes		esp+18H		; checklimit (R1)
+	%define work_oState_half_length	esp+1CH		; checklimit (R1)
+	%define work_levHalfDepth	esp+20H		; checklimit (R1)
+
+	%define work_checkpoint		esp+24H		; push/pop (tc only)
+	%define work_checkpoint_depth	esp+28H		; push/pop (tc only)
+	%define work_oState		esp+2CH		; init
+	%define work_pnodes		esp+30H		; init
+
 	sub	esp,38H
+
+;	initialization of variables and cached data
+
 	add	ecx,3
-	mov	dword [esp+1cH],ecx	; ogr_choose_dat+3
-	mov	dword [esp+0cH],edx	; pnodes
+	mov	dword [work_choosedat3],ecx	; ogr_choose_dat+3
+	mov	dword [work_pnodes],edx		; pnodes
 	mov	edx,[edx]
-	mov	dword [esp+2cH],edx	; *pnodes
-	mov	dword [esp+18H],eax	; oState
+	mov	dword [work_max_nodes],edx	; *pnodes
+	mov	dword [work_oState],eax		; oState
 	mov	edx,dword [eax+1cH]
 	inc	edx
-	mov	dword [esp+24H],edx	; int depth = oState->depth + 1;
+	mov	dword [work_depth],edx		; int depth = oState->depth + 1;
 	imul	edx,sizeof_level
-	lea	ebp,[eax+edx+20H]	; lev = &oState->Levels[depth]
+	lea	ebp,[eax+edx+20H]		; lev = &oState->Levels[depth]
 %if with_time_constraints
 	mov	edx,dword [eax+ostate_node_offset]
-	add	dword [esp+2cH],edx	; *pnodes += oState->node_offset;
-	mov	dword [esp+14H],edx	; nodes = oState->node_offset;
+	add	dword [work_max_nodes],edx	; *pnodes += oState->node_offset;
+	mov	dword [work_nodes],edx		; nodes = oState->node_offset;
 	xor	edx,edx
-	mov	dword [esp+30H],edx	; int checkpoint = 0;
+	mov	dword [work_checkpoint],edx	; int checkpoint = 0;
 	mov	dword [eax+ostate_node_offset],edx	; oState->node_offset = 0;
 	; int checkpoint_depth = (1+STUB_MAX) - oState->startdepth;
 	mov	edx,(1+STUB_MAX)
 	sub	edx,dword [eax+18H]
-	mov	dword [esp+34H],edx
+	mov	dword [work_checkpoint_depth],edx
 %else
-	and	dword [esp+14H],0	; int nodes = 0;
+	and	dword [work_nodes],0		; int nodes = 0;
 %endif
-	mov	ecx,dword [eax+10H]
+	mov	ecx,dword [eax+0cH]		; oState->half_length
+	mov	[work_oState_half_length],ecx	;   (cache it)
+	mov	ecx,dword [eax+10H]		; oState->half_depth
 	imul	ecx,sizeof_level
 	lea	ebx,[eax+ecx+20H]
-	mov	dword [esp+4],ebx	; levHalfDepth = &oState->Levels[oState->half_depth]
+	mov	dword [work_levHalfDepth],ebx	; levHalfDepth = &oState->Levels[oState->half_depth]
 	mov	edx,dword [eax]
-	mov	dword [esp+10H],edx	; maxlength  = oState->max;
-	mov	edx,dword [eax+8]
-	mov	ecx,dword [esp+24H]
-	sub	edx,ecx
-	mov	dword [esp+20H],edx	; remdepth   = oState->maxdepthm1 - depth;
+	mov	dword [work_maxlength],edx	; maxlength  = oState->max;
+	mov	edi,dword [eax+8]
+	mov	ecx,dword [work_depth]
+	sub	edi,ecx				; remdepth   = oState->maxdepthm1 - depth;
 	mov	edx,dword [eax+10H]
 	mov	ebx,dword [eax+18H]
 	sub	edx,ebx
-	mov	dword [esp],edx		; halfdepth  = oState->half_depth - oState->startdepth;
+	mov	dword [work_halflength],edx	; halfdepth  = oState->half_depth - oState->startdepth;
 	mov	edx,dword [eax+14H]
 	sub	edx,ebx
-	mov	dword [esp+8],edx	; halfdepth2 = oState->half_depth2 - oState->startdepth;
+	mov	dword [work_halfdepth2],edx	; halfdepth2 = oState->half_depth2 - oState->startdepth;
 	sub	ecx,ebx
-	mov	dword [esp+24H],ecx	; depth -= oState->startdepth;
-
-; stack allocation ([SP+##h]):
-;	00H	halfdepth
-;	04H	levHalfDepth
-;	08H	halfdepth2
-;	0CH	pnodes
-;	10H	maxlength
-;	14H	nodes
-;	18H	oState
-;	1CH	offsetof(level->comp) == [ebp+40] => KILLED => Now ogr_choose_dat+3
-;	20H	remdepth
-;	24H	depth
-;	28H	limit => KILLED.
-;	2cH	mark => KILLED. Now contains *pnodes for fast compare.
-;	30H	checkpoint
-;	34H	checkpoint_depth
+	mov	dword [work_depth],ecx		; depth -= oState->startdepth;
 
 	mov	ebx,dword [ebp+lev_mark]	;   int mark = lev->mark;
 
@@ -196,7 +196,7 @@ ogr_cycle_:
 	mov	eax,1
 	movd	mm7,eax			; special constant H=0000 L=0001
 
-	mov	edi,dword [ebp+lev_dist0]	; (u32) dist0
+	mov	ecx,dword [ebp+lev_dist0]	; (u32) dist0
 	mov	esi,dword [ebp+lev_comp0]	; (u32) lev->comp[0]
 	movq	mm0,[ebp+lev_comp1]		;	lev->comp[1]
 	movq	mm1,[ebp+lev_comp2]		;	lev->comp[2]
@@ -216,74 +216,20 @@ ogr_cycle_:
 %endif
 
 	align	16
-.checklimit:
-	; we enter here with:
-	;	eax = depth
-	;	edx = limit
-	; we must return back to store_limit with:
-	;	edx = limit
-	; Don't forget to store limit when leaving globally!
-	;      if (depth <= halfdepth) {
-	cmp	eax,dword [esp]
-	jg	.L$56
-%if with_time_constraints
-%else
-	;        if (nodes >= *pnodes) {
-	mov	eax,dword [esp+14H]	; nodes
-	cmp	eax,dword [esp+2cH]	; *pnodes
-	jge	.L$54
-%endif
-	;        limit = maxlength - OGR[remdepth];
-	mov	eax,dword [esp+20H]	; remdepth
-	mov	edx,dword [esp+10H]	; maxlength
-	mov	edi,dword [_OGR+eax*4]
-	sub	edx,edi			; limit (new)
-	;        if (limit > oState->half_length)
-	mov	edi,dword [esp+18H]	; oState
-	mov	eax,dword [edi+0cH]	; oState->half_length
-	cmp	edx,eax
-	jle	.store_limit
-	;          limit = oState->half_length;
-	mov	edx,eax
-	jmp	.store_limit
-
-	align	16
-.L$56:
-	;      else if (limit >= maxlength - levHalfDepth->mark) {
-	mov	eax,dword [esp+10H]	; maxlength
-	mov	edi,dword [esp+4]	; levHalfDepth
-	sub	eax,dword [edi+lev_mark]; levHalfDepth->mark
-	cmp	eax,edx
-	jg	.store_limit
-	;        limit = maxlength - levHalfDepth->mark - 1;
-	lea	edx,[eax-1]
-	jmp	.store_limit
-
-%if with_time_constraints
-%else
-.L$54:
-	;          retval = CORE_S_CONTINUE;
-	;          break;
-	mov	eax,1
-	jmp	.L$53_exit
-%endif
-
-	align	16
 .outerloop:
-					; edi must be loaded with dist0
+					; ecx must be loaded with dist0
 	;    limit = maxlength - choose(dist0 >> ttmDISTBITS, remdepth);
-	shr	edi,14H			; dist0
-	imul	edi,0cH
-	mov	edx,dword [esp+20H]	; remdepth
+	shr	ecx,14H			; dist0
+	imul	ecx,0cH
 	xor	eax,eax
-	add	edi,dword [esp+1cH]	; ogr_choose_dat+3
-	mov	al,byte [edi+edx]	; mov al,byte [_ogr_choose_dat+edi+edx+3]
-	mov	edx,dword [esp+10H]	; maxlength
+	add	ecx,dword [work_choosedat3]	; ogr_choose_dat+3
+	mov	al,byte [ecx+edi]		; mov al,byte [_ogr_choose_dat+ecx+remdepth+3]
+	mov	edx,dword [work_maxlength]	; maxlength
 	sub	edx,eax
 	;    if (depth <= halfdepth2) {
-	mov	eax,dword [esp+24H]	; depth
-	cmp	eax,dword [esp+8]	; halfdepth2
-	jle	.checklimit
+	mov	eax,dword [work_depth]		; depth
+	cmp	eax,dword [work_halfdepth2]	; halfdepth2
+	jle	near .checklimit		; chance: < 0.5%
 .store_limit:
 	; limit (save on stack) => KILLED
 	;    lev->limit = limit;
@@ -291,23 +237,25 @@ ogr_cycle_:
 	; funny but it improves only MMX core on my PII-Celeron...
 	mov	dword [dword ebp+lev_limit],edx
 	;    nodes++;
-	inc	dword [dword esp+14H]
+	inc	dword [dword work_nodes]
 
 	_natural_align
 
-.stay:   ; Most important internal loop
+.stay:  ; Most important internal loop
+	; Chance to get here: 50% by fallthru, 50% by jump below
 	;
 	; entry: ebx = mark   (keep and update!)
+	;	 edi = remdepth (keep, update)
 	;	 esi = comp0  (reloaded immediately after shift)
 	;	 ebp = level
 	;	 ecx =
-	;	 edi will be dist0  (reloaded later, don't care)
-	; usable: eax, edx, ecx, edi
+	;	 ecx will be dist0  (reloaded later, don't care)
+	; usable: eax, edx, ecx
 	; usable with care: esi
 	;
 	;    if (comp0 < 0xfffffffe) {
 	cmp	esi,0fffffffeH
-	jnb	.L$57_
+	jnb	.L$57_			; chance: 25%
 	;      int s = LOOKUP_FIRSTBLANK( comp0 );
 	not	esi
 	mov	ecx,20H
@@ -316,7 +264,7 @@ ogr_cycle_:
 	;      if ((mark += s) > limit) goto up;   /* no spaces left */
 	add	ebx,ecx
 	cmp	ebx,dword [ebp+lev_limit]	; limit (==lev->limit)
-	jg	.up
+	jg	.up			; chance: 35%
 	;      COMP_LEFT_LIST_RIGHT(lev, s);
 
 	;    #define COMP_LEFT_LIST_RIGHT(lev, s) {  \
@@ -365,8 +313,8 @@ ogr_cycle_:
 	;    lev->mark = mark;
 	;    if (remdepth == 0) {                  /* New ruler ? (last mark placed) */
 	mov	dword [ebp+lev_mark],ebx	; lev->mark
-	cmp	dword [esp+20H],0		; remdepth
-	je	.L$61
+	or	edi,edi				; remdepth
+	je	.L$61				; chance: almost zero
 	;    PUSH_LEVEL_UPDATE_STATE(lev);         /* Go deeper */
 
 	;    #define PUSH_LEVEL_UPDATE_STATE(lev)    \
@@ -383,18 +331,18 @@ ogr_cycle_:
 	;
 	; lev2->dist[n] = lev->dist[n] | lev->list[n];
 	;
-	; Note! new comp0, dist0 must be loaded to esi, edi
+	; Note! new comp0, dist0 must be loaded to esi, ecx
 
-	movd	edi,mm2				; list0
-	mov	[ebp+lev_list0],edi		; lev->list[0] = list0
+	movd	ecx,mm2				; list0
+	mov	[ebp+lev_list0],ecx		; lev->list[0] = list0
 	movq	[ebp+lev_list1],mm3		; lev->list[1] = list1
 	movq	[ebp+lev_list2],mm4		; lev->list[2] = list2
 	mov	[ebp+lev_comp0],esi		; lev->comp[0] = comp0
 	movq	[ebp+lev_comp1],mm0		; lev->comp[1] = comp1
 	movq	[ebp+lev_comp2],mm1		; lev->comp[2] = comp2
-	or	edi,[ebp+lev_dist0]		; dist0 |= list0
-	mov	[ebp+sizeof_level+lev_dist0],edi ; lev2->dist0
-	or	esi,edi				; comp0 |= dist0
+	or	ecx,[ebp+lev_dist0]		; dist0 |= list0
+	mov	[ebp+sizeof_level+lev_dist0],ecx ; lev2->dist0
+	or	esi,ecx				; comp0 |= dist0
 	movq	mm6,[ebp+lev_dist1]		; lev->dist[1]
 	por	mm6,mm3				; dist1 |= list1
 	movq	[ebp+sizeof_level+lev_dist1],mm6 ; lev2->dist1 = dist1
@@ -410,12 +358,9 @@ ogr_cycle_:
 	;    lev++;
 	add	ebp,sizeof_level
 	;    remdepth--;
-	sub	dword [esp+20H], 1	; 'sub' is better. stall on flags or better decoding?
+	sub	edi, 1			; 'sub' is better. stall on flags or better decoding?
 	;    depth++;
-;	inc	dword [esp+24H]
-	mov	edx,dword [esp+24H]
-	inc	edx			; but here expanded 'inc' is better...
-	mov	dword [esp+24H],edx
+	;    (split under %if)
 	;    continue;
 
 ; When core processed at least given number of nodes (or a little more)
@@ -428,13 +373,16 @@ ogr_cycle_:
 ; command sequence from "outerloop" to "stay".
 
 %if with_time_constraints
+	;    depth++;
 	;      if (depth <= checkpoint_depth)
 	;        checkpoint = nodes;
-;	mov	edx,dword [esp+24H]	; depth cached above
-	mov	eax,dword [esp+14H]	; nodes
-	cmp	edx,dword [esp+34H]	; checkpoint_depth
+	mov	edx,dword [work_depth]
+	inc	edx			; but here expanded 'inc' is better...
+	mov	dword [work_depth],edx
+	mov	eax,dword [work_nodes]	; nodes
+	cmp	edx,dword [work_checkpoint_depth]	; checkpoint_depth
 	jg	.L$01
-	mov	dword [esp+30H],eax
+	mov	dword [work_checkpoint],eax
 
 	align	16	; cannot align naturally :-(
 
@@ -443,21 +391,61 @@ ogr_cycle_:
 ;	_natural_align
 .L$01:
 	;      if (nodes >= *pnodes) {
-	cmp	eax,dword [esp+2cH]	; *pnodes
+	cmp	eax,dword [work_max_nodes]	; *pnodes
 	jl	.outerloop
 	;        oState->node_offset = nodes - checkpoint;
-	sub	eax,dword [esp+30H]	; nodes - checkpoint
-	mov	edx,dword [esp+18H]	; oState
+	sub	eax,dword [work_checkpoint]	; nodes - checkpoint
+	mov	edx,dword [work_oState]		; oState
 	mov	dword [edx+ostate_node_offset],eax
 	;        nodes = checkpoint;
-	mov	eax,dword [esp+30H]
-	mov	dword [esp+14H],eax
+	mov	eax,dword [work_checkpoint]
+	mov	dword [work_nodes],eax
 	;	 retval = CORE_S_CONTINUE;
 	mov	eax,1
 	jmp	.L$53_exit
 %else
+	;    depth++;
+	inc	dword [work_depth]
 	jmp	.outerloop
 %endif
+
+
+	align	16
+.up:
+	;    lev--;
+	sub	ebp,sizeof_level
+
+	;    #define POP_LEVEL(lev)    \
+	;      list0 = lev->list[0];   \
+	;      list1 = lev->list[1];   \
+	;      list2 = lev->list[2];   \
+	;      dist0 &= ~list0;        \
+	;      dist1 &= ~list1;        \
+	;      dist2 &= ~list2;        \
+	;      comp0 = (u32) lev->comp[0];   \
+	;      comp1 = lev->comp[1];   \
+	;      comp2 = lev->comp[2];
+
+	mov	esi,dword [ebp+lev_comp0]	; (u32) lev->comp[0]
+	movq	mm0,[ebp+lev_comp1]		;	lev->comp[1]
+	movq	mm1,[ebp+lev_comp2]		;	lev->comp[2]
+	movd	mm2,[ebp+lev_list0]		; (u32) lev->list[0], newbit = 0
+	movq	mm3,[ebp+lev_list1]		;	lev->list[1]
+	movq	mm4,[ebp+lev_list2]		;	lev->list[2]
+	; dist not loaded because it's not cached
+
+	;    limit = lev->limit; KILLED
+	;    mark = lev->mark;
+	mov	ebx,dword [ebp+lev_mark]
+	;    remdepth++;
+	add	edi,1				; stall on flags below
+	;    depth--;
+	;    if (depth <= 0) {
+	dec	dword [work_depth]
+	jg	.stay				; chance: almost 100%
+	xor	eax,eax
+	jmp	.L$53_exit
+
 
 	align	16
 
@@ -466,7 +454,7 @@ ogr_cycle_:
 .L$57_:
 	add	ebx,20H
 	cmp	ebx,dword [ebp+lev_limit]	; limit (==lev->limit)
-	jg	.up
+	jg	.up				; chance: 75%
 
 	; small optimize. in both cases (esi == -1 and not) we perform
 	; COMP_LEFT_LIST_RIGHT_32, then just jump to different labels.
@@ -502,43 +490,74 @@ ogr_cycle_:
 
 	; Good that MMX do not set ALU flags
 
-	jne	.L$58	; esi != -1
-	jmp	.stay	; esi == -1
+	je	.stay	; esi == -1	chance: 95%
+	jmp	.L$58	; esi != -1
 
 	align	16
-.up:
-	;    lev--;
-	sub	ebp,sizeof_level
+.checklimit:
+	; we enter here with:
+	;	eax = depth
+	;	edx = limit
+	; we must return back to store_limit with:
+	;	edx = limit
+	; Don't forget to store limit when leaving globally!
+	;      if (depth <= halfdepth) {
+	cmp	eax,dword [work_halflength]
+	jg	.L$56
+%if with_time_constraints
+%else
+	;        if (nodes >= *pnodes) {
+	mov	eax,dword [work_nodes]		; nodes
+	cmp	eax,dword [work_max_nodes]	; *pnodes
+	jge	.L$54
+%endif
+	;        limit = maxlength - OGR[remdepth];
+	mov	edx,dword [work_maxlength]	; maxlength
+	mov	ecx,dword [_OGR+edi*4]		; remdepth
+	sub	edx,ecx				; limit (new)
+	;        if (limit > oState->half_length)
+	mov	eax,dword [work_oState_half_length]	; oState->half_length (cached)
+	cmp	edx,eax
+	jle	.store_limit
+	;          limit = oState->half_length;
+	mov	edx,eax
+	jmp	.store_limit
 
-	;    #define POP_LEVEL(lev)    \
-	;      list0 = lev->list[0];   \
-	;      list1 = lev->list[1];   \
-	;      list2 = lev->list[2];   \
-	;      dist0 &= ~list0;        \
-	;      dist1 &= ~list1;        \
-	;      dist2 &= ~list2;        \
-	;      comp0 = (u32) lev->comp[0];   \
-	;      comp1 = lev->comp[1];   \
-	;      comp2 = lev->comp[2];
+	align	16
+.L$56:
+	;      else if (limit >= maxlength - levHalfDepth->mark) {
+	mov	eax,dword [work_maxlength]	; maxlength
+	mov	ecx,dword [work_levHalfDepth]	; levHalfDepth
+	sub	eax,dword [ecx+lev_mark]	; levHalfDepth->mark
+	cmp	eax,edx
+	jg	.store_limit
+	;        limit = maxlength - levHalfDepth->mark - 1;
+	lea	edx,[eax-1]
+	jmp	.store_limit
 
-	mov	esi,dword [ebp+lev_comp0]	; (u32) lev->comp[0]
-	movq	mm0,[ebp+lev_comp1]		;	lev->comp[1]
-	movq	mm1,[ebp+lev_comp2]		;	lev->comp[2]
-	movd	mm2,[ebp+lev_list0]		; (u32) lev->list[0], newbit = 0
-	movq	mm3,[ebp+lev_list1]		;	lev->list[1]
-	movq	mm4,[ebp+lev_list2]		;	lev->list[2]
-	; dist not loaded because it's not cached
+%if with_time_constraints
+%else
+.L$54:
+	;          retval = CORE_S_CONTINUE;
+	;          break;
+	mov	eax,1
+	jmp	.L$53_exit
+%endif
 
-	;    limit = lev->limit; KILLED
-	;    mark = lev->mark;
-	mov	ebx,dword [ebp+lev_mark]
-	;    remdepth++;
-	inc	dword [esp+20H]
-	;    depth--;
-	;    if (depth <= 0) {
-	dec	dword [esp+24H]
-	jg	.stay
-	xor	eax,eax
+.L$61:
+	mov	eax,dword [work_oState]
+	push	ebx			; preserve regs clobbered by cdecl
+	push	ecx
+	push	edx
+	push	eax			; parameter
+	call	[_found_one_cdecl_ptr]
+	add	esp, 4
+	pop	edx
+	pop	ecx
+	pop	ebx			; restore clobbered regs
+	cmp	eax,1
+	je	.stay
+
 .L$53_exit:
 	;
 	; esi must be loaded with comp0!
@@ -562,13 +581,13 @@ ogr_cycle_:
 	movq	[ebp+lev_comp2],mm1
 
 	mov	dword [ebp+lev_mark],ebx	; mark
-	mov	edx,dword [esp+24H]
+	mov	edx,dword [work_depth]
 	dec	edx
-	mov	ecx,dword [esp+18H]
+	mov	ecx,dword [work_oState]
 	add	edx,dword [ecx+18H]
 	mov	dword [ecx+1cH],edx
-	mov	edx,dword [esp+14H]
-	mov	ecx,dword [esp+0cH]
+	mov	edx,dword [work_nodes]
+	mov	ecx,dword [work_pnodes]
 	mov	dword [ecx],edx
 	add	esp,38H
 	pop	ebp
@@ -578,20 +597,6 @@ ogr_cycle_:
 	emms
 	ret
 
-.L$61:
-	mov	eax,dword [esp+18H]
-	push	ebx			; preserve regs clobbered by cdecl
-	push	ecx
-	push	edx
-	push	eax			; parameter
-	call	[_found_one_cdecl_ptr]
-	add	esp, 4
-	pop	edx
-	pop	ecx
-	pop	ebx			; restore clobbered regs
-	cmp	eax,1
-	jne	.L$53_exit
-	jmp	.stay
 %ifndef OGR_DEBUG
 	%endmacro
 %endif
