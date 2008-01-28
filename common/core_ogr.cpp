59afe2f9c8300365593a8977defeecb67e63338f
Peter Teichmann <teichp@distributed.net>
2008-01-28 22:14:28 +0000

    remove OGR assembler cores for arm since they do not fit to the current infrastructure. use ansi core again.

--- common/core_ogr.cpp
+++ common/core_ogr.cpp
@@ -4,7 +4,7 @@
  * Any other distribution or use of this source violates copyright.
 */
 const char *core_ogr_cpp(void) {
-return "@(#)$Id: core_ogr.cpp,v 1.1.2.57 2007/11/19 13:41:57 stream Exp $"; }
+return "@(#)$Id: core_ogr.cpp,v 1.1.2.58 2008/01/28 22:14:28 teichp Exp $"; }
 
 //#define TRACE
 
@@ -74,9 +74,10 @@ return "@(#)$Id: core_ogr.cpp,v 1.1.2.57 2007/11/19 13:41:57 stream Exp $"; }
     extern "C" CoreDispatchTable *ogr_get_dispatch_table_asm_mmx_amd(void);
     #endif
 #elif (CLIENT_CPU == CPU_ARM)
-    extern "C" CoreDispatchTable *ogr_get_dispatch_table_arm1(void);
-    extern "C" CoreDispatchTable *ogr_get_dispatch_table_arm2(void);
-    extern "C" CoreDispatchTable *ogr_get_dispatch_table_arm3(void);
+    extern "C" CoreDispatchTable *ogr_get_dispatch_table(void);
+//    extern "C" CoreDispatchTable *ogr_get_dispatch_table_arm1(void);
+//    extern "C" CoreDispatchTable *ogr_get_dispatch_table_arm2(void);
+//    extern "C" CoreDispatchTable *ogr_get_dispatch_table_arm3(void);
 #elif (CLIENT_CPU == CPU_AMD64)
     extern "C" CoreDispatchTable *ogr64_get_dispatch_table(void);
 #elif (CLIENT_CPU == CPU_SPARC) && (SIZEOF_LONG == 8)
@@ -195,9 +196,10 @@ const char **corenames_for_contest_ogr()
   #elif (CLIENT_CPU == CPU_AMD64)
       "GARSP 6.0-64",
   #elif (CLIENT_CPU == CPU_ARM)
-      "GARSP 6.0 StrongARM",
+/*      "GARSP 6.0 StrongARM",
       "GARSP 6.0 ARM 2/3/6/7",
-      "GARSP 6.0 XScale",
+      "GARSP 6.0 XScale",*/
+      "GARSP 6.0 ansi",
   #elif (CLIENT_CPU == CPU_68K)
       "GARSP 6.0 68000",
       "GARSP 6.0 68020",
@@ -280,11 +282,11 @@ int apply_selcore_substitution_rules_ogr(int cindex)
   long det = GetProcessorType(1);
   if (det == 68000) cindex = 0;
 # elif (CLIENT_CPU == CPU_ARM)
-  long det = GetProcessorType(1);
+/*  long det = GetProcessorType(1);
   int have_clz = (((det >> 12) & 0xf) != 0) &&
                  (((det >> 12) & 0xf) != 7) &&
                  (((det >> 16) & 0xf) >= 3);
-  if (!have_clz && (cindex == 2))  cindex = 0;
+  if (!have_clz && (cindex == 2))  cindex = 0;*/
 # elif (CLIENT_CPU == CPU_POWERPC) || (CLIENT_CPU == CPU_CELLBE)
   int feature = 0;
   feature = GetProcessorFeatureFlags();
@@ -583,15 +585,19 @@ int selcoreSelectCore_ogr(unsigned int threadindex, int *client_cpuP,
   unit_func.ogr = ogr64_get_dispatch_table();
   coresel = 0;
 #elif (CLIENT_CPU == CPU_ARM)
-  if (coresel == 0)
+/*  if (coresel == 0)
     unit_func.ogr = ogr_get_dispatch_table_arm1();
   else if (coresel == 2)
     unit_func.ogr = ogr_get_dispatch_table_arm3();
+  else if (coresel == 3)
+    unit_func.ogr = ogr_get_dispatch_table();
   else
   {
     unit_func.ogr = ogr_get_dispatch_table_arm2();
     coresel = 1;
-  }
+  }*/
+  unit_func.ogr = ogr_get_dispatch_table();
+  coresel = 0;
 #elif (CLIENT_CPU == CPU_SPARC) && (SIZEOF_LONG == 8)
   unit_func.ogr = ogr64_get_dispatch_table();
   coresel = 0;
--- ogr/arm/ogr-arm.cpp
+++ ogr/arm/ogr-arm.cpp
@@ -6,24 +6,18 @@
  */
 
 const char *ogr_arm_cpp(void) {
-return "@(#)$Id: ogr-arm.cpp,v 1.1.4.1 2004/08/14 23:36:58 kakace Exp $"; }
+return "@(#)$Id: ogr-arm.cpp,v 1.1.4.2 2008/01/28 22:14:28 teichp Exp $"; }
 
 #if defined(ASM_ARM)
 
   #define OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM   1 /* 0-2 - partial support */
   #define OGROPT_STRENGTH_REDUCE_CHOOSE         1 /* 0/1 - 'yes' (default) */
   #define OGROPT_NO_FUNCTION_INLINE             0 /* 0/1 - 'no'  (default) */
-  #define OGROPT_HAVE_OGR_CYCLE_ASM             1 /* 0-2 - 'yes'           */
+  #define OGROPT_HAVE_OGR_CYCLE_ASM             0 /* 0-2 - 'yes'           */
   #define OGROPT_CYCLE_CACHE_ALIGN              0 /* 0/1 - 'no'  (default) */
   #define OGROPT_ALTERNATE_CYCLE                0 /* 0-2 - 'no'  (default) */
   #define OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT 0 /* 0/1 - 'std' (default) */
 
-  /*
-  ** Note from Kakace :
-  ** The following code has been borrowed from ogr.cpp
-  ** Since ARM platforms appear to use ASM cores, most of the settings as
-  ** well as this inline code only apply to ogr_create/ogr_create_pass2.
-  */
   #if defined(__GNUC__)
     #if (OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM == 1)
       static __inline__ int __CNTLZ__(register unsigned int input,
@@ -43,40 +37,14 @@ return "@(#)$Id: ogr-arm.cpp,v 1.1.4.1 2004/08/14 23:36:58 kakace Exp $"; }
                  : "1" (input), "r" ((unsigned int)bitarray));
         return result;
       }
-      #define __CNTLZ_ARRAY_BASED(x) __CNTLZ__(x)
-    #endif
-
-    #if (OGROPT_ALTERNATE_CYCLE == 0 && OGROPT_ALTERNATE_COMP_LEFT_LIST_RIGHT == 1)
-      #error fixme: No longer compatible with existing code
-      // need to copy "newbit" into "list0"
-      #define COMP_LEFT_LIST_RIGHT_32(lev) {  \
-        int a1, a2;                           \
-        asm ("ldr %0,[%2,#44]\n               \
-              ldr %1,[%2,#48]\n               \
-              str %0,[%2,#40]\n               \
-              ldr %0,[%2,#52]\n               \
-              str %1,[%2,#44]\n               \
-              ldr %1,[%2,#56]\n               \
-              str %0,[%2,#48]\n               \
-              ldr %0,[%2,#12]\n               \
-              str %1,[%2,#52]\n               \
-              ldr %1,[%2,#8]\n                \
-              str %0,[%2,#16]\n               \
-              ldr %0,[%2,#4]\n                \
-              str %1,[%2,#12]\n               \
-              ldr %1,[%2,#0]\n                \
-              str %0,[%2,#8]\n                \
-              mov %0,#0\n                     \
-              str %1,[%2,#4]\n                \
-              str %0,[%2,#56]\n               \
-              str %0,[%2,#0]" :               \
-            "=r" (a1), "=r" (a2),             \
-            "=r" (lev) : "2" (lev));          \
-      }
-
-    #endif  /* OGROPT_ALTERNATE_CYCLE == 0 */
+      #define __CNTLZ_ARRAY_BASED(x, y) __CNTLZ__(x, y)
+    #endif  /* OGROPT_HAVE_FIND_FIRST_ZERO_BIT_ASM */
   #endif  /* __GNUC__ */
 
-#endif  /* ASM_ARM */
+  #include "ansi/ogr.cpp"
+  
+#else  /* ASM_ARM */
 
-#include "ansi/ogr.cpp"
+  #error use this only with arm since it contains arm assembly
+  
+#endif
