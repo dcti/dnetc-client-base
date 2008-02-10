467689219676110dadafe43fe43eb5034a488f4e
Didier Levet <kakace@distributed.net>
2008-02-10 00:13:28 +0000

    Removed outdated stuff

--- ogr2/ogr.h
+++
@@ -1,9 +0,0 @@
-/* 
- * Currently (Aug, 9th 2000) this is only a stub to pull in the
- * one-and-only ogr.h. If custom 'ogr.h's should ever become necessary, 
- * they can be (conditionally) included from here.
- *
- * $Id: ogr.h,v 1.7 2002/09/02 00:35:47 andreasb Exp $
-*/ 
-#include "ansi/ogr.h"
- 
--- plat/macos/libsup/ReadTemp.h
+++
@@ -1,6 +0,0 @@
-#ifndef __READTEMP_H__
-#define __READTEMP_H__
-
-int GetProcessorTemperature(void);
-
-#endif /* __READTEMP_H__ */
