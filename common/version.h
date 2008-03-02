564fce4eba1958e1ae2dc5bd33b1a4356b1f188f
Didier Levet <kakace@distributed.net>
2008-03-02 14:18:46 +0000

    Bumped to 2.9016.506 (OGR-NG)

--- common/version.h
+++ common/version.h
@@ -25,7 +25,7 @@
  * ---------------------------------------------------------------------
 */
 #ifndef __VERSION_H__
-#define __VERSION_H__ "@(#)$Id: version.h,v 1.81 2008/02/07 10:24:03 kakace Exp $"
+#define __VERSION_H__ "@(#)$Id: version.h,v 1.82 2008/03/02 14:18:46 kakace Exp $"
 
 /* BETA etc is handled internally/at-runtime by cliident.cpp. */
 /* Do not adjust for BETA here, particularly CLIENT_VERSIONSTRING. */
@@ -36,11 +36,11 @@
 #define CLIENT_MAJOR_VER_HEX   0x02   /* needed for macos version resource */
 #define CLIENT_CONTEST         90
 #define CLIENT_CONTEST_HEX     0x5A   /* needed for macos version resource */
-#define CLIENT_BUILD           15
-#define CLIENT_BUILD_HEX       0x0F   /* needed for macos version resource */
-#define CLIENT_BUILD_FRAC      505
-#define CLIENT_BUILD_FRAC_HEX  0x01F9 /* needed for macos version resource */
-#define CLIENT_VERSIONSTRING   "2.9015-505"
+#define CLIENT_BUILD           16
+#define CLIENT_BUILD_HEX       0x10   /* needed for macos version resource */
+#define CLIENT_BUILD_FRAC      506
+#define CLIENT_BUILD_FRAC_HEX  0x01FA /* needed for macos version resource */
+#define CLIENT_VERSIONSTRING   "2.9016-506"
 
 /* combined version used in packets etc. ... */
 #define CLIENT_VERSION         ( (((u32)(CLIENT_CONTEST))    * 1000000UL) +  \
