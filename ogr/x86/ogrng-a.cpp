bbb6ab8e8467e074384e7c905dd85209e8a46feb
Roman Trunov <stream@distributed.net>
2008-11-13 08:34:37 +0000

    Added safety checks for compilation

--- ogr/x86/ogrng-a.cpp
+++ ogr/x86/ogrng-a.cpp
@@ -8,11 +8,31 @@
 
 #include "ansi/ogrng_codebase.cpp"
 
-#include "ccoreio.h"
+#include "ccoreio.h"       /* CDECL */
+#include <stddef.h>        /* offsetof */
 
 extern "C" int CDECL ogr_cycle_256_rt1(struct OgrState *oState, int *pnodes, const u16* pchoose);
 
 static int ogr_cycle_256(struct OgrState *oState, int *pnodes, const u16* pchoose)
 {
+    /* Check structures layout and alignment to match assembly */
+
+    STATIC_ASSERT(offsetof(struct OgrState, max)         == 0 );
+    STATIC_ASSERT(offsetof(struct OgrState, maxdepthm1)  == 8 );
+    STATIC_ASSERT(offsetof(struct OgrState, half_depth)  == 12);
+    STATIC_ASSERT(offsetof(struct OgrState, half_depth2) == 16);
+    STATIC_ASSERT(offsetof(struct OgrState, stopdepth)   == 24);
+    STATIC_ASSERT(offsetof(struct OgrState, depth)       == 28);
+    STATIC_ASSERT(offsetof(struct OgrState, Levels)      == 32);
+
+    STATIC_ASSERT(sizeof(struct OgrLevel) == 104);
+    STATIC_ASSERT(sizeof(oState->Levels)  == 104 * OGR_MAXDEPTH);
+
+    STATIC_ASSERT(offsetof(struct OgrLevel, list)  ==   0);
+    STATIC_ASSERT(offsetof(struct OgrLevel, dist)  ==  32);
+    STATIC_ASSERT(offsetof(struct OgrLevel, comp)  ==  64);
+    STATIC_ASSERT(offsetof(struct OgrLevel, mark)  ==  96);
+    STATIC_ASSERT(offsetof(struct OgrLevel, limit) == 100);
+
     return ogr_cycle_256_rt1(oState, pnodes, pchoose);
 }
