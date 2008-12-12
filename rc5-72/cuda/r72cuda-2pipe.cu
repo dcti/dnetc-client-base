0964875367f68e5105091d1c798ce8d80c47471c
Andreas Beckmann <andreasb@distributed.net>
2008-12-12 04:46:08 +0000

    reduce memory and register usage of 2-pipe core

--- rc5-72/cuda/r72cuda-2pipe.cu
+++ rc5-72/cuda/r72cuda-2pipe.cu
@@ -118,14 +118,14 @@ static s32 CDECL rc5_72_run_cuda_2(RC5_72UnitWork *rc5_72unitwork, u32 *iteratio
   }
 
   /* Allocate the results arrays */
-  results = (u8 *)malloc(grid_dim * num_threads * 2 * sizeof(u8));
+  results = (u8 *)malloc(grid_dim * num_threads * sizeof(u8));
   if( results == NULL ) {
     retval = -1;
     fprintf(stderr, "RC5 cuda: ERROR: malloc\r\n");
     goto error_exit;
   }
 
-  if( cudaMalloc((void **)&cuda_results, grid_dim * num_threads * 2 * sizeof(u8)) != (cudaError_t) CUDA_SUCCESS ) {
+  if( cudaMalloc((void **)&cuda_results, grid_dim * num_threads * sizeof(u8)) != (cudaError_t) CUDA_SUCCESS ) {
     retval = -1;
     fprintf(stderr, "RC5 cuda: ERROR: cudaMalloc: cuda_results\r\n");
     goto error_exit;
@@ -141,7 +141,7 @@ static s32 CDECL rc5_72_run_cuda_2(RC5_72UnitWork *rc5_72unitwork, u32 *iteratio
     dim3 block_dimension(num_threads);
     dim3 grid_dimension(grid_dim);
     u32 process_amount = min_u32(*iterations - i, optimal_process_amount);
-    u32 j;
+    u32 j, k;
     u32 match_count = 0;
 
     /* Clear the match_found variable */
@@ -152,7 +152,7 @@ static s32 CDECL rc5_72_run_cuda_2(RC5_72UnitWork *rc5_72unitwork, u32 *iteratio
     }
 
     /* Clear the results array */
-    if( cudaMemset(cuda_results, 0, grid_dim * num_threads * 2 * sizeof(u8)) != (cudaError_t) CUDA_SUCCESS ) {
+    if( cudaMemset(cuda_results, 0, grid_dim * num_threads * sizeof(u8)) != (cudaError_t) CUDA_SUCCESS ) {
       retval = -1;
       fprintf(stderr, "RC5 cuda: ERROR: cudaMemset: cuda_results\r\n");
       goto error_exit;
@@ -267,17 +267,17 @@ static s32 CDECL rc5_72_run_cuda_2(RC5_72UnitWork *rc5_72unitwork, u32 *iteratio
     if(match_found) {
 
       /* Copy the results[] array to the host */
-      if( cudaMemcpy((void *)results, (void *)cuda_results, process_amount * sizeof(u8), cudaMemcpyDeviceToHost) != (cudaError_t) CUDA_SUCCESS ) {
+      if( cudaMemcpy((void *)results, (void *)cuda_results, (process_amount + pipeline_count - 1) / pipeline_count * sizeof(u8), cudaMemcpyDeviceToHost) != (cudaError_t) CUDA_SUCCESS ) {
         retval = -1;
         fprintf(stderr, "RC5 cuda: ERROR: cudaMemcpy: cuda_results\r\n");
         goto error_exit;
       }
 
       /* Check the results array for any matches. */
-      for(j = 0; j < process_amount; j++) {
-
+      for (j = 0; j < (process_amount + pipeline_count - 1) / pipeline_count; j++) {
+        for (k = 0; k < pipeline_count; k++) {
         /* Check if we have found a partial match */
-        if(results[j] > 0) {
+        if(((results[j] >> 2*k) & 3) > 0) {
           rc5_72unitwork->check.count++;
           match_count++;
 
@@ -287,22 +287,22 @@ static s32 CDECL rc5_72_run_cuda_2(RC5_72UnitWork *rc5_72unitwork, u32 *iteratio
           rc5_72unitwork->check.lo = rc5_72unitwork->L0.lo;
 
           /* Offset the key index to match out current position */
-          increment_L0(&rc5_72unitwork->check.hi, &rc5_72unitwork->check.mid, &rc5_72unitwork->check.lo, j);
+          increment_L0(&rc5_72unitwork->check.hi, &rc5_72unitwork->check.mid, &rc5_72unitwork->check.lo, pipeline_count * j + k);
 
           /* Check if we have found an exact match */
-          if(results[j] > 1) {
+          if(((results[j] >> 2*k) & 3) > 1) {
             /* Correct the L0 offste value */
-            increment_L0(&rc5_72unitwork->L0.hi, &rc5_72unitwork->L0.mid, &rc5_72unitwork->L0.lo, j);
+            increment_L0(&rc5_72unitwork->L0.hi, &rc5_72unitwork->L0.mid, &rc5_72unitwork->L0.lo, pipeline_count * j + k);
 
             /* Pass back the iterations count to the callee */
-            *iterations = i + j;
+            *iterations = i + pipeline_count * j + k;
 
             /* Update the return value and jump to the exit point */
             retval = RESULT_FOUND;
             goto sucess_exit;
           }
         }
-
+      }
       }       /* for(... j < process_amount ...) */
 
     }     /* if(match_found) */
@@ -386,9 +386,9 @@ __global__ void cuda_2pipe(const u32 plain_hi, const u32 plain_lo,
   int tx = threadIdx.x;
 
   /* RC5 local state variables */
-  u32 A1, A2, B1, B2;
-  u32 S1[26], S2[26];
-  u32 L1[3], L2[3];
+  u32 A, B;
+  u32 S[26];
+  u32 L[3];
 
   /* Drop out early if we don't have any data to process */
   if( ((bx * bd) + tx) > process_amount) {
@@ -401,7 +401,7 @@ __global__ void cuda_2pipe(const u32 plain_hi, const u32 plain_lo,
   }
 
   /* Initialize the S[] with constants */
-#define KEY_INIT(i) S1[i] = S2[i] = P + i*Q;
+#define KEY_INIT(i) S[i] = P + i*Q;
   KEY_INIT(0);
   KEY_INIT(1);
   KEY_INIT(2);
@@ -431,11 +431,10 @@ __global__ void cuda_2pipe(const u32 plain_hi, const u32 plain_lo,
 
   /* Initialize L0[] based on our block    */
   /* and thread index.                     */
-  L1[2] = L2[2] = L0_hi;
-  L1[1] = L2[1] = L0_mid;
-  L1[0] = L2[0] = L0_lo;
-  increment_L0(&L1[2], &L1[1], &L1[0], 2*((bx * bd) + tx));
-  increment_L0(&L2[2], &L2[1], &L2[0], 2*((bx * bd) + tx) + 0x01);
+  L[2] = L0_hi;
+  L[1] = L0_mid;
+  L[0] = L0_lo;
+  increment_L0(&L[2], &L[1], &L[0], 2*((bx * bd) + tx));
 
   /* ------------------------------------- */
   /* ------------------------------------- */
@@ -444,41 +443,30 @@ __global__ void cuda_2pipe(const u32 plain_hi, const u32 plain_lo,
 #define ROTL_BLOCK(i,j) ROTL_BLOCK_j ## j (i)
 
 #define ROTL_BLOCK_i0_j1 \
-  S1[0] = ROTL3(S1[0]+(S1[25]+L1[0])); \
-  S2[0] = ROTL3(S2[0]+(S2[25]+L2[0])); \
-  L1[1] = ROTL(L1[1]+(S1[0]+L1[0]),(S1[0]+L1[0])); \
-  L2[1] = ROTL(L2[1]+(S2[0]+L2[0]),(S2[0]+L2[0])); \
+  S[0] = ROTL3(S[0]+(S[25]+L[0])); \
+  L[1] = ROTL(L[1]+(S[0]+L[0]),(S[0]+L[0])); \
 
 #define ROTL_BLOCK_i0_j2 \
-  S1[0] = ROTL3(S1[0]+(S1[25]+L1[1])); \
-  S2[0] = ROTL3(S2[0]+(S2[25]+L2[1])); \
-  L1[2] = ROTL(L1[2]+(S1[0]+L1[1]),(S1[0]+L1[1])); \
-  L2[2] = ROTL(L2[2]+(S2[0]+L2[1]),(S2[0]+L2[1])); \
+  S[0] = ROTL3(S[0]+(S[25]+L[1])); \
+  L[2] = ROTL(L[2]+(S[0]+L[1]),(S[0]+L[1])); \
 
 #define ROTL_BLOCK_j0(i) \
-  S1[i] = ROTL3(S1[i]+(S1[i-1]+L1[2])); \
-  S2[i] = ROTL3(S2[i]+(S2[i-1]+L2[2])); \
-  L1[0] = ROTL(L1[0]+(S1[i]+L1[2]),(S1[i]+L1[2])); \
-  L2[0] = ROTL(L2[0]+(S2[i]+L2[2]),(S2[i]+L2[2])); \
+  S[i] = ROTL3(S[i]+(S[i-1]+L[2])); \
+  L[0] = ROTL(L[0]+(S[i]+L[2]),(S[i]+L[2])); \
 
 #define ROTL_BLOCK_j1(i) \
-  S1[i] = ROTL3(S1[i]+(S1[i-1]+L1[0])); \
-  S2[i] = ROTL3(S2[i]+(S2[i-1]+L2[0])); \
-  L1[1] = ROTL(L1[1]+(S1[i]+L1[0]),(S1[i]+L1[0])); \
-  L2[1] = ROTL(L2[1]+(S2[i]+L2[0]),(S2[i]+L2[0])); \
+  S[i] = ROTL3(S[i]+(S[i-1]+L[0])); \
+  L[1] = ROTL(L[1]+(S[i]+L[0]),(S[i]+L[0])); \
 
 #define ROTL_BLOCK_j2(i) \
-  S1[i] = ROTL3(S1[i]+(S1[i-1]+L1[1])); \
-  S2[i] = ROTL3(S2[i]+(S2[i-1]+L2[1])); \
-  L1[2] = ROTL(L1[2]+(S1[i]+L1[1]),(S1[i]+L1[1])); \
-  L2[2] = ROTL(L2[2]+(S2[i]+L2[1]),(S2[i]+L2[1])); \
+  S[i] = ROTL3(S[i]+(S[i-1]+L[1])); \
+  L[2] = ROTL(L[2]+(S[i]+L[1]),(S[i]+L[1])); \
+
 
   /* ---------- */
 
-  S1[0] = ROTL3(S1[0]);
-  S2[0] = ROTL3(S2[0]);
-  L1[0] = ROTL(L1[0]+S1[0],S1[0]);
-  L2[0] = ROTL(L2[0]+S2[0],S2[0]);
+  S[0] = ROTL3(S[0]);
+  L[0] = ROTL(L[0]+S[0],S[0]);
 
   /* ---------- */
 
@@ -574,18 +562,14 @@ __global__ void cuda_2pipe(const u32 plain_hi, const u32 plain_lo,
 
   /* ---------- */
 
-  A1 = plain_lo + S1[0];
-  A2 = plain_lo + S2[0];
-  B1 = plain_hi + S1[1];
-  B2 = plain_hi + S2[1];
+  A = plain_lo + S[0];
+  B = plain_hi + S[1];
 
   /* ---------- */
 
 #define FINAL_BLOCK(i) \
-  A1 = ROTL(A1^B1,B1)+S1[2*i]; \
-  A2 = ROTL(A2^B2,B2)+S2[2*i]; \
-  B1 = ROTL(B1^A1,A1)+S1[2*i+1]; \
-  B2 = ROTL(B2^A2,A2)+S2[2*i+1];
+  A = ROTL(A^B,B)+S[2*i]; \
+  B = ROTL(B^A,A)+S[2*i+1];
 
   FINAL_BLOCK(1);
   FINAL_BLOCK(2);
@@ -605,37 +589,200 @@ __global__ void cuda_2pipe(const u32 plain_hi, const u32 plain_lo,
   /* ------------------------------------- */
 
   /* Check the results for a match.        */
-  if (A1 == cypher_lo) {
+  if (A == cypher_lo) {
 
     /* Set the match_found flag */
     *match_found = 1;
 
     /* Record the "check_*" match   */
     /* in the results array.        */
-    results[2*((bx * bd) + tx)] = 1;
+    results[(bx * bd) + tx] = 1;
 
-    if (B1 == cypher_hi) {
+    if (B == cypher_hi) {
       /* Record the RESULT_FOUND match  */
       /* in the results array.          */
-      results[2*((bx * bd) + tx)] = 2;
+      results[(bx * bd) + tx] = 2;
     }
   }
 
-  if (A2 == cypher_lo) {
+  /* Now test second key */
+
+  /* Initialize the S[] with constants */
+
+  KEY_INIT(0);
+  KEY_INIT(1);
+  KEY_INIT(2);
+  KEY_INIT(3);
+  KEY_INIT(4);
+  KEY_INIT(5);
+  KEY_INIT(6);
+  KEY_INIT(7);
+  KEY_INIT(8);
+  KEY_INIT(9);
+  KEY_INIT(10);
+  KEY_INIT(11);
+  KEY_INIT(12);
+  KEY_INIT(13);
+  KEY_INIT(14);
+  KEY_INIT(15);
+  KEY_INIT(16);
+  KEY_INIT(17);
+  KEY_INIT(18);
+  KEY_INIT(19);
+  KEY_INIT(20);
+  KEY_INIT(21);
+  KEY_INIT(22);
+  KEY_INIT(23);
+  KEY_INIT(24);
+  KEY_INIT(25);
+
+  /* Initialize L0[] based on our block    */
+  /* and thread index.                     */
+  L[2] = L0_hi;
+  L[1] = L0_mid;
+  L[0] = L0_lo;
+  increment_L0(&L[2], &L[1], &L[0], 2*((bx * bd) + tx)+1);
+
+  /* ------------------------------------- */
+  /* ------------------------------------- */
+  /* ------------------------------------- */
+
+  S[0] = ROTL3(S[0]);
+  L[0] = ROTL(L[0]+S[0],S[0]);
+
+  /* ---------- */
+
+  ROTL_BLOCK(1,1);
+  ROTL_BLOCK(2,2);
+  ROTL_BLOCK(3,0);
+  ROTL_BLOCK(4,1);
+  ROTL_BLOCK(5,2);
+  ROTL_BLOCK(6,0);
+  ROTL_BLOCK(7,1);
+  ROTL_BLOCK(8,2);
+  ROTL_BLOCK(9,0);
+  ROTL_BLOCK(10,1);
+  ROTL_BLOCK(11,2);
+  ROTL_BLOCK(12,0);
+  ROTL_BLOCK(13,1);
+  ROTL_BLOCK(14,2);
+  ROTL_BLOCK(15,0);
+  ROTL_BLOCK(16,1);
+  ROTL_BLOCK(17,2);
+  ROTL_BLOCK(18,0);
+  ROTL_BLOCK(19,1);
+  ROTL_BLOCK(20,2);
+  ROTL_BLOCK(21,0);
+  ROTL_BLOCK(22,1);
+  ROTL_BLOCK(23,2);
+  ROTL_BLOCK(24,0);
+  ROTL_BLOCK(25,1);
+
+  /* ---------- */
+
+  ROTL_BLOCK_i0_j2;
+
+  /* ---------- */
+
+  ROTL_BLOCK(1,0);
+  ROTL_BLOCK(2,1);
+  ROTL_BLOCK(3,2);
+  ROTL_BLOCK(4,0);
+  ROTL_BLOCK(5,1);
+  ROTL_BLOCK(6,2);
+  ROTL_BLOCK(7,0);
+  ROTL_BLOCK(8,1);
+  ROTL_BLOCK(9,2);
+  ROTL_BLOCK(10,0);
+  ROTL_BLOCK(11,1);
+  ROTL_BLOCK(12,2);
+  ROTL_BLOCK(13,0);
+  ROTL_BLOCK(14,1);
+  ROTL_BLOCK(15,2);
+  ROTL_BLOCK(16,0);
+  ROTL_BLOCK(17,1);
+  ROTL_BLOCK(18,2);
+  ROTL_BLOCK(19,0);
+  ROTL_BLOCK(20,1);
+  ROTL_BLOCK(21,2);
+  ROTL_BLOCK(22,0);
+  ROTL_BLOCK(23,1);
+  ROTL_BLOCK(24,2);
+  ROTL_BLOCK(25,0);
+
+  /* ---------- */
+
+  ROTL_BLOCK_i0_j1;
+
+  /* ---------- */
+
+  ROTL_BLOCK(1,2);
+  ROTL_BLOCK(2,0);
+  ROTL_BLOCK(3,1);
+  ROTL_BLOCK(4,2);
+  ROTL_BLOCK(5,0);
+  ROTL_BLOCK(6,1);
+  ROTL_BLOCK(7,2);
+  ROTL_BLOCK(8,0);
+  ROTL_BLOCK(9,1);
+  ROTL_BLOCK(10,2);
+  ROTL_BLOCK(11,0);
+  ROTL_BLOCK(12,1);
+  ROTL_BLOCK(13,2);
+  ROTL_BLOCK(14,0);
+  ROTL_BLOCK(15,1);
+  ROTL_BLOCK(16,2);
+  ROTL_BLOCK(17,0);
+  ROTL_BLOCK(18,1);
+  ROTL_BLOCK(19,2);
+  ROTL_BLOCK(20,0);
+  ROTL_BLOCK(21,1);
+  ROTL_BLOCK(22,2);
+  ROTL_BLOCK(23,0);
+  ROTL_BLOCK(24,1);
+  ROTL_BLOCK(25,2);
+
+  /* ---------- */
+
+  A = plain_lo + S[0];
+  B = plain_hi + S[1];
+
+  /* ---------- */
+
+  FINAL_BLOCK(1);
+  FINAL_BLOCK(2);
+  FINAL_BLOCK(3);
+  FINAL_BLOCK(4);
+  FINAL_BLOCK(5);
+  FINAL_BLOCK(6);
+  FINAL_BLOCK(7);
+  FINAL_BLOCK(8);
+  FINAL_BLOCK(9);
+  FINAL_BLOCK(10);
+  FINAL_BLOCK(11);
+  FINAL_BLOCK(12);
+
+  /* ------------------------------------- */
+  /* ------------------------------------- */
+  /* ------------------------------------- */
+
+  /* Check the results for a match.        */
+  if (A == cypher_lo) {
 
     /* Set the match_found flag */
     *match_found = 1;
 
     /* Record the "check_*" match   */
     /* in the results array.        */
-    results[2*((bx * bd) + tx)+1] = 1;
+    results[(bx * bd) + tx] += (1<<2);
 
-    if (B2 == cypher_hi) {
+    if (B == cypher_hi) {
       /* Record the RESULT_FOUND match  */
       /* in the results array.          */
-      results[2*((bx * bd) + tx)+1] = 2;
+      results[(bx * bd) + tx] += (1<<2); /* add another 1 */
     }
   }
+  
 }
 
 // vim: syntax=cpp
