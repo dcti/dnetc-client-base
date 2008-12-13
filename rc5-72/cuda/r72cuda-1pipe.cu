c45a10b0d88a96efbeddbfa1cc7b2063c0a18996
Andreas Beckmann <andreasb@distributed.net>
2008-12-13 14:25:46 +0000

    reindent the 1-pipe core to match the 2,4-pipe cores

--- rc5-72/cuda/r72cuda-1pipe.cu
+++ rc5-72/cuda/r72cuda-1pipe.cu
@@ -299,33 +299,34 @@ static s32 CDECL rc5_72_run_cuda_1(RC5_72UnitWork *rc5_72unitwork, u32 *iteratio
       /* Check the results array for any matches. */
       for(j = 0; j < process_amount; j++) {
 
-        /* Check if we have found a partial match */
-        if(results[j] > 0) {
-          rc5_72unitwork->check.count++;
-          match_count++;
+        {
+          /* Check if we have found a partial match */
+          if(results[j] > 0) {
+            rc5_72unitwork->check.count++;
+            match_count++;
 
-          /* Copy over the current key */
-          rc5_72unitwork->check.hi = rc5_72unitwork->L0.hi;
-          rc5_72unitwork->check.mid = rc5_72unitwork->L0.mid;
-          rc5_72unitwork->check.lo = rc5_72unitwork->L0.lo;
+            /* Copy over the current key */
+            rc5_72unitwork->check.hi = rc5_72unitwork->L0.hi;
+            rc5_72unitwork->check.mid = rc5_72unitwork->L0.mid;
+            rc5_72unitwork->check.lo = rc5_72unitwork->L0.lo;
 
-          /* Offset the key index to match out current position */
-          increment_L0(&rc5_72unitwork->check.hi, &rc5_72unitwork->check.mid, &rc5_72unitwork->check.lo, j);
+            /* Offset the key index to match out current position */
+            increment_L0(&rc5_72unitwork->check.hi, &rc5_72unitwork->check.mid, &rc5_72unitwork->check.lo, j);
 
-          /* Check if we have found an exact match */
-          if(results[j] > 1) {
-            /* Correct the L0 offste value */
-            increment_L0(&rc5_72unitwork->L0.hi, &rc5_72unitwork->L0.mid, &rc5_72unitwork->L0.lo, j);
+            /* Check if we have found an exact match */
+            if(results[j] > 1) {
+              /* Correct the L0 offste value */
+              increment_L0(&rc5_72unitwork->L0.hi, &rc5_72unitwork->L0.mid, &rc5_72unitwork->L0.lo, j);
 
-            /* Pass back the iterations count to the callee */
-            *iterations = i + j;
+              /* Pass back the iterations count to the callee */
+              *iterations = i + j;
 
-            /* Update the return value and jump to the exit point */
-            retval = RESULT_FOUND;
-            goto sucess_exit;
+              /* Update the return value and jump to the exit point */
+              retval = RESULT_FOUND;
+              goto sucess_exit;
+            }
           }
         }
-
       }       /* for(... j < process_amount ...) */
 
     }     /* if(match_found) */
@@ -397,7 +398,7 @@ __global__ void cuda_1pipe(const u32 plain_hi, const u32 plain_lo,
                            const u32 process_amount, u8 * results, u8 * match_found)
 {
   /* Grid of blocks dimension */
-//	int gd = gridDim.x;
+  //int gd = gridDim.x;
 
   /* Block index */
   int bx = blockIdx.x;
@@ -423,45 +424,46 @@ __global__ void cuda_1pipe(const u32 plain_hi, const u32 plain_lo,
     return;
   }
 
-  /* Initialize the S[] with constants */
+  {
+    /* Initialize the S[] with constants */
 #define KEY_INIT(i) S[i] = P + i*Q;
-  KEY_INIT(0);
-  KEY_INIT(1);
-  KEY_INIT(2);
-  KEY_INIT(3);
-  KEY_INIT(4);
-  KEY_INIT(5);
-  KEY_INIT(6);
-  KEY_INIT(7);
-  KEY_INIT(8);
-  KEY_INIT(9);
-  KEY_INIT(10);
-  KEY_INIT(11);
-  KEY_INIT(12);
-  KEY_INIT(13);
-  KEY_INIT(14);
-  KEY_INIT(15);
-  KEY_INIT(16);
-  KEY_INIT(17);
-  KEY_INIT(18);
-  KEY_INIT(19);
-  KEY_INIT(20);
-  KEY_INIT(21);
-  KEY_INIT(22);
-  KEY_INIT(23);
-  KEY_INIT(24);
-  KEY_INIT(25);
+    KEY_INIT(0);
+    KEY_INIT(1);
+    KEY_INIT(2);
+    KEY_INIT(3);
+    KEY_INIT(4);
+    KEY_INIT(5);
+    KEY_INIT(6);
+    KEY_INIT(7);
+    KEY_INIT(8);
+    KEY_INIT(9);
+    KEY_INIT(10);
+    KEY_INIT(11);
+    KEY_INIT(12);
+    KEY_INIT(13);
+    KEY_INIT(14);
+    KEY_INIT(15);
+    KEY_INIT(16);
+    KEY_INIT(17);
+    KEY_INIT(18);
+    KEY_INIT(19);
+    KEY_INIT(20);
+    KEY_INIT(21);
+    KEY_INIT(22);
+    KEY_INIT(23);
+    KEY_INIT(24);
+    KEY_INIT(25);
 
-  /* Initialize L0[] based on our block    */
-  /* and thread index.                     */
-  L[2] = L0_hi;
-  L[1] = L0_mid;
-  L[0] = L0_lo;
-  increment_L0(&L[2], &L[1], &L[0], (bx * bd) + tx);
+    /* Initialize L0[] based on our block    */
+    /* and thread index.                     */
+    L[2] = L0_hi;
+    L[1] = L0_mid;
+    L[0] = L0_lo;
+    increment_L0(&L[2], &L[1], &L[0], (bx * bd) + tx);
 
-  /* ------------------------------------- */
-  /* ------------------------------------- */
-  /* ------------------------------------- */
+    /* ------------------------------------- */
+    /* ------------------------------------- */
+    /* ------------------------------------- */
 
 #define ROTL_BLOCK(i,j) ROTL_BLOCK_j ## j (i)
 
@@ -485,144 +487,146 @@ __global__ void cuda_1pipe(const u32 plain_hi, const u32 plain_lo,
   S[i] = ROTL3(S[i]+(S[i-1]+L[1])); \
   L[2] = ROTL(L[2]+(S[i]+L[1]),(S[i]+L[1])); \
 
-  /* ---------- */
+    /* ---------- */
 
-  S[0] = ROTL3(S[0]);
-  L[0] = ROTL(L[0]+S[0],S[0]);
+    S[0] = ROTL3(S[0]);
+    L[0] = ROTL(L[0]+S[0],S[0]);
 
-  /* ---------- */
+    /* ---------- */
 
-  ROTL_BLOCK(1,1);
-  ROTL_BLOCK(2,2);
-  ROTL_BLOCK(3,0);
-  ROTL_BLOCK(4,1);
-  ROTL_BLOCK(5,2);
-  ROTL_BLOCK(6,0);
-  ROTL_BLOCK(7,1);
-  ROTL_BLOCK(8,2);
-  ROTL_BLOCK(9,0);
-  ROTL_BLOCK(10,1);
-  ROTL_BLOCK(11,2);
-  ROTL_BLOCK(12,0);
-  ROTL_BLOCK(13,1);
-  ROTL_BLOCK(14,2);
-  ROTL_BLOCK(15,0);
-  ROTL_BLOCK(16,1);
-  ROTL_BLOCK(17,2);
-  ROTL_BLOCK(18,0);
-  ROTL_BLOCK(19,1);
-  ROTL_BLOCK(20,2);
-  ROTL_BLOCK(21,0);
-  ROTL_BLOCK(22,1);
-  ROTL_BLOCK(23,2);
-  ROTL_BLOCK(24,0);
-  ROTL_BLOCK(25,1);
+    ROTL_BLOCK(1,1);
+    ROTL_BLOCK(2,2);
+    ROTL_BLOCK(3,0);
+    ROTL_BLOCK(4,1);
+    ROTL_BLOCK(5,2);
+    ROTL_BLOCK(6,0);
+    ROTL_BLOCK(7,1);
+    ROTL_BLOCK(8,2);
+    ROTL_BLOCK(9,0);
+    ROTL_BLOCK(10,1);
+    ROTL_BLOCK(11,2);
+    ROTL_BLOCK(12,0);
+    ROTL_BLOCK(13,1);
+    ROTL_BLOCK(14,2);
+    ROTL_BLOCK(15,0);
+    ROTL_BLOCK(16,1);
+    ROTL_BLOCK(17,2);
+    ROTL_BLOCK(18,0);
+    ROTL_BLOCK(19,1);
+    ROTL_BLOCK(20,2);
+    ROTL_BLOCK(21,0);
+    ROTL_BLOCK(22,1);
+    ROTL_BLOCK(23,2);
+    ROTL_BLOCK(24,0);
+    ROTL_BLOCK(25,1);
 
-  /* ---------- */
+    /* ---------- */
 
-  ROTL_BLOCK_i0_j2;
+    ROTL_BLOCK_i0_j2;
 
-  /* ---------- */
+    /* ---------- */
 
-  ROTL_BLOCK(1,0);
-  ROTL_BLOCK(2,1);
-  ROTL_BLOCK(3,2);
-  ROTL_BLOCK(4,0);
-  ROTL_BLOCK(5,1);
-  ROTL_BLOCK(6,2);
-  ROTL_BLOCK(7,0);
-  ROTL_BLOCK(8,1);
-  ROTL_BLOCK(9,2);
-  ROTL_BLOCK(10,0);
-  ROTL_BLOCK(11,1);
-  ROTL_BLOCK(12,2);
-  ROTL_BLOCK(13,0);
-  ROTL_BLOCK(14,1);
-  ROTL_BLOCK(15,2);
-  ROTL_BLOCK(16,0);
-  ROTL_BLOCK(17,1);
-  ROTL_BLOCK(18,2);
-  ROTL_BLOCK(19,0);
-  ROTL_BLOCK(20,1);
-  ROTL_BLOCK(21,2);
-  ROTL_BLOCK(22,0);
-  ROTL_BLOCK(23,1);
-  ROTL_BLOCK(24,2);
-  ROTL_BLOCK(25,0);
+    ROTL_BLOCK(1,0);
+    ROTL_BLOCK(2,1);
+    ROTL_BLOCK(3,2);
+    ROTL_BLOCK(4,0);
+    ROTL_BLOCK(5,1);
+    ROTL_BLOCK(6,2);
+    ROTL_BLOCK(7,0);
+    ROTL_BLOCK(8,1);
+    ROTL_BLOCK(9,2);
+    ROTL_BLOCK(10,0);
+    ROTL_BLOCK(11,1);
+    ROTL_BLOCK(12,2);
+    ROTL_BLOCK(13,0);
+    ROTL_BLOCK(14,1);
+    ROTL_BLOCK(15,2);
+    ROTL_BLOCK(16,0);
+    ROTL_BLOCK(17,1);
+    ROTL_BLOCK(18,2);
+    ROTL_BLOCK(19,0);
+    ROTL_BLOCK(20,1);
+    ROTL_BLOCK(21,2);
+    ROTL_BLOCK(22,0);
+    ROTL_BLOCK(23,1);
+    ROTL_BLOCK(24,2);
+    ROTL_BLOCK(25,0);
 
-  /* ---------- */
+    /* ---------- */
 
-  ROTL_BLOCK_i0_j1;
+    ROTL_BLOCK_i0_j1;
 
-  /* ---------- */
+    /* ---------- */
 
-  ROTL_BLOCK(1,2);
-  ROTL_BLOCK(2,0);
-  ROTL_BLOCK(3,1);
-  ROTL_BLOCK(4,2);
-  ROTL_BLOCK(5,0);
-  ROTL_BLOCK(6,1);
-  ROTL_BLOCK(7,2);
-  ROTL_BLOCK(8,0);
-  ROTL_BLOCK(9,1);
-  ROTL_BLOCK(10,2);
-  ROTL_BLOCK(11,0);
-  ROTL_BLOCK(12,1);
-  ROTL_BLOCK(13,2);
-  ROTL_BLOCK(14,0);
-  ROTL_BLOCK(15,1);
-  ROTL_BLOCK(16,2);
-  ROTL_BLOCK(17,0);
-  ROTL_BLOCK(18,1);
-  ROTL_BLOCK(19,2);
-  ROTL_BLOCK(20,0);
-  ROTL_BLOCK(21,1);
-  ROTL_BLOCK(22,2);
-  ROTL_BLOCK(23,0);
-  ROTL_BLOCK(24,1);
-  ROTL_BLOCK(25,2);
+    ROTL_BLOCK(1,2);
+    ROTL_BLOCK(2,0);
+    ROTL_BLOCK(3,1);
+    ROTL_BLOCK(4,2);
+    ROTL_BLOCK(5,0);
+    ROTL_BLOCK(6,1);
+    ROTL_BLOCK(7,2);
+    ROTL_BLOCK(8,0);
+    ROTL_BLOCK(9,1);
+    ROTL_BLOCK(10,2);
+    ROTL_BLOCK(11,0);
+    ROTL_BLOCK(12,1);
+    ROTL_BLOCK(13,2);
+    ROTL_BLOCK(14,0);
+    ROTL_BLOCK(15,1);
+    ROTL_BLOCK(16,2);
+    ROTL_BLOCK(17,0);
+    ROTL_BLOCK(18,1);
+    ROTL_BLOCK(19,2);
+    ROTL_BLOCK(20,0);
+    ROTL_BLOCK(21,1);
+    ROTL_BLOCK(22,2);
+    ROTL_BLOCK(23,0);
+    ROTL_BLOCK(24,1);
+    ROTL_BLOCK(25,2);
 
-  /* ---------- */
+    /* ---------- */
 
-  A = plain_lo + S[0];
-  B = plain_hi + S[1];
+    A = plain_lo + S[0];
+    B = plain_hi + S[1];
 
-  /* ---------- */
+    /* ---------- */
 
 #define FINAL_BLOCK(i) \
   A = ROTL(A^B,B)+S[2*i]; \
   B = ROTL(B^A,A)+S[2*i+1];
-  FINAL_BLOCK(1);
-  FINAL_BLOCK(2);
-  FINAL_BLOCK(3);
-  FINAL_BLOCK(4);
-  FINAL_BLOCK(5);
-  FINAL_BLOCK(6);
-  FINAL_BLOCK(7);
-  FINAL_BLOCK(8);
-  FINAL_BLOCK(9);
-  FINAL_BLOCK(10);
-  FINAL_BLOCK(11);
-  FINAL_BLOCK(12);
 
-  /* ------------------------------------- */
-  /* ------------------------------------- */
-  /* ------------------------------------- */
+    FINAL_BLOCK(1);
+    FINAL_BLOCK(2);
+    FINAL_BLOCK(3);
+    FINAL_BLOCK(4);
+    FINAL_BLOCK(5);
+    FINAL_BLOCK(6);
+    FINAL_BLOCK(7);
+    FINAL_BLOCK(8);
+    FINAL_BLOCK(9);
+    FINAL_BLOCK(10);
+    FINAL_BLOCK(11);
+    FINAL_BLOCK(12);
 
-  /* Check the results for a match.        */
-  if (A == cypher_lo) {
+    /* ------------------------------------- */
+    /* ------------------------------------- */
+    /* ------------------------------------- */
 
-    /* Set the match_found flag */
-    *match_found = 1;
+    /* Check the results for a match.        */
+    if (A == cypher_lo) {
 
-    /* Record the "check_*" match   */
-    /* in the results array.        */
-    results[(bx * bd) + tx] = 1;
+      /* Set the match_found flag */
+      *match_found = 1;
 
-    if (B == cypher_hi) {
-      /* Record the RESULT_FOUND match  */
-      /* in the results array.          */
-      results[(bx * bd) + tx] = 2;
+      /* Record the "check_*" match   */
+      /* in the results array.        */
+      results[(bx * bd) + tx] = 1;
+
+      if (B == cypher_hi) {
+        /* Record the RESULT_FOUND match  */
+        /* in the results array.          */
+        results[(bx * bd) + tx] = 2;
+      }
     }
   }
 }
