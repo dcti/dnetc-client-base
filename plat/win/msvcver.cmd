ac46cc55c644b2932e57649c75f1a3af0b92f4ab
Jeff Lawson <bovine@distributed.net>
2009-01-05 08:37:45 +0000

    customize msvc command-line options using compile-time version detection.

---
+++ plat/win/msvcver.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+rem Exit with ERRORLEVEL containing the major version number reported by the Microsoft CL compiler.
+rem This is useful for doing version-dependent behavior inside Makefiles.
+rem An exit value of 0 indicates no CL was found in the PATH.
+
+for %%i in (cl.exe) do if "%%~$PATH:i"=="" goto notfound
+
+set msvcversion=
+for /f "eol=. tokens=6-8 usebackq " %%i in (`cmd /c "cl.exe /D 2>&1 | findstr Version"`) do (
+  if "%%i"=="Version" set msvcversion=%%j
+  if "%%j"=="Version" set msvcversion=%%k
+)
+if "%msvcversion%"=="" goto notfound
+exit /b %msvcversion:~0,2%
+
+:notfound
+exit /b 0
