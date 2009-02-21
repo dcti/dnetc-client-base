561d139f5f53274e6d248c11220f218f44c61546
Andreas Beckmann <andreasb@distributed.net>
2009-02-21 10:53:10 +0000

    fix typo in comment

--- common/pack2.h
+++ common/pack2.h
@@ -1,18 +1,18 @@
 /* Hey, Emacs, this a -*-C-*- file !
  *
- * Copyright distributed.net 1997-2003 - All Rights Reserved
+ * Copyright distributed.net 1997-2009 - All Rights Reserved
  * For use in distributed.net projects only.
  * Any other distribution or use of this source violates copyright.
 */
 
 #ifndef __PACK2_H__
-# define __PACK2_H__ "@(#)$Id: pack2.h,v 1.3 2007/10/22 16:48:26 jlawson Exp $"
+# define __PACK2_H__ "@(#)$Id: pack2.h,v 1.4 2009/02/21 10:53:10 andreasb Exp $"
 
 # include "pack.h"
 #endif
 
 /* make sure we don't get confused by predefined macros and initialise
-** DNETC_PACKED to do 4-byte alignment */
+** DNETC_PACKED to do 2-byte alignment */
 #undef DNETC_PACKED
 #define DNETC_PACKED DNETC_ALIGNED2
 
