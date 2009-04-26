1b6bc184e453ae24731db2763ea4423e1d8b2d68
Roman Trunov <stream@distributed.net>
2009-04-26 08:57:50 +0000

    Improved speed of OGR-NG MMX core using ideas from SSE core.

--- ogr/x86/ogrng-b-asm-rt.asm
+++ ogr/x86/ogrng-b-asm-rt.asm
@@ -1,3 +1,20 @@
+;
+; OGR-NG x86 assembly MMX core. Created by Roman Trunov <stream@distributed.net>
+;
+; Uses only basic MMX instruction set. Can be used on any MMX-capable CPU.
+; Created and tuned on PII. Optimized for processors with "classic" structure
+; like Intel PII, PIII and Core series.
+;
+; 2009-04-26: Added few things suggested by Craig Johnston in his SSE core:
+;	- 'depth' now cached in esi
+;	- update of 'mark' delayed
+;	- more oState members cached in local area
+;	- aligned stack
+;	- improved calculation of 'dist0' during update of 'limit'
+;
+; $Id: ogrng-b-asm-rt.asm,v 1.5 2009/04/26 08:57:50 stream Exp $
+;
+
 %ifdef __NASM_VER__
 	cpu	586	; NASM doesnt know feature flags but accepts mmx in 586 mode
 %else
@@ -32,18 +49,26 @@ _ogr_cycle_256_rt1_mmx:
 ogr_cycle_256_rt1_mmx:
 
 	%define regsavesize	10h	; 4 registers saved
-	%define worksize	20h
 
-	%define	work_depth	esp+00h
-	%define work_halfdepth	esp+04h
-	%define work_halfdepth2	esp+08h
-	%define work_nodes	esp+0Ch
-	%define work_maxlen_m1	esp+10h
-	%define work_half_depth_mark_addr esp+14h
+	; Work area. Sorted by order of appearance and frequency of usage
+	; Started at esp+04h (can be +00h) because something weird
+	; happens in "after_if": difference of one byte in opcode length
+	; slowing things down. Intel sucks.
 
-	%define	param_oState	esp+regsavesize+worksize+04h
-	%define param_pnodes	esp+regsavesize+worksize+08h
-	%define param_pchoose	esp+regsavesize+worksize+0Ch
+	%define worksize	30h
+
+	%define work_oState	esp+04h
+	%define work_pchoose    esp+08h
+	%define work_halfdepth2	esp+0Ch
+	%define work_nodes	esp+10h
+	%define work_stopdepth	esp+14h
+
+	%define work_halfdepth	esp+18h
+	%define work_maxlen_m1	esp+1Ch
+	%define work_half_depth_mark_addr esp+20h
+
+	%define work_pnodes     esp+24h
+	%define work_save_esp	esp+28h
 
 	%define oState_max		00h
 
@@ -73,14 +98,32 @@ ogr_cycle_256_rt1_mmx:
 	push	esi
 	push	edi
 	push	ebp			; note: regsavesize bytes pushed
-	sub	esp, worksize
 
-	mov	ebx, [param_oState]
-	mov	ecx, [ebx+oState_depth]
-	mov	[work_depth], ecx	; depth = oState->depth (cached for SETUP_TOP_STATE)
-	imul	eax, ecx, sizeof_level
-	lea	ebp, [eax+ebx+oState_Levels+ebp_shift]	; lev = &oState->Levels[oState->depth]
+	%define	param_oState	esp+regsavesize+04h
+	%define param_pnodes	esp+regsavesize+08h
+	%define param_pchoose	esp+regsavesize+0Ch
+
+	; Get parameters while esp still valid
 	mov	eax, [param_pnodes]
+	mov	ebx, [param_oState]
+	mov	ecx, [param_pchoose]
+	mov	edx, esp
+
+	sub	esp, worksize		; Reserve space on stack
+	and	esp, -64		; Align stack by 64 bytes (caching issues)
+
+	; Store parameters to work area
+	mov	[work_pnodes],   eax
+	mov	[work_oState],   ebx
+	mov	[work_pchoose],  ecx
+	mov	[work_save_esp], edx
+
+	; Initialization (no need to optimize)
+
+	mov	esi, [ebx+oState_depth]	; depth = oState->depth
+	imul	eax, esi, sizeof_level
+	lea	ebp, [eax+ebx+oState_Levels+ebp_shift]	; lev = &oState->Levels[oState->depth]
+	mov	eax, [work_pnodes]
 	mov	eax, [eax]
 	mov	[work_nodes], eax	; nodes = *pnodes
 
@@ -94,6 +137,9 @@ ogr_cycle_256_rt1_mmx:
 
 	mov	eax, [ebx+oState_half_depth2]
 	mov	[work_halfdepth2], eax	; halfdepth2 = oState->half_depth2
+
+	mov	eax, [ebx+oState_stopdepth]
+	mov	[work_stopdepth], eax
 
 	mov	eax, [ebx+oState_max]
 	dec	eax
@@ -116,7 +162,7 @@ ogr_cycle_256_rt1_mmx:
 	; movq	mm3, cur(comp, 3)	; comp[3] not cached!
 	; int newbit = (depth < oState->maxdepthm1) ? 1 : 0;
 	xor	eax, eax
-	cmp	ecx, [ebx+oState_maxdepthm1]	; depth still in ecx
+	cmp	esi, [ebx+oState_maxdepthm1]
 	setl	al
 	movd	mm_newbit, eax
 
@@ -137,39 +183,38 @@ ogr_cycle_256_rt1_mmx:
 do_loop_split:
 for_loop:
 
-	; esi = comp0
-	; edi = newbit
 	; ebx = mark
+	; esi = depth
 
 	;      if (comp0 < (SCALAR)~1) {
 %if SCALAR_BITS == 32
 
 	movq	mm_temp_a, mm_comp0
 	punpckhdq mm_temp_a, mm_temp_a
-	movd	esi, mm_temp_a
-	cmp	esi, 0FFFFFFFEh
+	movd	eax, mm_temp_a
+	cmp	eax, 0FFFFFFFEh
 	jnb	comp0_ge_fffe		; ENTERED: 0x0B5ACBE2(100%), taken: 33-35%
 	;        int s = LOOKUP_FIRSTBLANK_SAFE(comp0);
-	not	esi
+	not	eax
 	mov	ecx, 20H
-	bsr	edx, esi
-	sub	ecx, edx			; s
+	bsr	eax, eax
+	sub	ecx, eax			; s
 
 %else
 
 	movq	mm_temp_a, mm_comp0
 	punpckhdq mm_temp_a, mm_temp_a
-	movd	esi, mm_temp_a
+	movd	eax, mm_temp_a
 	mov	ecx, 32
-	xor	esi, 0FFFFFFFFh		; implies 'not esi' below
+	xor	eax, 0FFFFFFFFh		; implies 'not eax' below
 	jne	use_this_word		; ENTERED: 0x????????(??%), taken: ??%
-	movd	esi, mm_comp0
-	cmp	esi, 0FFFFFFFEh
+	movd	eax, mm_comp0
+	cmp	eax, 0FFFFFFFEh
 	jnb	comp0_ge_fffe		; ENTERED: 0x????????(??%), taken: ??%
-	not	esi
+	not	eax
 	mov	ecx, 64			; add ecx, 32 ??? shorter but no gain
 use_this_word:
-	bsr	eax, esi
+	bsr	eax, eax
 	sub	ecx, eax		; s = ecx-bsr
 
 %endif
@@ -245,15 +290,18 @@ after_if:
 	; ebx = mark
 
 	;      lev->mark = mark;
-	mov	[ebp+level_mark-ebp_shift], ebx
-
 	;      if (depth == oState->maxdepthm1) {
 	;        goto exit;         /* Ruler found */
 	;      }
-	mov	eax, [param_oState]
-	mov	eax, [eax+oState_maxdepthm1]
-	cmp	eax, [work_depth]
-	je	exit			; ENTERED: 0x0513FD1B(44.72%), taken: 0%
+
+	; Strange code, but it's fastest sequence (caching to local var
+	; or even adding/removing one byte make everything worse)
+	mov	eax, [work_oState]
+	cmp	esi, [eax+oState_maxdepthm1]
+	je	save_mark_and_exit		; ENTERED: 0x0513FD1B(44.72%), taken: 0%
+
+	;      lev->mark = mark;  (delayed)
+	mov	[ebp+level_mark-ebp_shift], ebx
 
 	;      PUSH_LEVEL_UPDATE_STATE(lev);
 	; !!!
@@ -271,6 +319,8 @@ after_if:
 	por	mm_temp_b, cur(dist, 0)		; dist0 ready in mm_temp_b
 	movq	next(dist, 0), mm_temp_b
 	por	mm_comp0, mm_temp_b
+
+	; (1) see below
 
 	movq	mm_temp_a, cur(list, 1)
 	movq	next(list, 1), mm_temp_a
@@ -297,10 +347,7 @@ after_if:
 	;      lev++;
 	add	ebp, sizeof_level
 	;      depth++;
-;	inc	dword [work_depth]
-	mov	edx, [work_depth]
-	add	edx, 1
-	mov	[work_depth], edx
+	add	esi, 1
 
 	%define CHOOSE_DIST_BITS	16     ; /* number of bits to take into account  */
 
@@ -308,20 +355,20 @@ after_if:
 	; #define choose(dist,seg) pchoose[(dist >> (SCALAR_BITS-CHOOSE_DIST_BITS)) * 32 + (seg)]
 	; limit = choose(dist0, depth);
 
-	punpckhdq mm_temp_b, mm_temp_b	; dist0 >> 32
-	movd	ecx, mm_temp_b		; dist0 in ecx
+	; Theoretically, 4 commands below can be interleaved with code starting
+	; from (1) but I failed to achieve any improvement.
 
-	mov	eax, ecx ; dist 0
+	punpckhdq mm_temp_b, mm_temp_b	; dist0 >> 32
+	movd	eax, mm_temp_b		; dist0 in eax
 	shr	eax, 16
 	shl	eax, 5
-	add	eax, edx		; depth
-	mov	edx, [param_pchoose]
+	add	eax, esi		; depth
+	mov	edx, [work_pchoose]
 	movzx	edi, word [edx+eax*2]
 
 	;      if (depth > oState->half_depth && depth <= oState->half_depth2) {
 	;;;      if (depth > halfdepth && depth <= halfdepth2) {
-	mov	eax, [work_depth]
-	cmp	eax, [work_halfdepth2]
+	cmp	esi, [work_halfdepth2]
 	jle	continue_if_depth	; ENTERED: 0x0513FD14(44.72%), NOT taken 97.02%
 
 skip_if_depth:
@@ -337,16 +384,12 @@ skip_if_depth:
 
 	dec	dword [work_nodes]
 	jg	for_loop	; ENTERED: 0x0513FD14(44.72%), taken 99.99%
-
-	;        lev->mark = mark;
-	;        goto exit;
-	mov	[ebp+level_mark-ebp_shift], ebx
-	jmp	exit
+	jmp	save_mark_and_exit
 
 	align	16
 
 continue_if_depth:
-	cmp	eax, [work_halfdepth]
+	cmp	esi, [work_halfdepth]
 	jle	skip_if_depth		; ENTERED: 0x????????(??.??%), taken 0.5%
 
 ;        int temp = maxlen_m1 - oState->Levels[oState->half_depth].mark;
@@ -357,14 +400,14 @@ continue_if_depth:
 	sub	edx, [eax]
 
 ;        if (depth < oState->half_depth2) {
-	mov	eax, [work_depth]
-	cmp	eax, [work_halfdepth2]
+	cmp	esi, [work_halfdepth2]
 	jge	update_limit_temp	; ENTERED: 0x00267D08(1.32%), taken 78.38%
 
 ;          temp -= LOOKUP_FIRSTBLANK(dist0); // "33" version
 ;;;        temp -= LOOKUP_FIRSTBLANK(dist0 & -((SCALAR)1 << 32));
-;;;        (we already have upper part of dist0 in ecx)
+;;;        (we still have shuffled dist0 in mm_temp_b)
 
+	movd	ecx, mm_temp_b	; dist0 in ecx
 	xor	ecx, -1		; "not ecx" does not set flags!
 	mov	eax, -1
 	je	skip_bsr	; ENTERED: 0x00085254(0.29%), taken 0.00%
@@ -402,7 +445,7 @@ comp0_ge_fffe:
 	;        }
 	;        COMP_LEFT_LIST_RIGHT_WORD(lev);
 
-	cmp	esi,0ffffffffH		; if (comp0 == ~0u)
+	cmp	eax,0ffffffffH		; if (comp0 == ~0u)
 
 	; COMP_LEFT_LIST_RIGHT_WORD(lev);
 	; !!!
@@ -476,7 +519,7 @@ break_for:
 	;    lev--;
 	sub	ebp, sizeof_level
 	;    depth--;
-	dec	dword [work_depth]
+	sub	esi, 1
 	;    POP_LEVEL(lev);
 	; !!!
 	movq	mm_comp0, cur(comp, 0)	; SCALAR comp0 = lev->comp[0];
@@ -486,29 +529,33 @@ break_for:
 	pxor	mm_newbit, mm_newbit	;      newbit = 0;
 
 	;  } while (depth > oState->stopdepth);
-	mov	eax, [param_oState]
-	mov	eax, [eax+oState_stopdepth]
-	cmp	eax, [work_depth]
+	cmp	esi, [work_stopdepth]
 
 	; split loop header
 	mov	ebx, [ebp+level_mark-ebp_shift]	; mark  = lev->mark;
 
-	jl	do_loop_split		; ENTERED: 0x0513FCC2(44.72%), taken 99.99%
+	jg	do_loop_split		; ENTERED: 0x0513FCC2(44.72%), taken 99.99%
 
-exit:
+	; mark was just preloaded so we can simply store it back
+
+save_mark_and_exit:
+	;        lev->mark = mark;
+	;        goto exit;
+	mov	[ebp+level_mark-ebp_shift], ebx
+;exit:
 	;  SAVE_FINAL_STATE(lev);
 	; !!!
 	movq	cur(comp, 0), mm_comp0
 	movq	cur(comp, 1), mm_comp1
 	movq	cur(comp, 2), mm_comp2
 
-	mov	ebx, [param_pnodes]	; *pnodes -= nodes;
+	mov	ebx, [work_pnodes]	; *pnodes -= nodes;
 	mov	eax, [work_nodes]
 	sub	[ebx], eax
 
-	mov	eax, [work_depth]	; return depth;
+	mov	eax, esi		; return depth;
 
-	add	esp, worksize
+	mov	esp, [work_save_esp]
 	pop	ebp
 	pop	edi
 	pop	esi
