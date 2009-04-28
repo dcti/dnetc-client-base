81fd34ab7871b951c087fabfccae7b750a18f907
Jeff Lawson <bovine@distributed.net>
2009-04-28 05:41:25 +0000

    comments

--- plat/win/msvcver.cmd
+++ plat/win/msvcver.cmd
@@ -1,8 +1,15 @@
 @echo off
 
-rem Exit with ERRORLEVEL containing the major version number reported by the Microsoft CL compiler.
+rem Exit with ERRORLEVEL containing the major version number reported
+rem by the Microsoft CL compiler.
 rem This is useful for doing version-dependent behavior inside Makefiles.
 rem An exit value of 0 indicates no CL was found in the PATH.
+rem 
+rem   cl 12.00.xxxx = Visual Studio 6 (VC6/VC98)
+rem   cl 13.00.xxxx = Visual Studio.NET 2002 (VC7.0)
+rem   cl 13.10.xxxx = Visual Studio.NET 2003 (VC7.1)
+rem   cl 14.00.xxxx = Visual Studio 2005 (VC8)
+rem   cl 15.00.xxxx = Visual Studio 2008 (VC9)
 
 for %%i in (cl.exe) do if "%%~$PATH:i"=="" goto notfound
 
