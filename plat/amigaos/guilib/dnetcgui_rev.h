9558c83d5e157680fa5997ee5ba7cd0136fa3e71
Oliver Roberts <oliver@distributed.net>
2010-12-17 12:12:17 +0000

    version bump

--- plat/amigaos/guilib/dnetcgui_rev.h
+++ plat/amigaos/guilib/dnetcgui_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	12
-#define DATE		"16.2.2009"
-#define VERS		"dnetcgui 1.12"
-#define VSTRING		"dnetcgui 1.12 (16.2.2009)\r\n"
-#define VERSTAG		"\0$VER: dnetcgui 1.12 (16.2.2009)"
+#define REVISION	13
+#define DATE		"17.12.2010"
+#define VERS		"dnetcgui 1.13"
+#define VSTRING		"dnetcgui 1.13 (17.12.2010)\r\n"
+#define VERSTAG		"\0$VER: dnetcgui 1.13 (17.12.2010)"
