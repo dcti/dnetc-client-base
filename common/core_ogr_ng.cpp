576ad865cbff7f003218f30a1e038722f8e5e832
zebe <github@zebe.net>
2015-06-28 00:08:39 -0700

    initial commit of arm64 support
    adds cpu type and conditionals, allows for use of 64-bit ANSI core for
    OGR. completely untested!

--- common/core_ogr_ng.cpp
+++ common/core_ogr_ng.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright distributed.net 1998-2009 - All Rights Reserved
+ * Copyright distributed.net 1998-2015 - All Rights Reserved
  * For use in distributed.net projects only.
  * Any other distribution or use of this source violates copyright.
 */
 const char *core_ogr_ng_cpp(void) {
-return "@(#)$Id: core_ogr_ng.cpp,v 1.46 2012/04/29 14:30:34 snikkel Exp $"; }
+return "@(#)$Id: core_ogr_ng.cpp,v 1.47 2015/06/27 21:43:52 zebe Exp $"; }
 
 //#define TRACE
 
@@ -72,6 +72,8 @@ return "@(#)$Id: core_ogr_ng.cpp,v 1.46 2012/04/29 14:30:34 snikkel Exp $"; }
       CoreDispatchTable *ogrng_get_dispatch_table_cj1_sse41(void);  //G (asm #6)
       CoreDispatchTable *ogrng_get_dispatch_table_cj1_sse2_lzcnt(void);  //H (asm #7)
     #endif
+#elif (CLIENT_CPU == CPU_ARM64) && (SIZEOF_LONG == 8)
+    CoreDispatchTable *ogrng64_get_dispatch_table(void);
 #elif (CLIENT_CPU == CPU_ARM)
     CoreDispatchTable *ogrng_get_dispatch_table(void);
     CoreDispatchTable *ogrng_get_dispatch_table_arm1(void);
@@ -641,6 +643,9 @@ int selcoreSelectCore_ogr_ng(Client *client, unsigned int threadindex,
     unit_func.ogr = ogrng64_get_dispatch_table();
     coresel = 0;
   }
+#elif (CLIENT_CPU == CPU_ARM64) && (SIZEOF_LONG == 8)
+  unit_func.ogr = ogrng64_get_dispatch_table();
+  coresel = 0;
 #elif (CLIENT_CPU == CPU_ARM)
   if (coresel == 1)
     unit_func.ogr = ogrng_get_dispatch_table_arm1();
