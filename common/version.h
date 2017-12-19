7809f0c00808aa392f07c02026ff90605594ef9c
Mike Reed <mikereedwt@gmail.com>
2017-12-19 21:40:17 +0000

    arm64 new ScalarFusion core

--- common/version.h
+++ common/version.h
@@ -34,9 +34,9 @@
 
 #define CLIENT_MAJOR_VER       2
 #define CLIENT_CONTEST         91
-#define CLIENT_BUILD           12
-#define CLIENT_BUILD_FRAC      521
-#define CLIENT_VERSIONSTRING   "2.9112-521"
+#define CLIENT_BUILD           13
+#define CLIENT_BUILD_FRAC      522
+#define CLIENT_VERSIONSTRING   "2.9113-522"
 
 /* combined version used in packets etc. ... */
 #define CLIENT_VERSION         ( (((u32)(CLIENT_CONTEST))    * 1000000UL) +  \
